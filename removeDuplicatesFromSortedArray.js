24024b1b3aa7a3136663b0e1a1ee6aaf569a5420
Allen <allenvs@mail.ru>
2020-10-01 20:38:32 +0300

    problem spec

---
+++ removeDuplicatesFromSortedArray.js
@@ -0,0 +1,4 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+// Given a sorted array nums, remove the duplicates in-place such that each element 
+// appears only once and returns the new length.
+
