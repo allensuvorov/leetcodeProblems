be009b4a833db8d45034756e83f894f7957a6db0
Allen <allenvs@mail.ru>
2020-10-01 22:00:02 +0300

    solution passed leetcode tests

--- removeDuplicatesFromSortedArray.js
+++ removeDuplicatesFromSortedArray.js
@@ -5,26 +5,24 @@
 var removeDuplicates = function(nums) {
     let length = 0; // unique sequence index
     
-    for (let i = 0; i <nums.length; i++) {
+    for (let i = 0; i <nums.length-1; i++) {
         let first = i; // grab index of first element
         while (nums[first] === nums[i+1]) { // untill we get to next unique element 
-            console.log('here in while');
             i++; // one step forward
+            if (i===nums.length-1) return length+1;
         };
         console.log(`i+1 = ${i+1}, nums[i+1] = ${nums[i+1]}`);
-        if (i !== first) { // if found dubplicate on that i step
-            length++; // increase index for unique sequence
-            nums[length] = nums[i+1]; // put next value to next unique
-            
-            console.log(length,i, nums[i], nums);
-        };
-        
+
+        length++; // increase index for unique sequence
+        nums[length] = nums[i+1]; // put next value to next unique
+        // console.log(length,i, nums[i], nums);
 
     };
-
+    console.log(nums);
     return length+1;
 };
 
-let nums5 = [0,0,1,1,1,2,2,3,3,4];
+let nums5 = [0,0,1,1,1,2,2,3,3,4,5];
 
 console.log(removeDuplicates(nums5));
+// console.log(removeDuplicates([1]));
