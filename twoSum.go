622a59bae5d09fe08b816dd28b97406e4120214c
Allen Suvorov <56049635+allensuvorov@users.noreply.github.com>
2022-06-23 01:31:44 +0300

    Update twoSum.go

--- twoSum.go
+++ twoSum.go
@@ -1,1 +1,19 @@
-// working on it
+func twoSum(nums []int, target int) []int {
+	numMap := make(map[int]int)
+
+	for i, num := range nums {
+		var b int = target - num
+		fmt.Println(b)
+
+		if v, found := numMap[b]; found {
+			return []int{v, i}
+		}
+		numMap[num] = i
+		fmt.Println(numMap)
+	}
+	return []int {0, 0}
+}
+
+func main() {
+	fmt.Println(twoSum([]int{3, 3}, 6))
+}
