3d7fc9b59f3e5edbd1e79392b9f48143af48c4f2
Allen Suvorov <56049635+allensuvorov@users.noreply.github.com>
2022-06-24 00:10:11 +0300

    started

--- 7ReverseInteger.go
+++ 7ReverseInteger.go
@@ -1,1 +1,17 @@
-
+func reverse(x int) int {
+	int := x
+	// ma 2147483647 min -2147483648
+	var rev int32 = 0
+	
+	if int < 0 {
+		int = -int
+	}
+	
+	for int !=0 {
+		pop = int%10
+		
+		
+	}
+	
+	return 10
+}
