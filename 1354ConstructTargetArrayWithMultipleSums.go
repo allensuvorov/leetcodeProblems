9a014f5d5c5c558a45c1dbbb7a8bccb79f1e2c06
Allen Suvorov <56049635+allensuvorov@users.noreply.github.com>
2022-06-24 23:57:58 +0300

    fixed Dif, reset max and sum

--- 1354ConstructTargetArrayWithMultipleSums.go
+++ 1354ConstructTargetArrayWithMultipleSums.go
@@ -1,26 +1,27 @@
 // first idea - start from the end result (reverse engineering)
 func isPossible(target []int) bool {
-    max := 0
-    sum := 0
-    maxI:= 0
-    
-    for {
-        for i, num := range target {
-            sum += num
-            if num > max {
-                max = num
-                maxI = i
-            }
-        }
+	max := 0
+	sum := 0
+	maxI := 0
 
-        if max == 1 {
-            return true
-        }
-        if max < sum - max + 1 {
-            return false
-        } else {
-            target[maxI] = sum - max // replace max with dif
-        }   
-    }
-    
+	for {
+		sum = 0
+		max = 0
+		for i, num := range target {
+			sum += num
+			if num > max {
+				max = num
+				maxI = i
+			}
+		}
+		// fmt.Println(target, sum, max, maxI)
+		if max == 1 {
+			return true
+		}
+		if max < sum-max+1 {
+			return false
+		} else {
+			target[maxI] = max - (sum - max) // replace max with dif
+		}
+	}
 }
