dcb9a244a91c727ee917700744931c6da09720a9
Allen Suvorov <56049635+allensuvorov@users.noreply.github.com>
2022-06-24 23:56:46 +0300

    my whiteboard code
    mistake calculating Dif, and forgot to reset max and sum

--- 1354ConstructTargetArrayWithMultipleSums.go
+++ 1354ConstructTargetArrayWithMultipleSums.go
@@ -1,1 +1,26 @@
 // first idea - start from the end result (reverse engineering)
+func isPossible(target []int) bool {
+    max := 0
+    sum := 0
+    maxI:= 0
+    
+    for {
+        for i, num := range target {
+            sum += num
+            if num > max {
+                max = num
+                maxI = i
+            }
+        }
+
+        if max == 1 {
+            return true
+        }
+        if max < sum - max + 1 {
+            return false
+        } else {
+            target[maxI] = sum - max // replace max with dif
+        }   
+    }
+    
+}
