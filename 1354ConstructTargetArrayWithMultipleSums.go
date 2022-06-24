6d7aac978ee7fd9b690782d861ee435977f42b20
Allen Suvorov <56049635+allensuvorov@users.noreply.github.com>
2022-06-25 00:46:48 +0300

    fixed case with rest = 1

--- 1354ConstructTargetArrayWithMultipleSums.go
+++ 1354ConstructTargetArrayWithMultipleSums.go
@@ -1,4 +1,10 @@
 // first idea - start from the end result (reverse engineering)
+package main
+
+import (
+	"fmt"
+)
+
 func isPossible(target []int) bool {
 	max := 0
 	sum := 0
@@ -15,15 +21,25 @@ func isPossible(target []int) bool {
 				maxI = i
 			}
 		}
-		//fmt.Println(target, sum, max, maxI)
-		if max == 1 {
+		fmt.Println(target, sum, max, maxI)
+		rest = sum - max
+
+		if max == 1 || rest == 1 {
 			return true
 		}
-		rest = sum - max
-		if max < rest+1 {
+
+		if max < rest+1 || max%rest == 0 {
 			return false
 		} else {
 			target[maxI] = max % rest // replace max with remainder
 		}
 	}
 }
+
+func main() {
+
+	fmt.Println(isPossible([]int{1, 1}))
+	fmt.Println(isPossible([]int{2, 900000001}))
+	fmt.Println(isPossible([]int{2, 900000002}))
+}
+
