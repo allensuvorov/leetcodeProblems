efeac71bacdf28679a8365ea0a189c186c2fa3c7
Allen Suvorov <56049635+allensuvorov@users.noreply.github.com>
2022-07-09 19:26:55 +0300

    getting closer, need to keep head node

--- go/21MergeTwoSortedLists.go
+++ go/21MergeTwoSortedLists.go
@@ -7,18 +7,11 @@
  */
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
     
-    // type ListNode struct {
-    //     Val int
-    //     Next *ListNode
-    // }
-
-
+    var list3 *ListNode = new(ListNode)
     
     var node1 ListNode = *list1
     var node2 ListNode = *list2
-    var node3 ListNode = *new(ListNode)
-    
-    var list3 *ListNode = &HeadNode3
+    var node3 ListNode = *list3
     
     for node1.Next != nil && node2.Next != nil {
         
@@ -34,10 +27,18 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
         
         node3.Next = new(ListNode)
         
-        fmt.Println(list3)
+        fmt.Println("head is", list3)
         
         node3 = *node3.Next
+        fmt.Println("head is node3", list3)
     }
+    
+    if node1.Val >= node2.Val {
+            node3.Val = node1.Val
+    } else {
+            node3.Val = node2.Val
+    }
+    
     
     // fmt.Println(list1, node1, node2, node3, list3)
     
