a0ab171b2f55455cd0505fb1ae612a2b85144216
Allen Suvorov <56049635+allensuvorov@users.noreply.github.com>
2022-07-10 04:48:13 +0300

    huge optimisation, and realisation

--- go/21MergeTwoSortedLists.go
+++ go/21MergeTwoSortedLists.go
@@ -6,35 +6,39 @@
  * }
  */
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-    var list3 *ListNode = new(ListNode)
-    var head3 *ListNode = list3
-    var list1Done, list2Done bool
     if list1 == nil && list2 == nil {
         return nil
     }
-    for !list1Done || !list2Done {
-        if !list1Done && list1 != nil && list1.Val <= list2.Val {
+    if list1 == nil {
+        return list2
+    }
+    if list2 == nil {
+        return list1
+    }    
+    var list3 *ListNode = new(ListNode)
+    var head3 *ListNode = list3
+    
+    for {
+        if list1.Val <= list2.Val {
             list3.Val = list1.Val
             if list1.Next != nil {
                 list1 = list1.Next
             } else {
-                list1Done = true
+                list3.Next = list2
+                break
             }
-        } else if !list2Done && list2 != nil {
+        } else {
             list3.Val = list2.Val
             if list2.Next != nil{
                 list2 = list2.Next
             } else {
-                list2Done = true
+                list3.Next = list1
+                break
             }
         }
-        if !list1Done || !list2Done {
-            fmt.Println("testing - list 3 is growing")
-            list3.Next = new(ListNode)
-            list3 = list3.Next    
-        }
-        
-        
+
+        list3.Next = new(ListNode)
+        list3 = list3.Next
     }
     return head3
 }
