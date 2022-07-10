ddba8dcda832b6c6ce06ef614b3817c45fe40d68
Allen Suvorov <56049635+allensuvorov@users.noreply.github.com>
2022-07-10 03:49:55 +0300

    need to fix  [] and [0]

--- go/21MergeTwoSortedLists.go
+++ go/21MergeTwoSortedLists.go
@@ -9,31 +9,27 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
     var list3 *ListNode = new(ListNode)
     var head3 *ListNode = list3
     var list1Done, list2Done bool
-    
+    if list1 == nil && list2 == nil {
+        return nil
+    }
     for !list1Done || !list2Done {
-        if !list1Done && list1.Val <= list2.Val {
+        if !list1Done && list1 != nil && list1.Val <= list2.Val {
             list3.Val = list1.Val
-            fmt.Println("list1", list1.Val)
             if list1.Next != nil {
                 list1 = list1.Next
             } else {
                 list1Done = true
-                fmt.Println(list1Done)
             }
-        } else if !list2Done {
+        } else if !list2Done && list2 != nil {
             list3.Val = list2.Val
-            fmt.Println("list2", list2.Val)
             if list2.Next != nil{
                 list2 = list2.Next
             } else {
                 list2Done = true
-                fmt.Println(list2Done)
             }
         }
-        
-        fmt.Println(list1.Val, list2.Val, list3.Val)
-        
         if !list1Done || !list2Done {
+            fmt.Println("testing - list 3 is growing")
             list3.Next = new(ListNode)
             list3 = list3.Next    
         }
