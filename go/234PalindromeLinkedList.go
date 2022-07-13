3382f77ce2433c6a5cf50986ff03ad6a5a4b12e3
Allen <allenvs@mail.ru>
2022-07-13 11:48:37 +0300

    234 start

---
+++ go/234PalindromeLinkedList.go
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+package main
+
+import "fmt"
+
+func isPalindrome(head *ListNode) bool {
+    
+}
+
+func main() {
+    fmt.Println("Hello, World!")
+}
