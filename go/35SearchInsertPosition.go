72f3d99d843ce713cd60ee2ff5feeabd7df7950b
Allen <allenvs@mail.ru>
2022-07-23 23:51:49 +0300

    added last and first, but there is a bug 2+3/2 is always 2

--- go/35SearchInsertPosition.go
+++ go/35SearchInsertPosition.go
@@ -1,21 +1,37 @@
 func searchInsert(nums []int, target int) int {
-    
-    l := len(nums)-1 // biggest index
-    mid := math.Floor(l/2)
-    
-    for {
-        if target == nums[mid] {
-            return mid
-        }
 
-        if target > nums[mid] {
-            mid = math.Floor(mid + l-1)/2 // mid to len
-        } else {
-            mid = math.Floor(mid/2) // 0 to mid
-        }
-        
-        if mid == 0 
-    }
-    
-    return len(nums)
+	l := len(nums) - 1 // last index
+	ptr := l / 2
+
+	for {
+		if target == nums[ptr] {
+			return ptr
+		}
+		if target > nums[ptr] && target < nums[ptr+1] {
+			return ptr
+		}
+
+		if target < nums[ptr] && target > nums[ptr-1] {
+			return ptr - 1
+		}
+
+		if target > nums[ptr] {
+			ptr = (ptr + l) / 2 // mid to len
+		} else {
+			ptr = (ptr / 2) // 0 to mid
+		}
+
+		if ptr == l || ptr == 0 {
+			if target > nums[ptr] {
+				return ptr + 1
+			}
+			if target < nums[ptr] {
+				return ptr - 1
+			}
+		}
+		fmt.Println(ptr)
+
+	}
+
+	return len(nums)
 }
