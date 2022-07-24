affae4ff424dc7b616b81a95a70c339db9743595
Allen <allenvs@mail.ru>
2022-07-24 22:26:40 +0300

    217ContainsDuplicate.go lovely O(n) solution using hashmap

--- go/217ContainsDuplicate.go
+++ go/217ContainsDuplicate.go
@@ -1,4 +1,15 @@
-func containsDuplicate(nums []int) bool {
+func containsDuplicateFast(nums []int) bool {
+	uniqs := make(map[int]bool)
+	for _, num := range nums {
+		if _, contains := uniqs[num]; contains {
+			return true
+		}
+		uniqs[num] = true
+	}
+	return false
+}
+
+func containsDuplicateSlow(nums []int) bool {
 	for i := 1; i < len(nums); i++ {
 		for j := 0; j < i; j++ {
 			if nums[i] == nums[j] {
