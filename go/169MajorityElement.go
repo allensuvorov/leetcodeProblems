78be4912428d96b3abce52e5ea229b87f20404cb
Allen <allenvs@mail.ru>
2022-07-28 23:12:22 +0300

    fancy algo coming

---
+++ go/169MajorityElement.go
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/majority-element/
+// Will implement here:
+// Boyer-Moore Voting Algorithm
+// Complexity Analysis
+
+// Time complexity :
+// O(n)
+
+// Boyer-Moore performs constant work exactly
+// n
+// n times, so the algorithm runs in linear time.
+
+// Space complexity :
+
+// O(1)
+
+// Boyer-Moore allocates only constant additional memory.
