d755f4cde1280a69ff61c59644c3961e45090a22
Allen <allenvs@mail.ru>
2022-07-29 14:33:33 +0300

    implemented in 12 minutes

--- go/169MajorityElement.go
+++ go/169MajorityElement.go
@@ -6,12 +6,26 @@
 // Time complexity :
 // O(n)
 
-// Boyer-Moore performs constant work exactly
-// n
-// n times, so the algorithm runs in linear time.
+// Boyer-Moore performs constant work exactly n times, so the algorithm runs in linear time.
 
 // Space complexity :
 
 // O(1)
 
 // Boyer-Moore allocates only constant additional memory.
+
+func majorityElement(nums []int) int {
+	var ctr int
+	var mgr int
+	for _, num := range nums {
+		if ctr == 0 {
+			mgr = num
+		}
+		if num == mgr {
+			ctr++
+		} else {
+			ctr--
+		}
+	}
+	return mgr
+}
