833c30e6fa539c033f3a355c2aa8589674172752
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-01-05 20:27:08 +0600

    Update 507-Perfect-Number.go
    better solution

--- go/507-Perfect-Number.go
+++ go/507-Perfect-Number.go
@@ -1,12 +1,12 @@
 func checkPerfectNumber(num int) bool {
     end := int(math.Trunc(math.Sqrt(float64(num))))
+    if num == 1 {
+        return false
+    }
     sum := 0
-    for i := 1; i <= end; i++ {
+    for i := 1; i*i <= num; i++ {
         if num%i == 0 {
-            if i != num {
-                sum += i
-            }
-
+            sum += i
             if i != 1 || num/end != end && i == end {
                 sum += num/i
             } 
@@ -14,11 +14,6 @@ func checkPerfectNumber(num int) bool {
         if sum > num {
             return false
         }
-    }
-    
-    if sum == num {
-        return true
-    } else {
-        return false
-    }
+    } 
+    return sum == num
 }
