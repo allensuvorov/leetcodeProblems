3fbed587f9c82a24b1e3e9fe01baf54b5242ca0b
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-01-07 21:21:28 +0600

    Update 434-Number-of-Segments-in-a-String.go
    here is a shorter one

--- go/434-Number-of-Segments-in-a-String.go
+++ go/434-Number-of-Segments-in-a-String.go
@@ -1,12 +1,9 @@
 func countSegments(s string) int {
     res := 0
-    const space = byte(' ')
-    prev := space
     for i := 0; i < len(s); i++ {
-        if s[i] != space && prev == space {
+        if (i == 0 || s[i-1] == ' ') && s[i] != ' ' {
             res++
         }
-        prev = s[i]
     }
     return res
 }
