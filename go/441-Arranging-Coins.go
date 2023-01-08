5f4cdd6804336d163d0f10ee19dd42d7d11b3e1a
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-01-08 19:06:56 +0600

    Create 441-Arranging-Coins.go
    math and bruteforce

---
+++ go/441-Arranging-Coins.go
@@ -0,0 +1,21 @@
+// (k * (k + 1)) / 2 <= n
+
+/*
+func arrangeCoins(n int) int {
+    return int(math.Sqrt(2.0 * float64(n) + 0.25) - 0.5)
+}
+*/
+
+func arrangeCoins(n int) int {
+    sum := 0
+    inc := 1
+    i := 0
+    for ; sum < n; i++  {
+        sum += inc
+        inc ++
+        if sum > n {
+            return i
+        }
+    }
+    return i
+}
