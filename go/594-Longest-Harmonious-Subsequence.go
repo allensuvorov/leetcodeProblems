1d8a2e422cd6e15e04741133640f23910c4e9b1d
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-01-23 21:29:52 +0600

    Create  594-Longest-Harmonious-Subsequence.go
    my 34 min solution

---
+++ go/594-Longest-Harmonious-Subsequence.go
@@ -0,0 +1,19 @@
+func findLHS(nums []int) int {
+
+    hm := map[int]int{}
+    for _, v := range nums {
+        hm[v]++
+    }
+    max := 0
+    for i := range hm {
+        _, ok := hm[i+1]
+        if ok && hm[i] + hm[i+1] > max {
+            max = hm[i] + hm[i+1]
+        }
+        _, ok = hm[i-1]
+        if ok && hm[i] + hm[i-1] > max {
+            max = hm[i] + hm[i+1]
+        }
+    }
+    return max
+}
