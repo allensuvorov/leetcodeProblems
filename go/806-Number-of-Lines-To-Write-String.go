c60428f3ddc3b36c27f9be64b1df052c9f025ff2
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-01-27 22:49:47 +0600

    Update 806-Number-of-Lines-To-Write-String.go
    no special cases. if next ch fits in 100, add it. if doesn't fit it in 100, add a line.

--- go/806-Number-of-Lines-To-Write-String.go
+++ go/806-Number-of-Lines-To-Write-String.go
@@ -1,51 +1,13 @@
-// someone else did a short one 
 func numberOfLines(widths []int, s string) []int {
-    line:=0
-    sum:=0
-    for _,val:=range s{
-        index:=int(val-'a')
-        width:=widths[index]
-        if sum+width>100{
-            line++
-            sum=width
-        }else{
-            sum+=width
-        }
-    }
-    return []int{line+1,sum}
-}
-// i did a long one
-func numberOfLines(widths []int, s string) []int {
-    var sum int
-    count := 0
-    for i := 0; ; {
-        w := widths[rune(s[i]) - 'a']
-        
-        if sum + w < 100 {
-            sum += w
-            i++
-            if i == len(s) {
-                count++
-                break
-            }
-            continue
-        }
-
-        if sum + w == 100 {
-            sum += w
-            count++
-            i++
-            if i == len(s) {
-                break
-            }
-            sum = 0
-            continue
-        }
-
+    var sum, lineCount int
+    for _, v := range s{
+        w := widths[v - 'a']
         if sum + w > 100 {
-            count++
-            sum = 0
+            lineCount++
+            sum = w
+        } else {
+            sum += w
         }
     }
-    return []int{count, sum}
+    return []int{lineCount+1, sum}
 }
