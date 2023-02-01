48446de51bef6b6d955dd9ffef0e1383e4c052d7
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-02-01 17:53:58 +0600

    Update 657-Robot-Return-to-Origin.go
    switch case. much faster

--- go/657-Robot-Return-to-Origin.go
+++ go/657-Robot-Return-to-Origin.go
@@ -5,3 +5,18 @@ func judgeCircle(moves string) bool {
     }
     return moveCount['U'] == moveCount['D'] && moveCount['R'] == moveCount['L']
 }
+
+
+// no map/ switch case. much faster.
+func judgeCircle(moves string) bool {
+    y, x := 0, 0
+    for _, m := range moves {
+        switch m {
+        case 'U': y++
+        case 'D': y--
+        case 'R': x++
+        case 'L': x--
+        }
+    }
+    return x == 0 && y == 0
+}
