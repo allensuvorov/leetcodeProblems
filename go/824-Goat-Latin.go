fd383c1513eacb0400b72897e2289d06cb1164a8
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-02-05 22:42:28 +0600

    Update 824-Goat-Latin.go
    here is my first solution

--- go/824-Goat-Latin.go
+++ go/824-Goat-Latin.go
@@ -1,1 +1,17 @@
-'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'
+func toGoatLatin(sentence string) string {
+    words := strings.Split(sentence, " ")
+    for i := range words{
+        word := []byte(words[i])
+
+        switch word[0] {
+        case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+            word = append(word, 'm', 'a')
+        default:
+            word = append(word[1:], word[0], 'm', 'a')
+        }
+
+        word = append(word, bytes.Repeat([]byte{'a'}, i+1)...)
+        words[i] = string(word)
+    }
+    return strings.Join(words, " ")
+}
