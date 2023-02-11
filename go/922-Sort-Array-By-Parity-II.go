39a0a04dd6c5a71091b54a453b7ac6a623107e20
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-02-11 20:36:59 +0600

    Create 922-Sort-Array-By-Parity-II.go
    16 minutes

---
+++ go/922-Sort-Array-By-Parity-II.go
@@ -0,0 +1,20 @@
+func sortArrayByParityII(nums []int) []int {
+    even := 0
+    odd := 1
+
+    for {
+        if even > len(nums)-2 || odd > len(nums)-1 {
+            break
+        }
+        if nums[even]%2 == 0 {
+            even += 2 
+            continue
+        }
+        if nums[odd]%2 != 0 {
+            odd += 2
+            continue
+        }
+        nums[even], nums[odd] = nums[odd], nums[even]
+    }
+    return nums
+}
