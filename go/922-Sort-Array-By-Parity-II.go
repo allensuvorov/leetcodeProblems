3d829a29fde6c1c5b897f1c0b51df4286003efdf
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-02-11 20:42:09 +0600

    Update 922-Sort-Array-By-Parity-II.go
    moved break condition to loop statement

--- go/922-Sort-Array-By-Parity-II.go
+++ go/922-Sort-Array-By-Parity-II.go
@@ -1,11 +1,7 @@
 func sortArrayByParityII(nums []int) []int {
     even := 0
     odd := 1
-
-    for {
-        if even > len(nums)-2 || odd > len(nums)-1 {
-            break
-        }
+    for even <= len(nums)-2 && odd <= len(nums)-1 {
         if nums[even]%2 == 0 {
             even += 2 
             continue
