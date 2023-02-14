5fbcbf8702c9896a7832fba570b52dfce504259a
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-02-14 13:52:34 +0600

    Update 2124-Check-if-All-As-Appears-Before-All-Bs.go
    can't make it shorter

--- go/2124-Check-if-All-As-Appears-Before-All-Bs.go
+++ go/2124-Check-if-All-As-Appears-Before-All-Bs.go
@@ -1,25 +1,13 @@
 func checkString(s string) bool {
     // look for a after first b
-    i := 0
-    j := 0
-    foundB := false
-    for i < len(s) {
+    for i := 0; i < len(s); i++ {
         if s[i] == 'b' {
-            j = i
-            foundB = true
-            break
-        }
-        i++
-    }
-
-    if foundB {
-        for j < len(s) {
-            if s[j] == 'a' {
-                return false
+            for ; i < len(s); i++ {
+                if s[i] == 'a' {
+                    return false
+                }
             }
-            j++
         }
     }
-
     return true
 }
