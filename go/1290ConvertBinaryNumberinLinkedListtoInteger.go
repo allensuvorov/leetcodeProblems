3a26de72f0fba624dff9580f7d4d0e63d8c6aaf3
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-02-15 15:27:15 +0600

    Update 1290ConvertBinaryNumberinLinkedListtoInteger.go
    4 lines, good reminder how to traverse linked lists

--- go/1290ConvertBinaryNumberinLinkedListtoInteger.go
+++ go/1290ConvertBinaryNumberinLinkedListtoInteger.go
@@ -5,31 +5,11 @@
  *     Next *ListNode
  * }
  */
-func getDecimalValue(head *ListNode) int {
-	var dec, l float64
-	// get len
-	ptr := head
-	for ptr != nil {
-		ptr = ptr.Next
-		l++
-	}
-	ptr = head
-	for i := l - 1; i >= 0; i-- {
-		dec += float64(ptr.Val) * math.Pow(2, i)
-		fmt.Println(ptr.Val, math.Pow(2, i))
-		ptr = ptr.Next
-	}
-	return int(dec)
-}
 
-// sweet solution to multiply and add next val
 func getDecimalValue(head *ListNode) int {
-	var num = head.Val
-
-	for head.Next != nil {
-		num = num*2 + head.Next.Val
-		fmt.Println(num, head.Next.Val)
-		head = head.Next
-	}
-	return num
+    result := 0
+    for node := head; node != nil; node = node.Next {
+        result = result*2 + node.Val
+    }
+    return result
 }
