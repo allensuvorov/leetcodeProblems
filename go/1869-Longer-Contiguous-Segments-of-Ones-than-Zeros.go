d8fcdc69dc93f56d377e20d7ca18d14176690fed
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-02-17 17:01:44 +0600

    Update 1869-Longer-Contiguous-Segments-of-Ones-than-Zeros.go
    Max update only at the end of Contiguous Segments. Corner case - loop end.

--- go/1869-Longer-Contiguous-Segments-of-Ones-than-Zeros.go
+++ go/1869-Longer-Contiguous-Segments-of-Ones-than-Zeros.go
@@ -4,13 +4,17 @@ func checkZeroOnes(s string) bool {
     mx := map[rune]int{}
     for _, v := range s{
         cb[v]++
-        if mx[v] < cb[v] {
-            mx[v] = cb[v]
-        }
-        if v != prev {
+        if v != prev{
+            if mx[prev] < cb[prev] {
+                mx[prev] = cb[prev]
+            }
             cb[prev] = 0
         }
         prev = v
     }
+    lr := rune(s[len(s)-1])
+        if mx[lr] < cb[lr] {
+            mx[lr] = cb[lr]
+        }
     return mx['1'] > mx['0']
 }
