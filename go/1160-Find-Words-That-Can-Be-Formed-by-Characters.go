4b2138bb2d97bf42e3e791719b0092447262c10e
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-02-19 18:12:40 +0600

    Update 1160-Find-Words-That-Can-Be-Formed-by-Characters.go
    from runes to bytes. same speed. 27 ms

--- go/1160-Find-Words-That-Can-Be-Formed-by-Characters.go
+++ go/1160-Find-Words-That-Can-Be-Formed-by-Characters.go
@@ -1,22 +1,23 @@
 func countCharacters(words []string, chars string) int {
-    cm := map[rune]int{}
-    for _, v := range chars {
-        cm[v]++
+    cm := map[byte]int{}
+    for i := range chars {
+        cm[chars[i]]++
     }
+
     ans := 0
-    for _, w := range words {
-        // cm has c's for w
+    for i := range words {
         isGood := true
-        wcm := map[rune]int{}
-        for _, c := range w {
-            wcm[c]++
-            if wcm[c] > cm[c] {
+        wcm := map[byte]int{}
+
+        for j := range words[i] {
+            wcm[words[i][j]]++
+            if wcm[words[i][j]] > cm[words[i][j]] {
                 isGood = false
                 break
             }
         }
         if isGood {
-            ans += len(w)
+            ans += len(words[i])
         }
     }
     return ans
