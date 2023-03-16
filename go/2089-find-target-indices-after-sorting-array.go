9672856c4a9b3e0ae7225044acba144c1007d191
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-03-16 16:04:35 +0600

    Update 2089-find-target-indices-after-sorting-array.go
    Count less and equal. No need to sort.

--- go/2089-find-target-indices-after-sorting-array.go
+++ go/2089-find-target-indices-after-sorting-array.go
@@ -1,14 +1,17 @@
 func targetIndices(nums []int, target int) []int {
-    sort.SliceStable(nums, func(i, j int) bool {
-        return nums[i] < nums[j]
-    })
-    i := sort.Search(len(nums), func(i int) bool {
-        return nums[i] >= target
-    })
-    result := []int{}
-    for i < len(nums) && nums[i] == target {
-        result = append(result, i)
-        i ++
+    lessCount := 0
+    targetCount := 0
+    for i := range nums {
+        if nums[i] == target {
+            targetCount ++
+        }
+        if nums[i] < target {
+            lessCount ++
+        }
+    }
+    result := make([]int, targetCount)
+    for i := range result {
+        result[i] = lessCount + i 
     }
     return result
 }
