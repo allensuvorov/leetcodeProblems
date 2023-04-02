e0ceb10fbe6a5030bf4dd45dcac64293b16b1a0f
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-04-02 20:49:42 +0600

    Update 35_search_insert_position.go
    * binary search with switch

--- go/easy/35_search_insert_position.go
+++ go/easy/35_search_insert_position.go
@@ -1,3 +1,13 @@
 func searchInsert(nums []int, target int) int {
-    return sort.Search(len(nums), func(i int) bool {return nums[i] >= target})
+    l := 0
+    r := len(nums)-1
+    for l <= r {
+        m := (l+r)/2
+        switch {
+        case nums[m] == target: return m
+        case nums[m] < target: l = m + 1
+        case nums[m] > target: r = m - 1
+        }
+    }
+    return l
 }
