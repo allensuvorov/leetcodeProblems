a6d86c1c3a8a46b1dc5e90a2473de2f55810d09b
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-04-12 18:34:38 +0600

    Create 1342_number_of_steps_to_reduce_a_number_to_zero.go
    * one simple loop

---
+++ go/easy/1342_number_of_steps_to_reduce_a_number_to_zero.go
@@ -0,0 +1,15 @@
+package easy
+
+func numberOfSteps(num int) int {
+    ans := 0
+    for num > 0 {
+        if num%2 == 0 {
+            num /= 2
+            ans++
+        } else {
+            num -= 1
+            ans++
+        }
+    }
+    return ans
+}
