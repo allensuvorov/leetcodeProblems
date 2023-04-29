c3d96d0401832c1cb7efccf1c6ecbeb97c315bd3
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-04-29 16:58:55 +0600

    Update 2062_count_vowel_substrings_of_a_string.go
    * hash map Len does it to know we have a complete set
    * zero extra memory

--- go/easy/2062_count_vowel_substrings_of_a_string.go
+++ go/easy/2062_count_vowel_substrings_of_a_string.go
@@ -1,18 +1,17 @@
-package easy
-
 func countVowelSubstrings(word string) int {
-    vm := map[byte]bool{'a':true, 'e':true, 'i':true, 'o':true, 'u':true}
-    targetVS := [26]bool{('a'-'a'):true, ('e'-'a'):true, ('i'-'a'):true, ('o'-'a'):true, ('u'-'a'):true}
-    
     res := 0
     for l := range word {
-        currentVS := [26]bool{}
-        for r := l; r < len(word) && vm[word[r]]; r++ {
-            currentVS[word[r]-'a'] = true
-            if currentVS == targetVS {
+        currentVS := map[byte]bool{}
+        for r := l; r < len(word) && isVowel(word[r]); r++ {
+            currentVS[word[r]] = true
+            if len(currentVS) == 5 {
                 res++
             }
         }
     }
     return res
+}
+
+func isVowel (b byte) bool {
+    return b == 'a' || b == 'e' || b == 'i' || b == 'o' || b == 'u'
 }
