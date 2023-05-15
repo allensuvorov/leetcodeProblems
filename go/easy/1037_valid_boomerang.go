93bd9f42f5719b50ecc5b336a1c420141df48cda
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-05-15 15:59:38 +0600

    Create 1037_valid_boomerang.go
    * Area of triangle
    * Math knowledge

---
+++ go/easy/1037_valid_boomerang.go
@@ -0,0 +1,9 @@
+func isBoomerang(points [][]int) bool {
+    x1, x2, x3 := points[0][0], points[1][0], points[2][0]
+    y1, y2, y3 := points[0][1], points[1][1], points[2][1]
+    area := x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2)
+
+    return area != 0
+}
+
+// Area of triangle (ΔABC) = (1/2) | x1 (y2 – y3 ) + x2 (y3 – y1 ) + x3(y1 – y2) |
