5408f0de9e12d05ef84aa6b46ef76720fc3a2f51
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-06-02 16:04:19 +0600

    Update and rename 1534. Count Good Triplets to 1534_count_good_triplets.go
    * 3 nested loops are 4 ms
    * O(n^2)

--- go/easy/1534_count_good_triplets.go
+++ go/easy/1534_count_good_triplets.go
@@ -1,3 +1,29 @@
+// 4 ms
+func abs(n int) int{
+    if n < 0 {
+        return -n
+    }
+    return n
+}
+
+func countGoodTriplets(arr []int, a int, b int, c int) int {
+    ans := 0
+    for i := 0; i < len(arr) - 2; i++ {
+        for j:= i + 1; j < len(arr) - 1; j++ {
+            if abs(arr[i] - arr[j]) <= a {
+                for k := j + 1; k < len(arr); k++ {
+                    if abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c{
+                        ans++
+                    }
+                }
+            }
+        }
+    }
+    return ans
+}
+
+// 500 ms
+
 func countGoodTriplets(arr []int, a int, b int, c int) int {
     ans := 0
     i, j, k := 0, 1, 2
