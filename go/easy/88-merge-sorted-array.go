241a0408bb7de8a69b84706d300e81238ec8ebc1
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-07-18 23:51:36 +0600

    Create 88-merge-sorted-array.go
    * there is a bug
    * need to fix

---
+++ go/easy/88-merge-sorted-array.go
@@ -0,0 +1,14 @@
+func merge(nums1 []int, m int, nums2 []int, n int)  {
+    // start from end of nums1
+    i1, i2 := m - 1, n - 1
+    
+    for i := m + n - 1; i >= 0 && i1 >= 0 && i2 >= 0; i-- {
+        if nums1[i1] >= nums2[i2] {
+            nums1[i] = nums1[i1]
+            i1--
+        } else {
+            nums1[i] = nums2[i2]
+            i2--
+        }
+    }
+}
