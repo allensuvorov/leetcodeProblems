238604eacfb92d054ce0a48e36f65e42bfb9d383
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-07-19 00:42:31 +0600

    Update 88-merge-sorted-array.go
    * if m == 0 case taken care of

--- go/easy/88-merge-sorted-array.go
+++ go/easy/88-merge-sorted-array.go
@@ -1,14 +1,15 @@
 func merge(nums1 []int, m int, nums2 []int, n int)  {
     // start from end of nums1
-    i1, i2 := m - 1, n - 1
-    
-    for i := m + n - 1; i >= 0 && i1 >= 0 && i2 >= 0; i-- {
-        if nums1[i1] >= nums2[i2] {
-            nums1[i] = nums1[i1]
-            i1--
+    for i := m + n - 1; i >= 0; i-- {
+        if n == 0 {
+            return
+        }
+        if m == 0 || nums1[m-1] < nums2[n-1] {
+            nums1[i] = nums2[n-1]
+            n--
         } else {
-            nums1[i] = nums2[i2]
-            i2--
+            nums1[i] = nums1[m-1]
+            m--
         }
     }
 }
