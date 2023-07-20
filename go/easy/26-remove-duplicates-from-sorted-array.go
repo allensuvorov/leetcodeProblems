da97b5d7fbd6ecd878ff5ce770d9919ca8bd98f1
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-07-21 00:12:57 +0600

    Update 26-remove-duplicates-from-sorted-array.go
    * two pointers
    * O(n)

--- go/easy/26-remove-duplicates-from-sorted-array.go
+++ go/easy/26-remove-duplicates-from-sorted-array.go
@@ -1,10 +1,10 @@
 func removeDuplicates(nums []int) int {
-    
+    lastUniq := 1
+    for i := 1; i < len(nums); i ++ {
+        if nums[i] != nums[lastUniq-1] {
+            nums[lastUniq] = nums[i]
+            lastUniq++
+        }
+    }
+    return lastUniq
 }
-
-/*
-Solution logic:
-1. remove dups
-2. move to close gaps
-
-*/
