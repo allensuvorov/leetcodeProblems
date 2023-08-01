61e138c36f064b29f9bfc8526ba15eaad2cb197f
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-08-01 14:00:53 +0600

    Update 14_longest_common_prefix.go
    * take first string and shorten it down

--- go/easy/14_longest_common_prefix.go
+++ go/easy/14_longest_common_prefix.go
@@ -1,33 +1,11 @@
 func longestCommonPrefix(strs []string) string {
-    // iterate array
-    // check 1st, 2nd ... letter in each string, is it the same?
-        // if different - return result, 
-        // else append letter to result
-    //
-    // capture letters one by one 
-
-    var (
-        res []byte
-        b byte
-        bi int // byte index
-    )
-    
-    for {
-        if bi > len(strs[0])-1 {
-                return string (res)
-            }
-        b = strs[0][bi]
-        
-        for i:=1; i<len(strs); i++ {
-            if bi > len(strs[i])-1 {
-                return string (res)
-            }
-            if b != strs[i][bi]{
-                return string (res)
-            }
+    ans := []byte(strs[0])
+    for i := 1; i < len(strs); i++ {
+        j := 0
+        for j < len(ans) && j < len(strs[i]) && ans[j] == strs[i][j] {
+            j++
         }
-        res = append(res, b)
-        bi++
+        ans = ans[:j]
     }
+    return string(ans)
 }
-
