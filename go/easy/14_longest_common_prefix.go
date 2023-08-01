2889df80a4e142e6cc6c34c25cf665d41995f5f7
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-08-01 14:05:22 +0600

    Update 14_longest_common_prefix.go
    * early break for no common prefix

--- go/easy/14_longest_common_prefix.go
+++ go/easy/14_longest_common_prefix.go
@@ -1,6 +1,6 @@
 func longestCommonPrefix(strs []string) string {
     ans := []byte(strs[0])
-    for i := 1; i < len(strs); i++ {
+    for i := 1; i < len(strs) && len(ans) > 0; i++ {
         j := 0
         for j < len(ans) && j < len(strs[i]) && ans[j] == strs[i][j] {
             j++
