fbbc9e5ac5811fcaa3aaffaa12eb042276aeb6bf
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-08-05 23:18:32 +0600

    Update 392-Is-Subsequence.go
    * 2 pointers: collector and scout
    * 0(n)

--- go/easy/392-Is-Subsequence.go
+++ go/easy/392-Is-Subsequence.go
@@ -1,52 +1,14 @@
 func isSubsequence(s string, t string) bool {
-    j := 0
-    hm := map[byte]int{}
-    for i := range t {
-        hm[t[i]] ++
-    }
-
-    for i := 0; i<len(s); i++ {
-        if j > len(t) - 1 {
-            return false
-        }
-        
-        if hm[s[i]] <= 0 {
-            return false
-        }
-
-        inSubseq := false
-        for j < len(t) {
-            if s[i] == t[j] {
-                inSubseq = true
-                hm[s[i]] --
-                j++
-                break
-            }
-            j++
-        }
-        if !inSubseq {
-            return false
-        }
-    }
-    return true
-}
-
-// a shorter one
-func isSubsequence(s string, t string) bool {
-    i,j := 0, 0
-    hm := map[byte]int{}
-    for i := range t {
-        hm[t[i]] ++
-    }
-    for i < len(s) && j < len(t) {
-        if s[i] == t[j] {
-            hm[s[i]] --
-            i++
-            if i < len(s) && hm[s[i]] <= 0 {
-                return false
-            }
-        }
-        j++
-    }
-    return i == len(s)
+   collector := 0
+   scout := 0
+   for collector < len(s) && scout < len(t) {
+       if s[collector] == t[scout] {
+           collector++
+       }
+       scout++
+   }
+   if collector != len(s) {
+       return false
+   }
+   return true
 }
