75850a7cc48f9b3b89dbdf3066142100756698f3
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-08-07 23:57:53 +0600

    Update 205-Isomorphic-Strings.go
    * rebuilding

--- go/easy/205-Isomorphic-Strings.go
+++ go/easy/205-Isomorphic-Strings.go
@@ -1,16 +1,14 @@
 func isIsomorphic(s string, t string) bool {
-    mapping_s_t := make(map[byte]byte)
-    mapping_t_s := make(map[byte]byte)
+    ab := map[byte]byte{s[0]:t[0]}
+    ba := map[byte]byte{t[0]:s[0]}
 
     for i := range s {
-        _, ok1 := mapping_s_t[s[i]]
-        _, ok2 := mapping_t_s[t[i]]
-        if !ok1 && !ok2 {
-            mapping_s_t[s[i]] = t[i]
-            mapping_t_s[t[i]] = s[i]
-        } else if mapping_s_t[s[i]] != t[i] || mapping_t_s[t[i]] != s[i] {
-                return false
+        if ab[s[i]] != t[i] || ba[t[i]] != s[i] {
+            return false
         }
+        ab[s[i]] = t[i]
+        ba[t[i]] = s[i]
     }
+
     return true
 }
