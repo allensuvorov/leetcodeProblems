0da74de27b2bdd79141eaaf199cb84648330de60
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-08-16 01:18:05 +0600

    Update 66_plus_one.go
    * carry is one

--- go/easy/66_plus_one.go
+++ go/easy/66_plus_one.go
@@ -1,12 +1,11 @@
 func plusOne(digits []int) []int {
-    var carry int = 1
-    for i:=len(digits)-1; i>=0; i-- {
+    for i := len(digits) - 1; i >= 0; i-- {
         if digits[i] < 9 {
-            digits[i] +=carry
+            digits[i] += 1
             return digits
         }
         digits[i] = 0
     }
-    digits = append ([]int{1}, digits...)
+    digits = append([]int{1}, digits...)
     return digits
 }
