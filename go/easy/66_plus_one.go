e5a82edc43c0f4f987e630b960db95b859f99396
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-08-16 01:54:33 +0600

    Update 66_plus_one.go
    * append returns a new slice

--- go/easy/66_plus_one.go
+++ go/easy/66_plus_one.go
@@ -1,13 +1,10 @@
 func plusOne(digits []int) []int {
-    i := len(digits) - 1
-    for i >= 0 && digits[i] == 9 {
+    for i := len(digits) - 1; i >= 0; i-- {
+        if digits[i] < 9 {
+            digits[i] += 1
+            return digits
+        }
         digits[i] = 0
-        i--
     }
-    if i < 0 {
-        digits = append([]int{1}, digits...)
-    } else {
-        digits[i] += 1
-    }
-    return digits
+    return append([]int{1}, digits...)
 }
