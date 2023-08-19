09e5ddd3fc9febe594c8814856eb3e13ab301e0d
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-08-19 20:04:45 +0600

    Update 219-Contains-Duplicate-II.go
    * primary solution with hash map
    * time O(n)

--- go/easy/219-Contains-Duplicate-II.go
+++ go/easy/219-Contains-Duplicate-II.go
@@ -1,41 +1,12 @@
 func containsNearbyDuplicate(nums []int, k int) bool {
-    hm := map[int]int{}
+    m := map[int]int{}
     for i, v := range nums {
-        if _, ok := hm[v]; !ok {
-            hm[v] = i
-            continue
+        if j, ok := m[v]; ok {
+            if i - j <= k {
+                return true
+            } 
         }
-        if i - hm[v] <= k {
-            return true
-        } else {
-            hm[v] = i
-        }
-    }
-    return false
-}
-
-// someone added a cleaner one
-func containsNearbyDuplicate(nums []int, k int) bool {
-    recorder := make(map[int]int)
-    for i, num := range nums {
-        j, ok := recorder[num]
-        if ok && i - j <= k {
-            return true
-        }
-        recorder[num] = i
-    }
-    return false
-}
-
-// updated my solution
-
-func containsNearbyDuplicate(nums []int, k int) bool {
-    hm := map[int]int{}
-    for i, v := range nums {
-        if _, ok := hm[v]; ok && i - hm[v] <= k {
-            return true
-        }
-        hm[v] = i
+        m[v] = i
     }
     return false
 }
