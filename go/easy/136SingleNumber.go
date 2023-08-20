a18010764d6be1fb49635b403d0bff5b3d51101c
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-08-20 21:53:50 +0600

    Update 136SingleNumber.go
    * XOR (exclusive OR) does the magic
    * duplicate bits turn to zeros
    * only the unique bits stay till the end

--- go/easy/136SingleNumber.go
+++ go/easy/136SingleNumber.go
@@ -1,19 +1,7 @@
-package main
-
-import (
-	"fmt"
-)
-
-func singleNumber (nums []int) int {
-  result := nums[0]
-  for i := 1; i<len(nums);i++ {
-    result ^= nums[i]
-  }
-  return result
-}
-
-func main() {
-  nums := []int {1,1,2,2,3}
-  fmt.Println(singleNumber(nums))
-	fmt.Println("Hello, World!")
+func singleNumber(nums []int) int {
+    ans := 0
+    for _, v := range nums {
+        ans ^= v
+    }
+    return ans
 }
