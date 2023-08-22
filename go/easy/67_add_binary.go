767a477559faf09c245f9e6c3e9aa73e8ac623ca
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-08-22 19:00:50 +0600

    Update and rename 67AddBinary.go to 67_add_binary.go
    * O(n)

---
+++ go/easy/67_add_binary.go
@@ -0,0 +1,30 @@
+func addBinary(a string, b string) string {
+    maxLen := len(a)
+    if len(b) > len(a) {
+        maxLen = len(b)
+    }
+    c := make([]byte, maxLen)
+    var carry byte
+    for i := 0; i < len(a) || i < len(b); i++ {
+        var aBit, bBit, cBit byte
+        if i < len(a) {
+            aBit = a[len(a) - 1 - i] - '0'
+        }
+        if i < len(b) {
+            bBit = b[len(b) - 1 - i] - '0'
+        }
+        cBit = carry + aBit + bBit // 0, 1, 2, 3
+        
+        if cBit > 1 {
+            carry = 1
+            cBit = cBit%2
+        } else {
+            carry = 0
+        }
+        c[len(c) - 1 - i] = cBit + '0'
+    }
+    if carry == 1 {
+        c = append([]byte{carry + '0'}, c...)
+    }
+    return string(c)
+}
