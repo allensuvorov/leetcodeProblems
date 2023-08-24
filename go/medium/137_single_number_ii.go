86dbb43699d8df5376ba8b3a846739ea9c4b8873
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-08-24 22:12:26 +0600

    Update 137_single_number_ii.go
    * XORs with AND NOTs
    * need to review how it works

--- go/medium/137_single_number_ii.go
+++ go/medium/137_single_number_ii.go
@@ -1,1 +1,13 @@
 // working on a bitwise solution
+func singleNumber(nums []int) int {
+  var ones int = 0
+	var twos int = 0
+
+	for i := 0; i < len(nums); i++ {
+		var number int = nums[i]
+		ones ^= (number & ^twos)
+		twos ^= (number & ^ones)
+	}
+
+	return ones
+}
