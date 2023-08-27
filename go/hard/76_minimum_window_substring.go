cfacd0ab75b00ba779e3edd148bb7b76eb7101fa
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-08-27 22:10:00 +0600

    Update 76_minimum_window_substring.go
    * needCode
    * great solution
    * O(n+m)
    * 17 ms

--- go/hard/76_minimum_window_substring.go
+++ go/hard/76_minimum_window_substring.go
@@ -1,68 +1,37 @@
 func minWindow(s string, t string) string {
-    checkList := map[byte]int{}
-    
-    targetMap := [127]int{}
-    windowMap := [127]int{}
+    tCount, window := map[byte]int{}, [127]int{}
 
-    for i := 0; i < len(t); i++ { // fill Target Map with letter frequencies
-        targetMap[t[i]]++
-        checkList[t[i]]++
+    for i := 0; i < len(t); i++ {
+        tCount[t[i]]++
     }
 
-    // init r. r captures letters till target collection is fully captured by the window
-    r := -1
-    for i := 0; i < len(s); i++ {
-        if targetMap[s[i]] != 0 { // fill up windowMap
-            windowMap[s[i]]++
-            if checkList[s[i]] != 0 {
-                checkList[s[i]]-- // to track progress
-            } 
-            if checkList[s[i]] == 0 {
-                delete(checkList, s[i])
+    have, need := 0, len(tCount)
+    res, resLen := []int{-1,-1}, len(s) + 10
+    l := 0
+    for r := range s {
+        c := s[r]
+        window[c]++
+
+        if tCount[c] != 0 && window[c] == tCount[c]{
+            have++
+        }
+
+        for have == need {
+            // update results
+            if (r - l + 1) < resLen {
+                res = []int{l, r}
+                resLen = r - l + 1
             }
-        }
-        if len(checkList) == 0 {
-            r = i
-            break
+            // pop from the left of our window
+            window[s[l]]--
+            if tCount[s[l]] != 0 && window[s[l]] < tCount[s[l]] {
+                have--
+            }
+            l++
         }
     }
-    if r == -1 {
+    if resLen == len(s) + 10 {
         return ""
     }
-    
-    // Second, loop through windows, find min
-    l := 0
-    minLen := len(s)
-    ans := s
-
-    for r < len(s) {
-        // l trims tail till a must-have letter
-        for ; l < len(s); l++ {
-            if targetMap[s[l]] != 0 {
-                if windowMap[s[l]] - targetMap[s[l]] >= 1 {
-                   windowMap[s[l]]--
-                } else {
-                    break
-                }
-            }
-        }
-        mustHave := s[l]
-        if r - l + 1 < minLen {
-            minLen = r - l + 1
-            ans = s[l:r+1]
-        }
-        
-        // look for next window, update r
-        r++
-        for r < len(s) {
-            if targetMap[s[r]] != 0 {
-                windowMap[s[r]]++
-                if s[r] == mustHave {
-                    break
-                }
-            }
-            r++
-        }
-    }
-    return ans
+    return s[res[0]:(res[1] + 1)]
 }
