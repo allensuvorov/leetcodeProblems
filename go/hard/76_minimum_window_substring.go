531920f680088e99b1672305e213dd96b159a477
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-08-27 20:42:33 +0600

    Update 76_minimum_window_substring.go
    * removed log prints
    * 27 ms

--- go/hard/76_minimum_window_substring.go
+++ go/hard/76_minimum_window_substring.go
@@ -1,19 +1,18 @@
 func minWindow(s string, t string) string {
-    targetMap := map[byte]int{}
     checkList := map[byte]int{}
+    targetMap := map[byte]int{}
     windowMap := map[byte]int{}
 
     for i := 0; i < len(t); i++ { // fill Target Map with letter frequencies
         targetMap[t[i]]++
         checkList[t[i]]++
     }
-    // look for the initial window l and r 
-    // first, init r. r captures letters till target collection is fully captured by the window
+
+    // init r. r captures letters till target collection is fully captured by the window
     r := -1
     for i := 0; i < len(s); i++ {
         if _, ok := targetMap[s[i]]; ok { // fill up windowMap
             windowMap[s[i]]++
-            fmt.Println(checkList)
             if checkList[s[i]] != 0 {
                 checkList[s[i]]-- // to track progress
             } 
@@ -30,6 +29,7 @@ func minWindow(s string, t string) string {
         return ""
     }
     
+    // Second, loop through windows, find min
     l := 0
     minLen := len(s)
     ans := s
@@ -46,7 +46,6 @@ func minWindow(s string, t string) string {
             }
         }
         mustHave := s[l]
-        fmt.Println(l, r)
         if r - l + 1 < minLen {
             minLen = r - l + 1
             ans = s[l:r+1]
