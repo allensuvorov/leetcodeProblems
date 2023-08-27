ca91e474c13bf7aaaca39b5e9ce6f773104a0477
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-08-27 20:30:19 +0600

    Update 76_minimum_window_substring.go
    * O(n+m)
    * Slow, 900 ms

--- go/hard/76_minimum_window_substring.go
+++ go/hard/76_minimum_window_substring.go
@@ -1,31 +1,27 @@
 func minWindow(s string, t string) string {
-    // fill Target Map with letter frequencies
-    tm := map[byte]int{}
-    for _, v := range t {
-        tm[v]++
-    }
-    
-    // look for the first window, initialise l and r 
+    targetMap := map[byte]int{}
+    checkList := map[byte]int{}
+    windowMap := map[byte]int{}
 
-    // init l
-    l := -1
-    for i, v := range s {
-        if _, ok := tm[v]; ok{
-            l = i
-            break
-        }
+    for i := 0; i < len(t); i++ { // fill Target Map with letter frequencies
+        targetMap[t[i]]++
+        checkList[t[i]]++
     }
-    if l == -1 {
-        return ""
-    }
-    // init r
-    // r captures letters till target collection is fully captured
+    // look for the initial window l and r 
+    // first, init r. r captures letters till target collection is fully captured by the window
     r := -1
-    for i := l; i < len(s); i++ {
-        tm[s[i]]--
-        if tm[s[i]] == 0
-        delete(tm, s[i])
-        if len(tm) == 0 {
+    for i := 0; i < len(s); i++ {
+        if _, ok := targetMap[s[i]]; ok { // fill up windowMap
+            windowMap[s[i]]++
+            fmt.Println(checkList)
+            if checkList[s[i]] != 0 {
+                checkList[s[i]]-- // to track progress
+            } 
+            if checkList[s[i]] == 0 {
+                delete(checkList, s[i])
+            }
+        }
+        if len(checkList) == 0 {
             r = i
             break
         }
@@ -34,13 +30,39 @@ func minWindow(s string, t string) string {
         return ""
     }
     
-    
-    wm(s[r])++
-    wm := map[byte]int{}
-    
+    l := 0
+    minLen := len(s)
+    ans := s
 
-
-    ans := ""
-    minLen := len(t)
+    for r < len(s) {
+        // l trims tail till a must-have letter
+        for ; l < len(s); l++ {
+            if _, ok := targetMap[s[l]]; ok {
+                if windowMap[s[l]] - targetMap[s[l]] >= 1 {
+                   windowMap[s[l]]--
+                } else {
+                    break
+                }
+            }
+        }
+        mustHave := s[l]
+        fmt.Println(l, r)
+        if r - l + 1 < minLen {
+            minLen = r - l + 1
+            ans = s[l:r+1]
+        }
+        
+        // look for next window, update r
+        r++
+        for r < len(s) {
+            if _, ok := targetMap[s[r]]; ok {
+                windowMap[s[r]]++
+                if s[r] == mustHave {
+                    break
+                }
+            }
+            r++
+        }
+    }
     return ans
 }
