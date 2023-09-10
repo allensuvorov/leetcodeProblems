a7e3ae46a745ff244443c74dfeaaae7b7701e987
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-09-09 21:35:47 -0600

    Update 705_design_hashset.go
    * updated Add
    * using Contains in it now

--- go/easy/705_design_hashset.go
+++ go/easy/705_design_hashset.go
@@ -13,23 +13,20 @@ func Constructor() MyHashSet {
 
 func (this *MyHashSet) Add(key int)  {
     group := key % this.setSize
-    keySlice:= this.a[group]
-    for _, v := range keySlice {
-        if v == key {
-            return
-        }
+    if !this.Contains(key) {
+        this.a[group] = append(this.a[group], key)
     }
-    this.a[key % this.setSize] = append(this.a[key % this.setSize], key)
 }
 
 
 func (this *MyHashSet) Remove(key int)  {
     group := key % this.setSize
-    keySlice:= this.a[group]
+    keySlice := this.a[group]
+    lastIndex := len(keySlice)-1
     for i, v := range keySlice {
         if v == key {
-            keySlice[i] = keySlice[len(keySlice)-1]
-            this.a[group] = keySlice[:len(keySlice)-1]
+            keySlice[i] = keySlice[lastIndex]
+            this.a[group] = keySlice[:lastIndex]
             return
         }
     }
