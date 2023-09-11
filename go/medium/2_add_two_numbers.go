2e906d771bf548dae17a18e2c7497e3e89dc58b4
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-09-10 20:21:49 -0600

    Update 2_add_two_numbers.go
    * carry = sum / 10

--- go/medium/2_add_two_numbers.go
+++ go/medium/2_add_two_numbers.go
@@ -5,40 +5,29 @@
  *     Next *ListNode
  * }
  */
-
-package medium
-
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	carry := 0
-	l3 := &ListNode{}
-	n1, n2, n3 := l1, l2, l3
-
-	for {
-		var v1, v2 int
-		if n1 != nil {
-			v1 = n1.Val
-			n1 = n1.Next
-		}
-
-		if n2 != nil {
-			v2 = n2.Val
-			n2 = n2.Next
-		}
-
-		sum := v1 + v2 + carry
-		n3.Val = sum % 10
-		if sum > 9 {
-			carry = 1
-		} else {
-			carry = 0
-		}
-
-		if n1 != nil || n2 != nil || carry != 0 {
-			n3.Next = &ListNode{}
-			n3 = n3.Next
-		} else {
-			break
-		}
-	}
-	return l3
+    var carry int
+    l3 := &ListNode{}
+    n1, n2, n3 := l1, l2, l3
+    for {
+        sum := carry
+        if n1 != nil {
+            sum += n1.Val
+            n1 = n1.Next
+        }
+        if n2 != nil {
+            sum += n2.Val
+            n2 = n2.Next
+        }
+        n3.Val = sum % 10
+        carry = sum / 10
+        
+        if n1 != nil || n2 != nil || carry != 0 {
+            n3.Next = &ListNode{}
+            n3 = n3.Next
+        } else {
+            break
+        }
+    }
+    return l3
 }
