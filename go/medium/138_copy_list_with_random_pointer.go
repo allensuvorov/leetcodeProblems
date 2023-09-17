b108ecd89ba1b551aab9a2be50884605a9531a8a
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-09-16 22:55:03 -0600

    Update 138_copy_list_with_random_pointer.go
    * just a hash map
    * and two loops

--- go/medium/138_copy_list_with_random_pointer.go
+++ go/medium/138_copy_list_with_random_pointer.go
@@ -8,8 +8,7 @@
  */
 
 func copyRandomList(head *Node) *Node {
-    ptrInd := map[*Node]int{}
-    indPtr := []*Node{}
+    listMap := map[*Node]*Node{}
     dummy := &Node{}
     l2 := dummy
     l1 := head
@@ -18,15 +17,14 @@ func copyRandomList(head *Node) *Node {
         l2 = l2.Next
         l2.Val = l1.Val
         
-        ptrInd[l1] = i
-        indPtr = append(indPtr, l2)
+        listMap[l1] = l2
         l1 = l1.Next
     }
     l1 = head
     l2 = dummy.Next
     for l1 != nil {
         if l1.Random != nil {
-            l2.Random = indPtr[ptrInd[l1.Random]]
+            l2.Random = listMap[l1.Random]
         }
         l1 = l1.Next
         l2 = l2.Next
