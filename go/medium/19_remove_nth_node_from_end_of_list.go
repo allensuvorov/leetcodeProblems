33357c7b034aab94086bec9a4198e30b50e68827
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-09-20 23:07:02 -0600

    Create 19_remove_nth_node_from_end_of_list.go
    * array to map indices and pointers
    * edge cases
    * bug or wrong test?

---
+++ go/medium/19_remove_nth_node_from_end_of_list.go
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+    if head.Next == nil {
+        return nil
+    }
+    ptrs := [33]*ListNode{}
+    cur := head
+    // fill the list of ptrs
+    i := 0
+    for i = 1; i < 30 && cur != nil; i++ {
+        ptrs[i] = cur
+        cur = cur.Next
+    }
+    i--
+    if i - n == 0 {
+        return head.Next
+    }
+    // go directly by pointers and reconnect nodes
+    ptrs[i-n].Next = ptrs[i-n+2]
+    return head
+}
