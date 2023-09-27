070291edc79e4e8ddd9b780acccffc47a4dc1ab8
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-09-26 19:36:58 -0600

    Update 2006_count_number_of_pairs_with_absolute_difference_k.go
    * O(n) time
    * hash map

--- go/easy/2006_count_number_of_pairs_with_absolute_difference_k.go
+++ go/easy/2006_count_number_of_pairs_with_absolute_difference_k.go
@@ -1,13 +1,10 @@
 func countKDifference(nums []int, k int) int {
-    if len(nums) == 1 {
-        return 0
-    }
     count := 0
-    numCount := [101]int{}
+    numCount := map[int]int{}
     for _, v := range nums {
         numCount[v]++
     }
-    for i := 1; i <= 100 - k; i++ {
+    for i := range numCount {
         count += numCount[i] * numCount[i+k]
     }
     return count
