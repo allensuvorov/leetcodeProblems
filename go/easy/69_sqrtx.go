1caaa0223d55ed5e9db126dec914707ae27c3538
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-10-03 20:35:44 -0600

    Update 69_sqrtx.go
    * binary search
    * left is good, so left side cuts include m 
    * but m is shifted to the right a notch

--- go/easy/69_sqrtx.go
+++ go/easy/69_sqrtx.go
@@ -1,10 +1,12 @@
 func mySqrt(x int) int {
-    z := 1 
-    for z*z <= x {
-        if z*z == x {
-            return z
+    l, r := 0, x
+    for l < r {
+        m := (l + r + 1)/2    
+        if m*m <= x {
+            l = m
+        } else {
+            r = m - 1
         }
-        z++
     }
-    return z - 1
+    return l
 }
