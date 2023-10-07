7cf4835ea3ff34f586c301e72841ae1b1aa99d98
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-10-06 23:06:21 -0600

    Update 278_first_bad_version.go
    * task type: find first match
    * m is l plus half distance between r and l
    * termination at l >= r
    * l pointer steps over m (cause its not a match)
    * r pointer moves on to m

--- go/easy/278_first_bad_version.go
+++ go/easy/278_first_bad_version.go
@@ -1,17 +1,15 @@
 package easy
 
-
 func firstBadVersion(n int) int {
-    // ggggggbbbbbbbbb 
     l := 1
     r := n
     for l < r {
-        m := (l + r)/2
+        m := l + (r - l)/2
         switch isBadVersion(m) {
-            case true:
-                r = m
-            case false:
-                l = m + 1
+        case true:
+            r = m
+        case false:
+            l = m + 1
         }
     }
     return l
