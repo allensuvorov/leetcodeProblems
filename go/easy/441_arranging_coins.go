a61e28e9611d4b4e62ab0770ed26c48ad321d2cd
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-10-07 22:12:53 -0600

    Update 441_arranging_coins.go
    * binary search is so fast

--- go/easy/441_arranging_coins.go
+++ go/easy/441_arranging_coins.go
@@ -1,11 +1,9 @@
-// (k * (k + 1)) / 2 <= n
-
-/
+// Solution 1 - math - (k * (k + 1)) / 2 <= n
 func arrangeCoins(n int) int {
     return int(math.Sqrt(2.0 * float64(n) + 0.25) - 0.5)
 }
-*/
 
+// Solution 2 - iteration
 func arrangeCoins(n int) int {
     sum := 0
     inc := 1
@@ -18,4 +16,24 @@ func arrangeCoins(n int) int {
         }
     }
     return i
+}
+
+// Solution 3 - Binary search
+func arrangeCoins(n int) int {
+    l := 1
+    r := n
+    for l < r {
+        m := l + (r - l + 1)/2
+        if countCoins(m) <= n {
+            l = m
+        } else {
+            r = m - 1
+        }
+    }
+    return l
+}
+
+func countCoins(rows int) (coins int) {
+    coins = (1 + rows) * rows / 2
+    return coins
 }
