26e3329228f9aa1bfc24a4a0478d57d9f9a2e64b
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-10-15 22:35:12 -0600

    Update 74_search_a_2d_matrix.go
    * convert number into coordinates

--- go/medium/74_search_a_2d_matrix.go
+++ go/medium/74_search_a_2d_matrix.go
@@ -1,23 +1,12 @@
 func searchMatrix(matrix [][]int, target int) bool {
-    // find the row (smallest that's with first element lower than target)
     l := 0
-    r := len(matrix) - 1
-    for l < r {
-        m := l + (r - l + 1)/2
-        if matrix[m][0] <= target {
-            l = m
-        } else {
-            r = m - 1
-        }
-    }
-    
-    // look within that row
-    row := l
-    l = 0
-    r = len(matrix[0]) - 1
+    r := len(matrix) * len(matrix[0]) - 1
     for l <= r {
         m := l + (r - l)/2
-        guess := matrix[row][m]
+        // convert number into coordinates
+        i := m / len(matrix[0])
+        j := m % len(matrix[0])
+        guess := matrix[i][j]
         switch {
         case guess == target:
             return true
@@ -28,4 +17,4 @@ func searchMatrix(matrix [][]int, target int) bool {
         }
     }
     return false
-}    
+}
