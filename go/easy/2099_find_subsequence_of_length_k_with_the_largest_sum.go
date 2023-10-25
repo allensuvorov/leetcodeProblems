7fa7b5f6a158b57dd2f8fcca3bb3914c4f846eb4
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-10-24 21:22:30 -0600

    Update 2099_find_subsequence_of_length_k_with_the_largest_sum.go
    * comments

--- go/easy/2099_find_subsequence_of_length_k_with_the_largest_sum.go
+++ go/easy/2099_find_subsequence_of_length_k_with_the_largest_sum.go
@@ -1,20 +1,21 @@
 func maxSubsequence(nums []int, k int) []int {
-    // solution 1 - O(n * log n)
-    // Sort, get frequency map
-    // Collect from source
+    // solution - sort
+    // time O(n * log n)
     size := len(nums)
+
+    // Sort
     sorted := make([]int, size)
     copy(sorted, nums)
     sort.Ints(sorted)
     
+    // Frequency map of top range
     fm := map[int]int{}
-    
     for i := size - k; i < size; i ++ {
         fm[sorted[i]]++
     }
 
+    // Collect from source
     res := make([]int, 0, k)
-
     for _, v := range nums {
         if fm[v] > 0 {
             res = append(res, v)
