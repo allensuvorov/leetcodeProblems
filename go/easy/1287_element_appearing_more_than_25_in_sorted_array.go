4f2f8a098229fe1422566a4f419e3e30188f2bdd
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-10-29 19:58:15 -0600

    Update 1287_element_appearing_more_than_25_in_sorted_array.go
    * time O(n)

--- go/easy/1287_element_appearing_more_than_25_in_sorted_array.go
+++ go/easy/1287_element_appearing_more_than_25_in_sorted_array.go
@@ -1,11 +1,10 @@
 func findSpecialInteger(arr []int) int {
-    threshold := len(arr)*25/100
-    fm := map[int]int{}
-    for _, v := range arr {
-        fm[v]++
-        if fm[v] > threshold {
-            return v
+    n := len(arr)
+    q := n/4
+    for i := 0; i < n - q; i ++ {
+        if arr[i] == arr[i+q] {
+            return arr[i]
         }
     }
-    return 0
+    return -1
 }
