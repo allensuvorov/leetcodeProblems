a1347839c0b155805077dc500042eb4b259db320
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-10-31 21:16:49 -0600

    Update 2264_largest_3_same_digit_number_in_string.go
    * strings compared alphanumerically

--- go/easy/2264_largest_3_same_digit_number_in_string.go
+++ go/easy/2264_largest_3_same_digit_number_in_string.go
@@ -1,12 +1,8 @@
 func largestGoodInteger(num string) string {
     var res string
-    var max byte
     for i := 0; i <= len(num) - 3; i ++ {
-        if num[i] == num[i+1] && num[i] == num[i+2] {
-            if max < num[i] {
-                max = num[i]
-                res = string([]byte{max, max, max})
-            }
+        if num[i] == num[i+1] && num[i] == num[i+2] && num[i:i+3] > res {
+            res = num[i:i+3]
         }
     }
     return res
