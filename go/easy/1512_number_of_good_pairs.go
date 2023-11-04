54eb6e349526484f2dbfef191d58c5d58ca78abf
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-11-03 23:05:17 -0600

    Update 1512_number_of_good_pairs.go
    * hash map
    * with capture

--- go/easy/1512_number_of_good_pairs.go
+++ go/easy/1512_number_of_good_pairs.go
@@ -1,12 +1,10 @@
 func numIdenticalPairs(nums []int) int {
-    // hash frequency map, then arith progression sum // n*(n-1)/2
+    // hash frequency map
     sum := 0
     fm := map[int]int{}
     for _, v := range nums {
+        sum += fm[v]
         fm[v]++
-    }
-    for _, v := range fm {
-        sum += v*(v-1)/2
     }
     return sum
 }
