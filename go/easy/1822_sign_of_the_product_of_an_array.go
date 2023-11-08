8870777f4e9445e754d7efeec08bafb6f883337e
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-11-07 20:15:46 -0600

    Update 1822_sign_of_the_product_of_an_array.go
    * sign = -sign

--- go/easy/1822_sign_of_the_product_of_an_array.go
+++ go/easy/1822_sign_of_the_product_of_an_array.go
@@ -1,15 +1,12 @@
 func arraySign(nums []int) int {
-    sign := false // positive
+    sign := 1
     for _, v := range nums {
-        if v < 0 {
-            sign = sign != true
-        }
         if v == 0 {
             return 0
         }
+        if v < 0 {
+            sign = -sign
+        }
     }
-    if sign {
-        return -1
-    }
-    return 1
+    return sign
 }
