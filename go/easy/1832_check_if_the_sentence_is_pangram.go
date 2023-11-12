2036bf52a1fa66d92a2345f249eafd60038fe184
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-11-11 18:58:00 -0600

    Update 1832_check_if_the_sentence_is_pangram.go
    * array

--- go/easy/1832_check_if_the_sentence_is_pangram.go
+++ go/easy/1832_check_if_the_sentence_is_pangram.go
@@ -1,7 +1,7 @@
 func checkIfPangram(sentence string) bool {
-    cl := make([]int, 26)
-    for i := range sentence {
-        cl[sentence[i]-'a'] = 1
+    cl := [26]int{}
+    for _, v := range sentence {
+        cl[v - 'a'] = 1
     }
     for _, v := range cl {
         if v == 0 {
