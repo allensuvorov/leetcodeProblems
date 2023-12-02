2b1ec6c514dca738f1d16ce641229e5016c4198e
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-12-01 20:37:19 -0600

    Update 2535_difference_between_element_sum_and_digit_sum_of_an_array.go
    * shorter version

--- go/easy/2535_difference_between_element_sum_and_digit_sum_of_an_array.go
+++ go/easy/2535_difference_between_element_sum_and_digit_sum_of_an_array.go
@@ -1,24 +1,11 @@
 func differenceOfSum(nums []int) int {
     elmSum, dgtSum := 0, 0
-
     for _, v := range nums {
         elmSum += v
-        dgtSum += getDgtSum(v)
+        for v > 0 {
+            dgtSum += v % 10
+            v /= 10
+        }
     }
-
-    dif := elmSum - dgtSum
-    if dif < 0 {
-        return - dif
-    }
-    return dif
-}
-
-func getDgtSum(n int) int {
-    dgtSum := 0
-    for n > 0 {
-        d := n % 10
-        dgtSum += d
-        n /= 10
-    }
-    return dgtSum
+    return elmSum - dgtSum
 }
