3151e3b4ccc4c9e499ce507b5b87602fa073a946
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-12-05 22:13:12 -0600

    Update 6_zigzag_conversion.go
    * shorter version

--- go/medium/6_zigzag_conversion.go
+++ go/medium/6_zigzag_conversion.go
@@ -2,17 +2,13 @@ func convert(s string, numRows int) string {
     if numRows == 1 {
         return s
     }
-    
     res := make([]byte, 0, len(s))
-    step := (numRows-1)*2
-    midStep := step
-    
     for r := 0; r < numRows; r++ {
-        midStep = step - r*2
-        for i := r; i < len(s); i += step {
+        inc := (numRows-1) * 2
+        for i := r; i < len(s); i += inc {
             res = append(res, s[i])
-            if r != 0 && r != numRows - 1 && i + midStep < len(s) {
-                res = append(res, s[i+midStep])
+            if r > 0 && r < numRows - 1 && i + inc - r * 2 < len(s) {
+                res = append(res, s[i + inc - r * 2])
             }
         }
     }    
