046b311afb2f4fadefc8214f2a8b0bda2a43fbe7
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-12-13 21:29:59 -0600

    Update 1475_final_prices_with_a_special_discount_in_a_shop.go
    * updating input slice

--- go/easy/1475_final_prices_with_a_special_discount_in_a_shop.go
+++ go/easy/1475_final_prices_with_a_special_discount_in_a_shop.go
@@ -1,23 +1,18 @@
-func finalPrices(prices []int) []int {
-    res := make([]int, len(prices))
-    var stack *node
-    for i := range prices {
-        for stack != nil && stack.Val >= prices[i] { // found discount
-            res[stack.Ind] = stack.Val - prices[i]
-            stack = stack.Next
-        }
-        newNode := node{prices[i], i, stack}
-        stack = &newNode
-    }
-    for stack != nil {
-        res[stack.Ind] = prices[stack.Ind]
-        stack = stack.Next
-    }
-    return res
-}
-
 type node struct{
     Val int
     Ind int
     Next *node
+}
+
+func finalPrices(prices []int) []int {
+    var stack *node
+    for i, discount := range prices {
+        for stack != nil && stack.Val >= discount { // found discount
+            prices[stack.Ind] -= discount
+            stack = stack.Next
+        }
+        newNode := node{discount, i, stack}
+        stack = &newNode
+    }
+    return prices
 }
