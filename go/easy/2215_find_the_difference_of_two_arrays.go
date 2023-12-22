f54ef6ef15d6980fb067d931c9753bf01282187a
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-12-21 20:13:16 -0600

    Create 2215_find_the_difference_of_two_arrays.go
    * hash map as hash set

---
+++ go/easy/2215_find_the_difference_of_two_arrays.go
@@ -0,0 +1,23 @@
+func findDifference(nums1, nums2 []int) [][]int {
+    set1 := make(map[int]bool)
+    set2 := make(map[int]bool)
+    for _, v := range nums1 {
+        set1[v] = true
+    }
+    for _, v := range nums2 {
+        set2[v] = true
+    }
+    diff1 := []int{}
+    diff2 := []int{}
+    for k := range set1 {
+        if !set2[k] {
+            diff1 = append(diff1, k)
+        }
+    }
+    for k := range set2 {
+        if !set1[k] {
+            diff2 = append(diff2, k)
+        }
+    }
+    return [][]int{diff1, diff2}
+}
