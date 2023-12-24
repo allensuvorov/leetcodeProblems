14bd98f8c470b9a271c340e1b925384281520b33
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-12-24 13:35:40 -0600

    Update 735_asteroid_collision.go
    * removed a redundant collision check

--- go/medium/735_asteroid_collision.go
+++ go/medium/735_asteroid_collision.go
@@ -3,7 +3,7 @@ func asteroidCollision(asteroids []int) []int {
     for _, r := range asteroids {
         top := len(stack) - 1
         // collision cases checks: both exist and both trying to collide
-        for r != 0 && len(stack) > 0 && r < 0 && stack[top] > 0 {
+        for len(stack) > 0 && r < 0 && stack[top] > 0 {
             switch {
             case stack[top] > -r: // right asteroid explodes
                 r = 0
@@ -15,8 +15,7 @@ func asteroidCollision(asteroids []int) []int {
             }
             top = len(stack) - 1
         }
-        // append, if right asteroid survives collisions
-        if r != 0 {
+        if r != 0 { // append, if right asteroid survives collisions
             stack = append(stack, r)
         }
         
