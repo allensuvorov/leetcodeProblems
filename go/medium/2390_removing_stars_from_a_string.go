1875e60d7fd1628da9bfb6a68ee9b9609add4ba2
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-12-23 20:38:30 -0600

    Update 2390_removing_stars_from_a_string.go
    * reversed loop
    * O(n)

--- go/medium/2390_removing_stars_from_a_string.go
+++ go/medium/2390_removing_stars_from_a_string.go
@@ -9,3 +9,25 @@ func removeStars(s string) string {
     }
     return string(stack)
 }
+
+func removeStars(s string) string {
+    // reverse solution
+    rev := make([]byte, 0, len(s))
+    count := 0
+    for i := len(s) - 1; i >= 0; i-- {
+        if s[i] == '*' {
+            count++
+        } else {
+            if count > 0 {
+                count --
+            } else {
+                rev = append(rev, s[i])
+            }
+        }
+    }
+    res := make([]byte, 0, len(rev))
+    for i := len(rev) - 1; i >= 0; i-- {
+        res = append(res, rev[i])
+    }
+    return string(res)
+}
