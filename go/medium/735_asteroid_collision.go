5ee34432fa441487828a72080ff56c39cb1840c4
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-12-24 13:21:21 -0600

    Create 735_asteroid_collision.go
    * Stack
    * O(n)

---
+++ go/medium/735_asteroid_collision.go
@@ -0,0 +1,25 @@
+func asteroidCollision(asteroids []int) []int {
+    stack := make([]int, 0, len(asteroids))
+    for _, r := range asteroids {
+        top := len(stack) - 1
+        // collision cases checks: both exist and both trying to collide
+        for r != 0 && len(stack) > 0 && r < 0 && stack[top] > 0 {
+            switch {
+            case stack[top] > -r: // right asteroid explodes
+                r = 0
+            case stack[top] < -r: // left asteroid explodes
+                stack = stack[:top] 
+            default:                       // both explode
+                r = 0
+                stack = stack[:top]
+            }
+            top = len(stack) - 1
+        }
+        // append, if right asteroid survives collisions
+        if r != 0 {
+            stack = append(stack, r)
+        }
+        
+    }
+    return stack
+}
