e405c5535a2a911537a064df3fa34020bac41956
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-12-27 17:16:42 -0600

    Update 394_decode_string.go
    * fixed bugs in buildPFE()
    * building getSum()

--- go/medium/394_decode_string.go
+++ go/medium/394_decode_string.go
@@ -4,8 +4,8 @@ func decodeString(s string) string {
     fmt.Printf("%s", postFixExpression)
 
     // Calculate the sum
-        // what is it? numbers or letters
-        return ""
+    res := getSum(postFixExpression)
+    return string(res[0])
 }
 
 
@@ -14,21 +14,27 @@ func buildPostFixExpression(s string) [][]byte{
     postFixExpression := [][]byte{}
     digits := []byte{}
     letters := []byte{}
+    popFromStackToPostFixExpression := func() {
+        postFixExpression = append(postFixExpression, []byte{stack[len(stack)-1]})
+        stack = stack[:len(stack)-1]
+    }
+    pushPlus := func() {  // "+" pushes all "+" and "*" from stack to post-fix-exp
+        for len(stack) > 0 && (stack[len(stack)-1] == '+' || stack[len(stack)-1] == '*') {
+            popFromStackToPostFixExpression()
+        }
+        stack = append(stack, '+')    
+    }
     for i, v := range s {
-        fmt.Printf("loop start: v = %v, postFixExpression = %s \n", string(v), postFixExpression)
+        // fmt.Printf("loop start: v = %v, postFixExpression = %s \n", string(v), postFixExpression)
         if unicode.IsDigit(v) {
             digits = append(digits, s[i])
             if len(letters) != 0 {
-                fmt.Printf("about to flush letter to pfe: %s \n", postFixExpression)
+                // fmt.Printf("about to flush letter to pfe: %s \n", postFixExpression)
                 data := make([]byte, len(letters))
                 copy(data, letters)
                 postFixExpression = append(postFixExpression, data)
                 letters = letters[:0]
-                for len(stack) > 0 && (stack[len(stack)-1] == '+' || stack[len(stack)-1] == '*') {
-                    postFixExpression = append(postFixExpression, []byte{stack[len(stack)-1]})
-                    stack = stack[:len(stack)-1]
-                }
-                stack = append(stack, '+')
+                pushPlus()
             }
         }
         if unicode.IsLetter(v) {
@@ -40,34 +46,48 @@ func buildPostFixExpression(s string) [][]byte{
             postFixExpression = append(postFixExpression, data)
             digits = digits[:0]
             for len(stack) > 0 && stack[len(stack)-1] == '*' {
-                postFixExpression = append(postFixExpression, []byte{'*'})
-                stack = stack[:len(stack)-1]
+                popFromStackToPostFixExpression()
             }
             stack = append(stack, '*')
             stack = append(stack, '[')
         }
         if v == ']'{
-            postFixExpression = append(postFixExpression, letters)
+            data := make([]byte, len(letters))
+            copy(data, letters)
+            postFixExpression = append(postFixExpression, data)
             letters = letters[:0]
             for stack[len(stack)-1] != '[' {
-                postFixExpression = append(postFixExpression, []byte{stack[len(stack)-1]})
-                stack = stack[:len(stack)-1]
+                popFromStackToPostFixExpression()
             }
             stack = stack[:len(stack)-1]
+
+            if i + 1 < len(s) && s[i + 1] != ']' {
+                pushPlus()
+            }
         }
-        fmt.Printf("loop end: v = %v, postFixExpression = %s \n", string(v), postFixExpression)
+        // fmt.Printf("loop end: v = %v, postFixExpression = %s \n", string(v), postFixExpression)
     }
     for len(stack) != 0 {
-        postFixExpression = append(postFixExpression, []byte{stack[len(stack)-1]})
-        stack = stack[:len(stack)-1]
+        popFromStackToPostFixExpression()
     }
     return postFixExpression
 }
 
+func getSum(pfe [][]byte) []byte {
+    for i, v := range pfe {
+        // what is it? numbers or letters
+        switch {
+        case unicode.IsDigit(v[0]):
+        case unicode.IsLetter(v[0]):
+        case v[0] == '+':
+        case v[0] == '*':
+        }
+    }
+}
 
-// 3 * [a + 2 * [c] ] = 3 * [a + cc]
+// in-fix: 3 * a + 2 * bc
+// post-fix: 3 a * 2 bc * +
 
+
+// in-fix: 3 * [a + 2 * [c] ] = 3 * [a + cc]
 // post-fix: 3a2c*+*
-// stack: 3a2c
-// res:a cc
-
