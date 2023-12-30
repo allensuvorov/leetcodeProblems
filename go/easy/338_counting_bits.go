0ba72759e76b2335c992140d035bb2a9180648dc
Allen S <56049635+allensuvorov@users.noreply.github.com>
2023-12-30 14:33:54 -0600

    Update 338_counting_bits.go
    * even or odd
    * all bitwise

--- go/easy/338_counting_bits.go
+++ go/easy/338_counting_bits.go
@@ -1,45 +1,11 @@
-my solution - has human logic
 func countBits(n int) []int {
-    // 0, 1,    2, 3,    4, 5, 6, 7,    8, 9, 10, 11, 12, 13, 14, 15,   16 
-    // 0, 1,    1, 2,    1, 2, 2, 3,    1, 2,  2,  3,  2,  3,  3,  4,   1
-
     ans := make([]int, n + 1)
-    maxPowOfTwoProduct := 0
     for i := 1; i <= n; i++ {        
-        // calculate first values: 0, 1, 1, 2
-        if i == 1 {
-            maxPowOfTwoProduct = 1
-            ans[i] = 1
-            continue
-        }
-        if i == 2 {
-            maxPowOfTwoProduct = 2
-            ans[i] = 1
-            continue
-        }
-        
-        // calculate following values:
-        if i == maxPowOfTwoProduct * 2 {
-            maxPowOfTwoProduct *= 2
-            ans[i] = 1
-        } else {
-            ans[i] = ans[maxPowOfTwoProduct] + ans[i - maxPowOfTwoProduct]
+        if i & 1 == 0 { // even
+            ans[i] = ans[i/2]
+        } else { // odd
+            ans[i] = ans[i-1] + 1
         }
     }
-
     return ans
-}
-
-// short, but not very intuitive 
-func countBits(n int) []int {
-     bitArr := make([]int, n+1)
-     bitArr[0] = 0
-	   for i := 1; i <= n; i++ {
-		     if i%2 == 0 {
-			       bitArr[i] = bitArr[i/2]
-		     } else {
-			       bitArr[i] = bitArr[i-1] + 1
-		     }
-	   }
-	   return bitArr
 }
