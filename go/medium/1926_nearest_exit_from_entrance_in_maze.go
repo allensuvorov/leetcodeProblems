4dd30e535111966be7bf6cbb5a49ba49412e997c
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-01-07 19:21:05 -0600

    Update 1926_nearest_exit_from_entrance_in_maze.go
    * 19 lines

--- go/medium/1926_nearest_exit_from_entrance_in_maze.go
+++ go/medium/1926_nearest_exit_from_entrance_in_maze.go
@@ -1,40 +1,19 @@
-func nearestExit(maze [][]byte, entrance []int) int {
-    dist := 0
-    entrance = append(entrance, dist)
-    q := [][]int{entrance}
+func nearestExit(maze [][]byte, entrance []int) int {  
     maze[entrance[0]][entrance[1]] = '1'
-    
-    for len(q) != 0 {
-        now := q[0]
+    q := [][]int{append(entrance, 0)}
+    for len(q) > 0 {
+        for _, edge := range [][]int{[]int{-1,0}, []int{1,0}, []int{0,1}, []int{0,-1}} {
+            nextRow := q[0][0] + edge[0]
+            nextCol := q[0][1] + edge[1]
+            if nextRow >= 0 && nextRow < len(maze) && nextCol >= 0 && nextCol < len(maze[0]) && maze[nextRow][nextCol] == '.' {
+                if nextRow == 0 || nextRow == len(maze) - 1 || nextCol == 0 || nextCol == len(maze[0]) - 1  {
+                    return q[0][2] + 1
+                }
+                maze[nextRow][nextCol] = '1'
+                q = append(q, []int{nextRow, nextCol, q[0][2] + 1})
+            }
+        }
         q = q[1:]
-        dist = now[2]
-        
-        checkVertex := func(m, n int) bool {
-            if m >= 0 && m < len(maze) && n >= 0 && n < len(maze[0]) {
-                if maze[m][n] == '.' {  // empty - mark dist and enq
-                    q = append(q, []int{m, n, dist + 1})
-                    maze[m][n] = '1'
-                }
-            } else if dist != 0 { // now is an exit
-                return true
-            }
-            return false
-        }
-
-        checkNearestVertices := func(now []int) bool {
-            edges := [][]int{[]int{-1,0}, []int{1,0}, []int{0,1}, []int{0,-1}}   
-            for _, v := range edges{
-                if exit := checkVertex(now[0] + v[0], now[1] + v[1]); exit { 
-                    return true 
-                }
-            }
-            return false
-        }
-        
-        if exit := checkNearestVertices(now); exit {
-            return int(dist)
-        }
     }
-    
     return -1
 }
