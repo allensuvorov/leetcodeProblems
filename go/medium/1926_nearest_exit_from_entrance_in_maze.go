d260fab9bf723d7cedded821f2a7973f3360d5e7
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-01-07 19:23:55 -0600

    Update 1926_nearest_exit_from_entrance_in_maze.go
    * nr - nextRow
    * nc - nextCol

--- go/medium/1926_nearest_exit_from_entrance_in_maze.go
+++ go/medium/1926_nearest_exit_from_entrance_in_maze.go
@@ -3,14 +3,14 @@ func nearestExit(maze [][]byte, entrance []int) int {
     q := [][]int{append(entrance, 0)}
     for len(q) > 0 {
         for _, edge := range [][]int{[]int{-1,0}, []int{1,0}, []int{0,1}, []int{0,-1}} {
-            nextRow := q[0][0] + edge[0]
-            nextCol := q[0][1] + edge[1]
-            if nextRow >= 0 && nextRow < len(maze) && nextCol >= 0 && nextCol < len(maze[0]) && maze[nextRow][nextCol] == '.' {
-                if nextRow == 0 || nextRow == len(maze) - 1 || nextCol == 0 || nextCol == len(maze[0]) - 1  {
+            nr := q[0][0] + edge[0]
+            nc := q[0][1] + edge[1]
+            if nr >= 0 && nr < len(maze) && nc >= 0 && nc < len(maze[0]) && maze[nr][nc] == '.' {
+                if nr == 0 || nr == len(maze) - 1 || nc == 0 || nc == len(maze[0]) - 1  {
                     return q[0][2] + 1
                 }
-                maze[nextRow][nextCol] = '1'
-                q = append(q, []int{nextRow, nextCol, q[0][2] + 1})
+                maze[nr][nc] = '1'
+                q = append(q, []int{nr, nc, q[0][2] + 1})
             }
         }
         q = q[1:]
