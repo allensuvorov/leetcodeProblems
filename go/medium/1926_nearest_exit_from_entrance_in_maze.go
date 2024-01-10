6dab3125dff325bb4dc3ee3ec0315aa8a0e83794
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-01-09 19:19:14 -0600

    Update 1926_nearest_exit_from_entrance_in_maze.go
    * slice literal refactored

--- go/medium/1926_nearest_exit_from_entrance_in_maze.go
+++ go/medium/1926_nearest_exit_from_entrance_in_maze.go
@@ -2,11 +2,14 @@ func nearestExit(maze [][]byte, entrance []int) int {
     maze[entrance[0]][entrance[1]] = '1'
     q := [][]int{append(entrance, 0)}
     for len(q) > 0 {
-        for _, edge := range [][]int{[]int{-1,0}, []int{1,0}, []int{0,1}, []int{0,-1}} {
+        for _, edge := range [][]int{{-1,0}, {1,0}, {0,1}, {0,-1}} {
             nr := q[0][0] + edge[0]
             nc := q[0][1] + edge[1]
-            if nr >= 0 && nr < len(maze) && nc >= 0 && nc < len(maze[0]) && maze[nr][nc] == '.' {
-                if nr == 0 || nr == len(maze) - 1 || nc == 0 || nc == len(maze[0]) - 1  {
+            if nr >= 0 && nr < len(maze) && 
+                nc >= 0 && nc < len(maze[0]) && 
+                maze[nr][nc] == '.' {
+                if nr == 0 || nr == len(maze) - 1 || 
+                    nc == 0 || nc == len(maze[0]) - 1  {
                     return q[0][2] + 1
                 }
                 maze[nr][nc] = '1'
