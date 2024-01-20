2645649ffd5df9d48d34e34405dce1c7d5ca4dd8
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-01-20 14:26:37 -0600

    Update 79_word_search.go
    * tracking path inside input data
    * tmp to recover input data

--- go/medium/79_word_search.go
+++ go/medium/79_word_search.go
@@ -1,25 +1,23 @@
 func exist(board [][]byte, word string) bool {
     rows, cols := len(board), len(board[0])
-    path := make([][]bool, rows)
-    for i := range path {
-        path[i] = make([]bool, cols)
-    }
     
-    var dfs func(r, c, i int) bool
+    var dfs func(int, int, int) bool
     dfs = func(r, c, i int) bool {
         if i == len(word) {
             return true
         }
-        if r < 0 || r == rows || c < 0 || c == cols || path[r][c] || board[r][c] != word[i] {
+        if r < 0 || r == rows || c < 0 || c == cols || board[r][c] != word[i]{
             return false
         }
-        path[r][c] = true
+
+        tmp := board[r][c]
+        board[r][c] = '1'
         res := 
             dfs(r + 1, c, i + 1) ||
             dfs(r - 1, c, i + 1) ||
             dfs(r, c + 1, i + 1) ||
             dfs(r, c - 1, i + 1)
-        path[r][c] = false
+        board[r][c] = tmp
         return res
     }
 
