22500ddd614f351563c8c415b2e7b738207b20f3
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-01-20 13:59:55 -0600

    Update 79_word_search.go
    * neetCode's solution
    * really nice, but still slow - 120 ms

--- go/medium/79_word_search.go
+++ go/medium/79_word_search.go
@@ -1,38 +1,34 @@
 func exist(board [][]byte, word string) bool {
-    for i := range board {
-        for j := range board[i] {
-            if board[i][j] == word[0] {
-                visited := make([][]bool, len(board))
-                for i := range visited {
-                    visited[i] = make([]bool, len(board[0]))
-                }
+    rows, cols := len(board), len(board[0])
+    path := make([][]bool, rows)
+    for i := range path {
+        path[i] = make([]bool, cols)
+    }
+    
+    var dfs func(r, c, i int) bool
+    dfs = func(r, c, i int) bool {
+        if i == len(word) {
+            return true
+        }
+        if r < 0 || r == rows || c < 0 || c == cols || path[r][c] || board[r][c] != word[i] {
+            return false
+        }
+        path[r][c] = true
+        res := 
+            dfs(r + 1, c, i + 1) ||
+            dfs(r - 1, c, i + 1) ||
+            dfs(r, c + 1, i + 1) ||
+            dfs(r, c - 1, i + 1)
+        path[r][c] = false
+        return res
+    }
 
-                if dfs(board, word, visited, []int{i, j}, 1) {
-                    return true
-                }
+    for r := range board {
+        for c := range board[r] {
+            if dfs(r, c, 0) {
+                return true
             }
         }
     }
-    return false
-}
-
-func dfs(board [][]byte, word string, visited [][]bool, now []int, i int) bool {
-    if i == len(word) {
-        return true
-    }
-    visited[now[0]][now[1]] = true
-
-    for _, direction := range [][]int{{1,0}, {-1,0}, {0, 1}, {0, -1}} {
-        nr := now[0] + direction[0]
-        nc := now[1] + direction[1]
-        if nr >= 0 && nr < len(board) && nc >= 0 && nc < len(board[0]) && 
-            !visited[nr][nc] &&
-            board[nr][nc] == word[i] {
-                if dfs(board, word, visited, []int{nr, nc}, i + 1) {
-                    return true
-                }
-        }
-    }
-    visited[now[0]][now[1]] = false
     return false
 }
