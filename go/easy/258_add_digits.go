4428e55781d942962f563d5711c42d3dffad999a
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-01-22 17:49:17 -0600

    Update and rename 258AddDigits.go to 258_add_digits.go
    * added iteration solution

---
+++ go/easy/258_add_digits.go
@@ -0,0 +1,26 @@
+// fancy digital root solution
+func addDigits(num int) int {
+	if num == 0 {
+		return 0
+	}
+
+	if num%9 == 0 {
+		return 9
+	}
+
+	return num % 9
+}
+
+// iteration solution
+func addDigits(num int) int {
+    for num / 10 > 0 {
+        sum := 0
+        for num > 0 {
+            dig := num % 10
+            sum += dig
+            num /= 10
+        }
+        num = sum
+    }
+    return num
+}
