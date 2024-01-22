342e9b7e4f617bf11270713cc33204683370ecb5
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-01-22 17:31:15 -0600

    Update 509_fibonacci_number.go
    * nice and short

--- go/easy/509_fibonacci_number.go
+++ go/easy/509_fibonacci_number.go
@@ -1,14 +1,7 @@
-package easy
-
 func fib(n int) int {
-    // loop
     a, b := 0, 1
     for i := 1; i <= n; i++ {
         a, b = b, a+b
     }
     return a
 }
-
-// other solutions: 
-// * closure
-// * recursion
