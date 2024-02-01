e8061adcdd6ea42d8ee5556eea1b86998805f861
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-01-31 20:13:05 -0600

    Update 912_sort_an_array.go
    * quick sort

--- go/medium/912_sort_an_array.go
+++ go/medium/912_sort_an_array.go
@@ -1,13 +1,30 @@
 func sortArray(nums []int) []int {
-    // insertion sort
-    for i := 1; i < len(nums); i++ {
-        cur := nums[i]
-        j := i
-        for j > 0 && cur < nums[j - 1] {
-            j--
-            nums[j + 1] = nums[j]
-        }
-        nums[j] = cur
-    }
+    quickSort(nums, 0, len(nums) - 1)
     return nums
+}
+
+func quickSort(nums []int, l, r int) {
+    if l < r {
+        p := partition(nums, l, r)
+        quickSort(nums, l, p)
+        quickSort(nums, p + 1, r)
+    }
+}
+
+func partition(nums []int, l, r int) int {
+    pivot := nums[(l+r)/2]
+    for l <= r {
+        for nums[l] < pivot {
+            l++
+        }
+        for nums[r] > pivot {
+            r--
+        }
+        if l <= r {
+            nums[l], nums[r] = nums[r], nums[l]
+            l++
+            r--
+        }
+    }
+    return l - 1
 }
