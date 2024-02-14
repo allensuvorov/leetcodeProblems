41ad10a900ccecad4066e68a8e8c437b2589a8e4
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-02-13 19:30:57 -0600

    Update 509_fibonacci_number.go
    * for loop range over integer

--- go/easy/509_fibonacci_number.go
+++ go/easy/509_fibonacci_number.go
@@ -1,3 +1,4 @@
+// GO 1.21
 func fib(n int) int {
     a, b := 0, 1
     for i := 1; i <= n; i++ {
@@ -5,3 +6,13 @@ func fib(n int) int {
     }
     return a
 }
+
+// GO 1.22
+func fib(n int) int {
+	a, b := 0, 1
+	for _ = range n {
+		a, b = b, a+b
+	}
+	return a
+}
+
