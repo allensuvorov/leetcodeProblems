cd7938013dd9ae87a0fa253b65d43e9354deff98
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-02-13 20:20:18 -0600

    Update 733_flood_fill.go
    * DFS - stack

--- go/easy/733_flood_fill.go
+++ go/easy/733_flood_fill.go
@@ -2,27 +2,31 @@ func floodFill(image [][]int, sr int, sc int, color int) [][]int {
 	rows, cols := len(image), len(image[0])
 	curColor := image[sr][sc]
 	newColor := color
-	if curColor == newColor {
-		return image
+    if curColor == newColor {
+	    return image
 	}
-	var dfs func(r, c int)
-	dfs = func(r, c int) {
-		if image[r][c] == curColor {
+    stack := [][]int{{sr, sc}}
+    const maxStackLenth = 100
+    for len(stack) > 0 {
+        r := stack[len(stack) - 1][0]
+        c := stack[len(stack) - 1][1]
+        stack = stack[:len(stack) - 1]
+
+        if image[r][c] == curColor {
 			image[r][c] = newColor
 			if r > 0 {
-				dfs(r-1, c)
+				stack = append(stack, []int{r-1, c})
 			}
 			if r < rows-1 {
-				dfs(r+1, c)
+				stack = append(stack, []int{r+1, c})
 			}
 			if c > 0 {
-				dfs(r, c-1)
+				stack = append(stack, []int{r, c-1})
 			}
 			if c < cols-1 {
-				dfs(r, c+1)
+				stack = append(stack, []int{r, c+1})
 			}
 		}
-	}
-	dfs(sr, sc)
+    }
 	return image
 }
