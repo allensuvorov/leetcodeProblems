cdf06a15ee585ab1e0965665938aaa3c650e6c0a
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-02-13 20:36:29 -0600

    Update 733_flood_fill.go
    * refactored

--- go/easy/733_flood_fill.go
+++ go/easy/733_flood_fill.go
@@ -1,19 +1,16 @@
 func floodFill(image [][]int, sr int, sc int, color int) [][]int {
 	rows, cols := len(image), len(image[0])
-	curColor := image[sr][sc]
-	newColor := color
-    if curColor == newColor {
+    canvas := image[sr][sc]
+    if canvas == color {
 	    return image
 	}
     stack := [][]int{{sr, sc}}
-    const maxStackLenth = 100
     for len(stack) > 0 {
         r := stack[len(stack) - 1][0]
         c := stack[len(stack) - 1][1]
         stack = stack[:len(stack) - 1]
-
-        if image[r][c] == curColor {
-			image[r][c] = newColor
+        if image[r][c] == canvas {
+			image[r][c] = color
 			if r > 0 {
 				stack = append(stack, []int{r-1, c})
 			}
