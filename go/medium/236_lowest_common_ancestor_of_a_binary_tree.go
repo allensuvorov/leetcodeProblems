0fd25f03e21879e56c008dc97ddb400d366ee241
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-02-17 19:09:44 -0600

    Update 236_lowest_common_ancestor_of_a_binary_tree.go
    * DFS

--- go/medium/236_lowest_common_ancestor_of_a_binary_tree.go
+++ go/medium/236_lowest_common_ancestor_of_a_binary_tree.go
@@ -1,1 +1,25 @@
-https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+    if root == nil {
+        return nil
+    }    
+    if root == p || root == q {
+        return root
+    }
+    l := lowestCommonAncestor(root.Left, p, q)
+    r := lowestCommonAncestor(root.Right, p, q)
+    if l != nil && r != nil {
+        return root
+    }
+    if l != nil {
+        return l
+    }
+    return r
+}
