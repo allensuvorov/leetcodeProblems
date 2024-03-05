066b73c2486634e37700b6da115c3bd20416a6a2
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-03-04 22:44:22 -0800

    Update 2810_faulty_keyboard.go
    * handle multiple 'i' in a row

--- go/easy/2810_faulty_keyboard.go
+++ go/easy/2810_faulty_keyboard.go
@@ -1,11 +1,19 @@
 func finalString(s string) string {
     ans := make([]byte, 0, len(s))
+    needToReverse := -1
     for i := range s {
         if s[i] == 'i' {
-            reverse(ans)
+            needToReverse *= -1
         } else {
+            if needToReverse == 1 {
+                reverse(ans)
+                needToReverse = -1    
+            }
             ans = append(ans, s[i])
         }
+    }
+    if needToReverse == 1 {
+        reverse(ans)
     }
     return string(ans)
 }
