79cd222eea87781056a526d6636abdca6f3b3e6a
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-03-06 20:27:29 -0800

    Update 1323_maximum_69_number.go
    * stack

--- go/easy/1323_maximum_69_number.go
+++ go/easy/1323_maximum_69_number.go
@@ -1,18 +1,22 @@
 func maximum69Number (num int) int {
-    rev := 0
+    stack := []int{}
     res := 0
     for tmp := num; tmp > 0; tmp /= 10 {
-        rev = rev * 10 + tmp % 10
+        stack = append(stack, tmp % 10)
+    }
+    
+    for i := len(stack) - 1; i >= 0; i-- {
+        if stack[i] == 6 {
+            stack[i] = 9
+            break
+        }
     }
 
-    firstSixFound := false
-    for tmp := rev; tmp > 0; tmp /= 10 {
-        d := tmp % 10
-        if d == 6 && !firstSixFound {
-            d = 9
-            firstSixFound = true
-        }
-        res = res * 10 + d
+    for len(stack) > 0 {
+        top := len(stack) - 1
+        res = res*10 + stack[top]
+        stack = stack[:top]   
     }
+
     return res
 }
