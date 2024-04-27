f477234cc94ea1f9c384823f77971ef7b0c4e52f
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-04-26 23:04:57 -0700

    Update 2566_maximum_difference_by_remapping_a_digit.go
    * removed digits reversing

--- go/easy/2566_maximum_difference_by_remapping_a_digit.go
+++ go/easy/2566_maximum_difference_by_remapping_a_digit.go
@@ -1,21 +1,21 @@
 func minMaxDifference(num int) int {
-    stack := []int{}
+    digits := []int{}
     for x := num; x > 0; x /= 10 {
-        d := x%10
-        stack = append(stack, d)
+        digits = append([]int{x%10}, digits...)
     }
+
     target := -1
-    for i := len(stack) - 1; i >= 0; i-- {
-        if stack[i] != 9 {
-            target = stack[i]
+    for i := range digits {
+        if digits[i] != 9 {
+            target = digits[i]
             break
         }
     }
     max := num
     if target != -1 {
         max = 0
-        for i := len(stack) - 1; i >= 0; i-- {
-            d := stack[i]
+        for i := range digits {
+            d := digits[i]
             if d == target {
                 d = 9
             }
@@ -23,14 +23,13 @@ func minMaxDifference(num int) int {
         }
     }
     min := 0
-    target = stack[len(stack) - 1]
-    for i := len(stack) - 2; i >= 0; i-- {
-        d := stack[i]
+    target = digits[0]
+    for i := 1; i < len(digits); i++ {
+        d := digits[i]
         if d == target {
             d = 0
         }
         min = min*10 + d
     }
-    fmt.Println(stack, max, min)
     return max - min
 }
