df7064948ccfe69cd47512bf08d32620d2ecef06
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-05-02 22:18:01 -0700

    Update 2540_minimum_common_value.go
    * two ifs

--- go/easy/2540_minimum_common_value.go
+++ go/easy/2540_minimum_common_value.go
@@ -1,13 +1,13 @@
 func getCommon(nums1 []int, nums2 []int) int {
     i1, i2 := 0, 0
     for i1 < len(nums1) && i2 < len(nums2) {
-        switch {
-        case nums1[i1] < nums2[i2]:
-            i1++
-        case nums1[i1] > nums2[i2]:
-            i2++
-        default:
+        if nums1[i1] == nums2[i2] {
             return nums1[i1]
+        }
+        if nums1[i1] > nums2[i2] {
+            i2++
+        } else {
+            i1++
         }
     }
     return -1
