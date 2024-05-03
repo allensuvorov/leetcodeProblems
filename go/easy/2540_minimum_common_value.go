2c218f9a32529ab2ed144ad53747d453aacdedef
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-05-02 22:15:59 -0700

    Create 2540_minimum_common_value.go
    * switch case

---
+++ go/easy/2540_minimum_common_value.go
@@ -0,0 +1,14 @@
+func getCommon(nums1 []int, nums2 []int) int {
+    i1, i2 := 0, 0
+    for i1 < len(nums1) && i2 < len(nums2) {
+        switch {
+        case nums1[i1] < nums2[i2]:
+            i1++
+        case nums1[i1] > nums2[i2]:
+            i2++
+        default:
+            return nums1[i1]
+        }
+    }
+    return -1
+}
