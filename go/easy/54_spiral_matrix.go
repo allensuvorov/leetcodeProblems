2b673edafd2067ad433266f32822d9523fba7d4c
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-05-06 19:28:12 -0700

    Update 54_spiral_matrix.go
    * KISS - very simple
    * reduce the area by each array
    * have a short simple loop for each direction

--- go/easy/54_spiral_matrix.go
+++ go/easy/54_spiral_matrix.go
@@ -1,33 +1,26 @@
 func spiralOrder(matrix [][]int) []int {
-    const visited int = 101
     rows, cols := len(matrix), len(matrix[0])
+    t, b, l, r := 0, rows - 1, 0, cols - 1
     size := rows*cols
-    ans := make([]int, 0, rows * cols)
-    dirs := [][]int{{0,1}, {1,0}, {0,-1}, {-1,0}}
-    nowR, nowC := 0, 0
-    ans = append(ans, matrix[nowR][nowC])
-    matrix[nowR][nowC] = visited
-    dir := dirs[0]
+    ans := make([]int, 0, size)
+
     for len(ans) < size { 
-        nextR, nextC := nowR + dir[0], nowC + dir[1]; 
-        for nextR >= 0 && nextR < rows && nextC >= 0 && nextC < cols && matrix[nextR][nextC] != visited {
-            ans = append(ans, matrix[nextR][nextC])
-            nowR, nowC = nextR, nextC
-            matrix[nowR][nowC] = visited
-            nextR += dir[0]
-            nextC += dir[1]
+        for i := l; i <= r && len(ans) < size; i++ {
+            ans = append(ans, matrix[t][i])
         }
-        dir = getNextDir(dir)
+        t++
+        for i := t; i <= b && len(ans) < size; i++ {
+            ans = append(ans, matrix[i][r])
+        }
+        r--
+        for i := r; i >= l && len(ans) < size; i-- {
+            ans = append(ans, matrix[b][i])
+        }
+        b--
+        for i := b; i >= t && len(ans) < size; i-- {
+            ans = append(ans, matrix[i][l])
+        }
+        l++
     }
     return ans
-}
-
-func getNextDir (curDir []int) []int {
-    switch {
-    case curDir[0] == 0 && curDir[1] == 1: return []int{1,0}
-    case curDir[0] == 1 && curDir[1] == 0: return []int{0,-1}
-    case curDir[0] == 0 && curDir[1] == -1: return []int{-1,0}
-    case curDir[0] == -1 && curDir[1] == 0: return []int{0,1}
-    }
-    return nil
 }
