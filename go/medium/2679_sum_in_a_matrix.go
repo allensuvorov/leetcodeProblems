85806742e87b656bbf8a81563e2544441adc894f
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-05-10 00:00:33 -0700

    Update 2679_sum_in_a_matrix.go
    * sort.Ints

--- go/medium/2679_sum_in_a_matrix.go
+++ go/medium/2679_sum_in_a_matrix.go
@@ -1,1 +1,14 @@
-2679_sum_in_a_matrix.go
+func matrixSum(nums [][]int) int {
+    for i := range nums {
+        sort.Ints(nums[i])
+    }
+    score := 0
+    for j := len(nums[0]) - 1; j >= 0; j -- {
+        maxCol := 0
+        for i := range nums {
+            maxCol = max(nums[i][j], maxCol)
+        }
+        score += maxCol
+    }
+    return score
+}
