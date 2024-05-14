b1c8b6f4c0f71d15b24ca8916dc831a2207a6ffc
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-05-13 21:19:14 -0700

    Update 102_binary_tree_level_order_traversal.go
    * implemented BFS with a queue

--- go/medium/102_binary_tree_level_order_traversal.go
+++ go/medium/102_binary_tree_level_order_traversal.go
@@ -1,2 +1,32 @@
-// BFS
-// with queue
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func levelOrder(root *TreeNode) [][]int {
+    q := []*TreeNode{}
+    if root != nil {
+        q = append(q, root)
+    }
+    ans := [][]int{}
+    for len(q) != 0 {
+        rowLen := len(q)
+        row := []int{}
+        for range rowLen {
+            now := q[0]
+            q = q[1:]
+            row = append(row, now.Val)
+            if now.Left != nil {
+                q = append(q, now.Left) 
+            }
+            if now.Right != nil { 
+                q = append(q, now.Right) 
+            }
+        }
+        ans = append(ans, row)
+    }
+    return ans
+}
