3adab7d79c82c681fbc0c41f8d481fd48e1f9cc7
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-05-15 22:39:16 -0700

    Update 101_symmetric_tree.go
    * DFS 
    * two pointers traversing in mirrored order

--- go/easy/101_symmetric_tree.go
+++ go/easy/101_symmetric_tree.go
@@ -1,1 +1,22 @@
-// two pointers traversing in mirrored order
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+    return dfs(root.Left, root.Right)
+}
+
+func dfs(root1, root2 *TreeNode) bool {
+    if root1 == nil && root2 == nil {
+        return true
+    }
+    if root1 == nil || root2 == nil || root1.Val != root2.Val {
+        return false
+    }
+    dfs(root1.Left, root2.Right)
+    return dfs(root1.Left, root2.Right) && dfs(root1.Right, root2.Left)
+}
