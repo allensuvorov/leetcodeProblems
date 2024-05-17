fedd8b158c11e3e47c217225196ffdb05cad98fa
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-05-16 23:09:21 -0700

    Update 1325_delete_leaves_with_a_given_value.go
    * one function

--- go/medium/1325_delete_leaves_with_a_given_value.go
+++ go/medium/1325_delete_leaves_with_a_given_value.go
@@ -6,16 +6,10 @@
  *     Right *TreeNode
  * }
  */
-func removeLeafNodes(root *TreeNode, target int) *TreeNode { 
-    
-    var DFS func(root *TreeNode) bool
-    DFS = func(root *TreeNode) bool {
-        if root == nil { return false }
-        if DFS(root.Left) { root.Left = nil }
-        if DFS(root.Right) { root.Right = nil }
-        if root.Left == nil && root.Right == nil && root.Val == target { return true }
-        return false
-    }
-    if DFS(root) { return nil }
+func removeLeafNodes(root *TreeNode, target int) *TreeNode {         
+    if root == nil { return root }
+    if removeLeafNodes(root.Left, target) == nil { root.Left = nil }
+    if removeLeafNodes(root.Right, target) == nil { root.Right = nil }
+    if root.Left == nil && root.Right == nil && root.Val == target { return nil }
     return root
 }
