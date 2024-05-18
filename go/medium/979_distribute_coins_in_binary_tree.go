987353dcf6a6ea54f16c74d64b49361edf335af0
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-05-17 20:57:13 -0700

    Create 979_distribute_coins_in_binary_tree.go
    * solution logic O(n^2)

---
+++ go/medium/979_distribute_coins_in_binary_tree.go
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func distributeCoins(root *TreeNode) int {
+    // target val = 0
+    // source val > 1
+    // look for a target with DFS
+    // starting from the target,
+    // lool for a source with BFS
+    // counting distance
+    // update values of target and source
+}
+
+// corner cases:
+// 1 node
+// only one side children
