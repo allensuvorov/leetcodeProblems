0a8e426629912bebaaee50ab1ab45de6f8b20127
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-05-19 23:20:39 -0700

    Update 111_minimum_depth_of_binary_tree.go
    * DFS solution

--- go/easy/111_minimum_depth_of_binary_tree.go
+++ go/easy/111_minimum_depth_of_binary_tree.go
@@ -1,1 +1,34 @@
+// DFS solution
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func minDepth(root *TreeNode) int {
+    md := 100000
+    depth := 0
+    if root == nil {
+        return 0
+    }
+    var DFS func (root *TreeNode)
+    DFS = func(root *TreeNode) {
+        if root == nil {
+            return
+        }
+        depth++
+        if root.Left == nil && root.Right == nil {
+            md = min(md, depth)
+        }
+        DFS(root.Left)
+        DFS(root.Right)
+        depth--
+        return
+    }
+    DFS(root)
+    return md
+}
+
 // BFS, counting distance till first leaf
