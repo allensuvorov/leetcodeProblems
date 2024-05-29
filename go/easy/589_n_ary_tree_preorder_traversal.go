68614f3ebf1f5c26b0dffdbbb1b5aa6feb9a449b
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-05-28 21:46:12 -0700

    Create 589_n_ary_tree_preorder_traversal.go
    * DFS
    * one function

---
+++ go/easy/589_n_ary_tree_preorder_traversal.go
@@ -0,0 +1,18 @@
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+func preorder(root *Node) []int {
+    if root == nil {
+        return nil
+    }
+    res := []int{root.Val}
+    for _, child := range root.Children {
+        res = append(res, preorder(child)...)
+    }
+    return res
+}
