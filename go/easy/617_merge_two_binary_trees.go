a035bef04d798ba526344ef5828c26ea15fe1321
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-05-29 21:44:57 -0700

    Update 617_merge_two_binary_trees.go
    * optimised

--- go/easy/617_merge_two_binary_trees.go
+++ go/easy/617_merge_two_binary_trees.go
@@ -7,23 +7,21 @@
  * }
  */
 func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
-    if root1 == nil && root2 == nil {
-        return nil
-    }
+	if root1 == nil && root2 == nil {
+		return nil
+	}
 
-    node := new(TreeNode)
+	if root2 == nil {
+		return root1
+	}
 
-    if root2 == nil {
-        return root1
-    }
+	if root1 == nil {
+		return root2
+	}
 
-    if root1 == nil {
-        return root2
-    }
-
-    node.Val = root1.Val + root2.Val
-    node.Left = mergeTrees(root1.Left, root2.Left)
-    node.Right = mergeTrees(root1.Right, root2.Right)
-
-    return node
+    return &TreeNode{
+		Val:   root1.Val + root2.Val,
+		Left:  mergeTrees(root1.Left, root2.Left),
+		Right: mergeTrees(root1.Right, root2.Right),
+	}
 }
