ab8587a4b74c410da71460477bb05a6512d8dcfe
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-06-04 23:58:26 -0700

    Create 868_binary_gap.go
    * work in progress

---
+++ go/easy/868_binary_gap.go
@@ -0,0 +1,24 @@
+func binaryGap(n int) int {
+    maxGap := 0
+    curGap := 0
+    inGap := false
+    for n > 0 {
+        d := n & 1
+        if inGap {
+            if d == 0 {
+                curGap++
+            }
+            if d == 1 {
+                maxGap = max(maxGap, curGap)
+                curGap = 0
+            }
+        } else {
+            if d == 1 {
+                inGap = true
+            }
+        }
+        n >>= 1
+    }
+
+    return maxGap
+}
