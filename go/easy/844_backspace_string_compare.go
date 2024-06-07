2d4fddbc1f4a4e869d0dc08e95e2ee8e1ec1b02f
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-06-07 00:02:03 -0700

    Update 844_backspace_string_compare.go
    * TODO fix bug

--- go/easy/844_backspace_string_compare.go
+++ go/easy/844_backspace_string_compare.go
@@ -1,1 +1,43 @@
-in progress
+func backspaceCompare(s string, t string) bool {
+    i := len(s) - 1
+    j := len(t) - 1
+    sBackspaceCount := 0
+    tBackspaceCount := 0
+    for i >= 0 && j >= 0 {
+        if s[i] == '#' {
+            sBackspaceCount++
+            i--
+            continue
+        } else {
+            if sBackspaceCount > 0 {
+                i--
+                sBackspaceCount--
+                continue
+            }
+        }
+        if t[j] == '#' {
+            tBackspaceCount++
+            j--
+            continue
+        } else {
+            if tBackspaceCount > 0 {
+                j--
+                tBackspaceCount--
+                continue
+            }
+        }
+        if i >= 0 && j >= 0 {
+            if s[i] != t[j] {
+                return false
+            }
+        }
+        i--
+        j--
+    }
+
+    if i < 0 && j < 0 {
+        return true
+    }
+    fmt.Println("i < 0 || j < 0", i, j)
+    return false
+}
