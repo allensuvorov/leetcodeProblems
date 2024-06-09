172808a58cdfdd44da88e6c2ed62c4b9de9142db
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-06-08 23:27:25 -0700

    Update 844_backspace_string_compare.go
    * t - O(n)
    * s - O(1)

--- go/easy/844_backspace_string_compare.go
+++ go/easy/844_backspace_string_compare.go
@@ -3,36 +3,47 @@ func backspaceCompare(s string, t string) bool {
     j := len(t) - 1
     sBackspaceCount := 0
     tBackspaceCount := 0
-    for i >= 0 && j >= 0 {
-        if s[i] == '#' {
-            sBackspaceCount++
-            i--
-            continue
-        } else {
-            if sBackspaceCount > 0 {
+    for i >= 0 || j >= 0 {
+        if i >= 0 {
+            if s[i] == '#' {
+                sBackspaceCount++
                 i--
-                sBackspaceCount--
                 continue
+            } else {
+                if sBackspaceCount > 0 {
+                    i--
+                    sBackspaceCount--
+                    continue
+                }
             }
         }
-        if t[j] == '#' {
-            tBackspaceCount++
-            j--
-            continue
-        } else {
-            if tBackspaceCount > 0 {
+        if j >= 0 {
+            if t[j] == '#' {
+                tBackspaceCount++
                 j--
-                tBackspaceCount--
                 continue
+            } else {
+                if tBackspaceCount > 0 {
+                    j--
+                    tBackspaceCount--
+                    continue
+                }
             }
         }
+
+        if i < 0 && j < 0 {
+            return true
+        }
+
         if i >= 0 && j >= 0 {
             if s[i] != t[j] {
                 return false
             }
+            i--
+            j--
+            continue
         }
-        i--
-        j--
+        return false
     }
 
     if i < 0 && j < 0 {
