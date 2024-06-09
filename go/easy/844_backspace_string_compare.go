4ada13158db2033c790c0be21855710c394d49a3
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-06-09 16:03:10 -0700

    Update 844_backspace_string_compare.go
    * 2 loops for finding letters

--- go/easy/844_backspace_string_compare.go
+++ go/easy/844_backspace_string_compare.go
@@ -1,54 +1,40 @@
 func backspaceCompare(s string, t string) bool {
     i := len(s) - 1
     j := len(t) - 1
-    sBackspaceCount := 0
-    tBackspaceCount := 0
+    skipS := 0
+    skipT := 0
     for i >= 0 || j >= 0 {
-        if i >= 0 {
+        for i >= 0 {
             if s[i] == '#' {
-                sBackspaceCount++
-                i--
-                continue
+                skipS++
+            } else if skipS > 0 {
+                skipS--
             } else {
-                if sBackspaceCount > 0 {
-                    i--
-                    sBackspaceCount--
-                    continue
-                }
-            }
-        }
-        if j >= 0 {
-            if t[j] == '#' {
-                tBackspaceCount++
-                j--
-                continue
-            } else {
-                if tBackspaceCount > 0 {
-                    j--
-                    tBackspaceCount--
-                    continue
-                }
-            }
-        }
-
-        if i < 0 && j < 0 {
-            return true
-        }
-
-        if i >= 0 && j >= 0 {
-            if s[i] != t[j] {
-                return false
+                break
             }
             i--
-            j--
-            continue
         }
-        return false
-    }
+        for j >= 0 {
+            if t[j] == '#' {
+                skipT++
+            } else if skipT > 0 {
+                skipT--
+            } else {
+                break
+            }
+            j--
+        }
 
-    if i < 0 && j < 0 {
-        return true
+        if i >= 0 && j >= 0 && s[i] != t[j] {
+            return false
+        }
+        
+        if (i>=0) != (j>=0) {
+            return false
+        }
+        i--
+        j--
+
     }
-    fmt.Println("i < 0 || j < 0", i, j)
-    return false
+    return true
 }
