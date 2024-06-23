06ee058152768c0e6a6538c7e18dde141d87c30d
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-06-22 20:51:53 -0700

    Update 942_di_string_match.go
    * at the end l and r are the same
    * just append any of them

--- go/easy/942_di_string_match.go
+++ go/easy/942_di_string_match.go
@@ -1,25 +1,14 @@
 func diStringMatch(s string) []int {
-    ans := make([]int, len(s) + 1)
-    l, r := 0, len(ans) - 1
-    for i := range s {
-        if s[i] == 'I' {
-            ans[i] = l
+    ans := make([]int, 0, len(s) + 1)
+    l, r := 0, cap(ans) - 1
+    for _, v := range s {
+        if v == 'I' {
+            ans = append(ans, l)
             l++
         } else {
-            ans[i] = r
+            ans = append(ans, r)
             r--
         }
-        if i == len(s) - 1 {
-            if s[i] == 'I' {
-                ans[i+1] = r
-            } else {
-                ans[i+1] = l
-            }
-        }
     }
-    return ans
+    return append(ans, r)
 }
-
-// 0, 1, 2, 3, 4
-// l           
-//             r
