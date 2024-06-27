27d1243a4c2af368c95dc7f8467cb604d406e37d
Allen S <56049635+allensuvorov@users.noreply.github.com>
2024-06-27 13:53:34 -0700

    Update 1791_find_center_of_star_graph.go
    * need to check just first two nodes

--- go/easy/1791_find_center_of_star_graph.go
+++ go/easy/1791_find_center_of_star_graph.go
@@ -1,12 +1,6 @@
 func findCenter(edges [][]int) int {
-    m := map[int]int{}
-    for i := range edges {
-        for j := range edges[i]{
-            m[edges[i][j]]++
-            if m[edges[i][j]] > 1 {
-                return edges[i][j]
-            }
-        }
+    if edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] {
+        return edges[0][0]
     }
-    return 0
+    return edges[0][1]
 }
