ef27db570abf7a35e5b40e951442169d564d1f58
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-07-23 21:13:31 -0700

    Update 2816_double_a_number_represented_as_a_linked_list.go

--- go/medium/2816_double_a_number_represented_as_a_linked_list.go
+++ go/medium/2816_double_a_number_represented_as_a_linked_list.go
@@ -7,17 +7,14 @@
  */
 func doubleIt(head *ListNode) *ListNode {
     if head.Val > 4 {
-        dummy := &ListNode{Next: head}
-        head = dummy
+        head = &ListNode{Next: head}
     }
 
     for runner := head; runner != nil; runner = runner.Next {
-        carry := 0
+        runner.Val = (runner.Val * 2) % 10
         if runner.Next != nil && runner.Next.Val > 4 {
-            carry = 1
+            runner.Val += 1
         }
-        runner.Val = runner.Val * 2 + carry
-        runner.Val = runner.Val % 10
     }
     return head
 }
