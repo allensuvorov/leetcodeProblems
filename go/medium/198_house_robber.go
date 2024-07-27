34c6a89bf182e48c06ed3f4252f54d742f2633a0
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-07-27 16:20:10 -0700

    Update 198_house_robber.go
    most intuitive solution

--- go/medium/198_house_robber.go
+++ go/medium/198_house_robber.go
@@ -1,7 +1,19 @@
 func rob(nums []int) int {
-    curMax, prevMax := 0, 0
-    for _, v := range nums {
-        curMax, prevMax = max(prevMax + v, curMax), curMax
+    dp := make([]int, len(nums))
+    maxMoney := 0
+    for i, stash := range nums {
+        
+        a := 0
+        b := 0
+        if i >= 2 {
+            a = dp[i-2]
+        }
+        if i >= 3 {
+            b = dp[i-3]
+        }
+        bestCur := max(a, b) + stash
+        maxMoney = max(maxMoney, bestCur)
+        dp[i] = bestCur
     }
-    return curMax
+    return maxMoney
 }
