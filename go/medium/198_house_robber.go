928de5dd9817f805bd5ea2b59a4e2303a08a70af
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-07-27 22:23:34 -0700

    Update 198_house_robber.go
    * fixed memory approach decomposed

--- go/medium/198_house_robber.go
+++ go/medium/198_house_robber.go
@@ -1,19 +1,19 @@
 func rob(nums []int) int {
-    dp := make([]int, len(nums))
-    maxMoney := 0
-    for i, stash := range nums {
-        
-        a := 0
-        b := 0
-        if i >= 2 {
-            a = dp[i-2]
-        }
-        if i >= 3 {
-            b = dp[i-3]
-        }
-        bestCur := max(a, b) + stash
-        maxMoney = max(maxMoney, bestCur)
-        dp[i] = bestCur
+
+    curMax := 0
+    prevMax := 0
+
+    for _, stash := range nums {
+        option1 := prevMax + stash
+        option2 := curMax
+
+        nextMax := max(option1, option2)
+
+        prevMax = curMax
+        curMax = nextMax
     }
-    return maxMoney
+    return curMax
 }
+
+// 1, 2, 3
+// prev, cur, next
