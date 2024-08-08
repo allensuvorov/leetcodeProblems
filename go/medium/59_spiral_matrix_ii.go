349e4c13ad878fd26b264a2ccff1a8163b076772
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-08-08 15:22:11 -0700

    Update 59_spiral_matrix_ii.go

--- go/medium/59_spiral_matrix_ii.go
+++ go/medium/59_spiral_matrix_ii.go
@@ -1,32 +1,32 @@
 func generateMatrix(n int) [][]int {
-	l, t := 0, 0
-    r, b := n-1, n-1
+	left, top := 0, 0
+    right, bottom := n-1, n-1
 	ans := make([][]int, n)
 	for i := range ans {
 		ans[i] = make([]int, n)
 	}
 
 	for count := 1; count <= n*n; {
-		for row, col := t, l; col <= r; col++ {
+		for row, col := top, left; col <= right; col++ {
 			ans[row][col] = count
 			count++
 		}
-		t++
-		for row, col := t, r; row <= b; row++ {
+		top++
+		for row, col := top, right; row <= bottom; row++ {
 			ans[row][col] = count
 			count++
 		}
-		r--
-		for row, col := b, r; col >= l; col-- {
+		right--
+		for row, col := bottom, right; col >= left; col-- {
 			ans[row][col] = count
 			count++
 		}
-		b--
-		for row, col := b, l; row >= t; row-- {
+		bottom--
+		for row, col := bottom, left; row >= top; row-- {
 			ans[row][col] = count
 			count++
 		}
-		l++
+		left++
 	}
 	return ans
 }
