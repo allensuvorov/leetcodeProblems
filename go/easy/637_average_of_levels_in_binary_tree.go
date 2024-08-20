7d1a7d8811e32394f01d41c8b4643538a223ecd4
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-08-20 16:20:51 -0700

    Update 637_average_of_levels_in_binary_tree.go

--- go/easy/637_average_of_levels_in_binary_tree.go
+++ go/easy/637_average_of_levels_in_binary_tree.go
@@ -10,23 +10,20 @@ func averageOfLevels(root *TreeNode) []float64 {
     res := []float64{}
     q := []*TreeNode{root}
     for len(q) > 0 {
-        rowLen := len(q)
         sum := 0
-        for range rowLen {
+        levLen := len(q)
+        for range levLen {
             now := q[0]
             q = q[1:]
-
             sum += now.Val
-
             if now.Left != nil {
                 q = append(q, now.Left)
             }
-
             if now.Right != nil {
                 q = append(q, now.Right)
             }
         }
-        res = append(res, float64(sum) / float64(rowLen))
+        res = append(res, float64(sum) / float64(levLen))
     }
     return res
 }
