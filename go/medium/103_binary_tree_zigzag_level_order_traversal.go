4944279411d40fc6db8433a74c6b62488801f301
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-08-20 17:45:48 -0700

    Update 103_binary_tree_zigzag_level_order_traversal.go
    * refactored naming

--- go/medium/103_binary_tree_zigzag_level_order_traversal.go
+++ go/medium/103_binary_tree_zigzag_level_order_traversal.go
@@ -14,15 +14,15 @@ func zigzagLevelOrder(root *TreeNode) [][]int {
     zig := true
     q := []*TreeNode{root}
     for len(q) > 0 {
-        lenth := len(q)
-        level := make([]int, lenth)
-        for i := range lenth {
+        levelSize := len(q)
+        level := make([]int, levelSize)
+        for i := range levelSize {
             now := q[0]
             q = q[1:]
             if zig {
                 level[i] = now.Val
             } else {
-                level[lenth - 1 - i] = now.Val
+                level[levelSize - 1 - i] = now.Val
             }
             if now.Left != nil {
                 q = append(q, now.Left)
