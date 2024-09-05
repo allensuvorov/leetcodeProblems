ae1e6d7d43ffa3f05b92ae1844a05c1ef9f1f7cc
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-09-05 12:53:41 -0700

    Update 64_minimum_path_sum.go
    - bug fixed

--- go/medium/64_minimum_path_sum.go
+++ go/medium/64_minimum_path_sum.go
@@ -6,42 +6,41 @@ func minPathSum(grid [][]int) int {
 
     q := [][2]int{[2]int{0,0}}
     for len(q) > 0 {
-        diagonalSize := len(q)
-        for range diagonalSize {
-            now := q[0]
-            q = q[1:]
+        now := q[0]
+        q = q[1:]
 
-            up := -1
-            if now[0] - 1 >= 0 {
-                parent := [2]int{now[0] - 1, now[1]}
-                up = dp[parent]
-            }
+        up := -1
+        if now[0] - 1 >= 0 {
+            parent := [2]int{now[0] - 1, now[1]}
+            up = dp[parent]
+        }
 
-            left := -1
-            if now[1] - 1 >= 0 {
-                parent := [2]int{now[0], now[1] - 1}
-                left = dp[parent]
-            }
+        left := -1
+        if now[1] - 1 >= 0 {
+            parent := [2]int{now[0], now[1] - 1}
+            left = dp[parent]
+        }
 
-            switch {
-            case up > 0 && left > 0:
-                dp[now] = min(up, left)
-            case up > 0:
-                dp[now] = up
-            case left > 0:
-                dp[now] = left
-            }
+        switch {
+        case up >= 0 && left >= 0:
+            dp[now] = min(up, left)
+        case up >= 0:
+            dp[now] = up
+        case left >= 0:
+            dp[now] = left
+        }
 
-            dp[now] += grid[now[0]][now[1]]
+        dp[now] += grid[now[0]][now[1]]
+        fmt.Println("row*cols:", rows*cols, "len(dp):", len(dp), "dp[now]: ", dp[now])
 
-            if now[0] + 1 < rows {
-                if len(q) == 0 || q[len(q)-1] != [2]int{now[0] + 1, now[1]} {
-                    q = append(q, [2]int{now[0] + 1, now[1]})
-                }
+        if now[0] + 1 < rows {
+            if len(q) == 0 || q[len(q)-1] != [2]int{now[0] + 1, now[1]} {
+                q = append(q, [2]int{now[0] + 1, now[1]})
             }
-            if now[1] + 1 < cols {
-                q = append(q, [2]int{now[0], now[1] + 1})
-            }
+        }
+
+        if now[1] + 1 < cols {
+            q = append(q, [2]int{now[0], now[1] + 1})
         }
     }
     end := [2]int{rows-1, cols-1}
