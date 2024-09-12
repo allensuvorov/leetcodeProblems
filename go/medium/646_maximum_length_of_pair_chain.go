7020df7243f5d308e0904926a8769acd20dc5876
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-09-11 22:02:28 -0700

    Update 646_maximum_length_of_pair_chain.go
    * time O(n log n)

--- go/medium/646_maximum_length_of_pair_chain.go
+++ go/medium/646_maximum_length_of_pair_chain.go
@@ -1,22 +1,18 @@
 func findLongestChain(pairs [][]int) int {
-    slices.SortFunc(pairs, func(a, b []int) int {
-        if a[0] != b[0] {
-            return a[0] - b[0]
-        }
-        return a[1] - b[1]
+    var count = len(pairs)
+    sort.Slice(pairs, func(i, j int) bool {
+        return pairs[i][1] < pairs[j][1]
     })
     fmt.Println(pairs)
-    
-    maxLen := 1
 
-    dp := make([]int, len(pairs))
-    for i := range pairs {
-        for j := 0; j < i; j++ {
-            if pairs[i][0] > pairs[j][1] {
-                maxLen = max(maxLen, dp[j] + 1)
-            }
+    maxEnd := pairs[0][1]
+    pairs = pairs[1:]
+    for _, pair := range pairs {
+        if (maxEnd >= pair[0]) {
+            count -= 1
+        } else {
+            maxEnd = pair[1]
         }
-        dp[i] = maxLen
     }
-    return maxLen
+    return count
 }
