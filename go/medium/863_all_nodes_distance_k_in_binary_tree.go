d7d137fa8a0d92fd4e67399fde4065efb8a0f6cd
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-09-13 22:05:29 -0700

    Update 863_all_nodes_distance_k_in_binary_tree.go
    * complete the graph and 
    * DFS or BFS starting from target

--- go/medium/863_all_nodes_distance_k_in_binary_tree.go
+++ go/medium/863_all_nodes_distance_k_in_binary_tree.go
@@ -7,40 +7,7 @@
  * }
  */
 func distanceK(root *TreeNode, target *TreeNode, k int) []int {
-    // descendents - target sub-tree - DFS from target
-    // ancestors 
-    //  - bfs from root to learn level of target
-    //  - go remaining distance in opposite subtree
-
-    // but let's try and create a more universal approach 
-    // may be it's in direction of treversal
-    res := []int{}
-    var dfs func(node *TreeNode, dist int) int
-    dfs = func(node *TreeNode, dist int) int {
-        if root == nil {
-            return 0
-        }
-
-        if dist == k {
-            res = append(res, node.Val)
-        }
-
-        if root == target || dist > 0{
-            dist =+
-        }
-
-        if dfs(node.Left, dist) > 0 {
-            dist++
-        }
-        if dfs(node.Right, dist) > 0 {
-            dist++
-        }
-        
-        return dist
-    }
-
-    dfs(root, 0)
-
-    return res
-
+    // The logic is to: 
+    // Save links to parents in a map
+    // go from the target in all directions, up and down
 }
