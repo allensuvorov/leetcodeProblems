d6f10c7528ddfa955c1e8b92db1b3337077a3d5b
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-09-18 23:49:58 -0700

    Update 310_minimum_height_trees.go
    * some ideas

--- go/medium/310_minimum_height_trees.go
+++ go/medium/310_minimum_height_trees.go
@@ -1,1 +1,15 @@
 // TODO
+
+func findMinHeightTrees(n int, edges [][]int) []int {
+    // bruteforce
+    // 20 000
+    // 400_000_000
+    
+    // dfs
+    // find min height, save heights
+    // find all trees with that height
+
+
+    // Linear
+    // start with leaves - save path to leaf
+}
