1c575993cdb74560079160a6a04dfc99d4289914
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-09-23 17:59:06 -0700

    Update 134_gas_station.go

--- go/medium/134_gas_station.go
+++ go/medium/134_gas_station.go
@@ -1,12 +1,10 @@
 func canCompleteCircuit(gas []int, cost []int) int {
-    // bruteforce, simulation: 10^5; n^2 = 10^10
     // answer is right after the lowest deficit point, if we ever get out of deficit
     worstDeficitPoint := 0
     worstDeficit := 10_000
     totalDeficit := 0
     for i := range gas {
         totalDeficit += gas[i] - cost[i] 
-        fmt.Println(totalDeficit)
         if totalDeficit <= worstDeficit {
             worstDeficit = totalDeficit
             worstDeficitPoint = i
