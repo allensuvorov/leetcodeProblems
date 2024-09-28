d08eb753339d33f1cee661952d82911248845691
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-09-28 15:33:48 -0700

    Update 2032_two_out_of_three.go
    * renamed func to getSet

--- go/easy/2032_two_out_of_three.go
+++ go/easy/2032_two_out_of_three.go
@@ -1,9 +1,9 @@
 func twoOutOfThree(nums1 []int, nums2 []int, nums3 []int) []int {
     repeatingNums := []int{}
 
-    set1 := freqCount(nums1)
-    set2 := freqCount(nums2)
-    set3 := freqCount(nums3)
+    set1 := getSet(nums1)
+    set2 := getSet(nums2)
+    set3 := getSet(nums3)
     
     for num := 1; num <= 100; num++ {
         if set1[num] + set2[num] + set3[num] > 1 {
@@ -13,12 +13,12 @@ func twoOutOfThree(nums1 []int, nums2 []int, nums3 []int) []int {
     return repeatingNums
 }
 
-func freqCount(nums []int) []int {
-    freqCount := make([]int, 101)
+func getSet(nums []int) []int {
+    set := make([]int, 101)
     for _, v := range nums {
-        if freqCount[v] == 0 {
-            freqCount[v] = 1
+        if set[v] == 0 {
+            set[v] = 1
         }
     }
-    return freqCount
+    return set
 }
