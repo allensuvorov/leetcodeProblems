6436544061a5174e69eb2224a76c98c016ee63ba
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-09-29 17:27:17 -0700

    Create 2025_maximum_number_of_ways_to_partition_an_array.go
    * TODO

---
+++ go/hard/2025_maximum_number_of_ways_to_partition_an_array.go
@@ -0,0 +1,20 @@
+// TODO
+
+// target time - 40 min (60 mins max)
+// logic - 15 min
+func waysToPartition(nums []int, k int) int {
+    // linear Big O (n)
+    // DP, reverse, map, prefix
+
+}
+
+// nums = [2,-1,2]
+// pref = [2, 1,3]
+
+// nums = [3,-1,2]
+// pref = [3, 2,4]
+
+// prefix == suffix
+
+// hash map
+// map[num]idx{}
