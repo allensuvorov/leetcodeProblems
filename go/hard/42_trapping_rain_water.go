a84ac048380d527b21409b5727c8b5ce96c6ba70
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-10-04 17:40:49 -0700

    Update 42_trapping_rain_water.go

--- go/hard/42_trapping_rain_water.go
+++ go/hard/42_trapping_rain_water.go
@@ -1,34 +1,31 @@
 func trap(height []int) int {
-    // find max
-    max := 0
+    maxH := 0
     maxI := 0
-    for i, v := range height{
-        if v > max {
-            max = v
+    for i, v := range height {
+        if maxH < v {
+            maxH = v
             maxI = i
         }
     }
 
-    // count water
-    res := 0
-
-    max = 0
-    for l := 0; l < maxI; l ++ {
-        if max < height[l] {
-            max = height[l]
+    water := 0 
+    curMax := 0
+    for i := 0; i < maxI; i++ {
+        if curMax < height[i] {
+            curMax = height[i]
         } else {
-            res +=(max - height[l])
-        }
-
-    }
-    
-    max = 0
-    for r := len(height)-1; r > maxI; r -- {
-        if max < height[r] {
-            max = height[r]
-        } else {
-            res +=(max - height[r])
+            water += curMax - height[i] 
         }
     }
-    return res
+
+    curMax = 0
+    for i := len(height) - 1; i > maxI; i-- {
+        if curMax < height[i] {
+            curMax = height[i]
+        } else {
+            water += curMax - height[i] 
+        }
+    }
+
+    return water
 }
