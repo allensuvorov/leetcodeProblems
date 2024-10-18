48fb3b946312033d923f70c265dbeeb11cea4185
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-10-17 19:56:46 -0700

    Update 238_product_of_array_except_self.go
    * Constant extra space

--- go/medium/238_product_of_array_except_self.go
+++ go/medium/238_product_of_array_except_self.go
@@ -1,25 +1,14 @@
 func productExceptSelf(nums []int) []int {
-    
-    left := make([]int, len(nums))
-    right := make([]int, len(nums))
-    
-    product := 1
-    for i, v := range nums {
-        product *= v
-        left[i] = product
-    }
-
-    product = 1
-    for i := len(nums) - 1; i >= 0; i-- {
-        product *= nums[i]
-        right[i] = product
-    }
-
     res := make([]int, len(nums))
-    res[0] = right[1]
-    res[len(res)-1] = left[len(res)-2]
-    for i := 1; i < len(res)-1; i++ {
-        res[i] = left[i-1] * right[i+1]
+    left := 1
+    for i := 0; i < len(nums); i++ {
+        res[i] = left
+        left *= nums[i]
+    }
+    right := 1
+    for i := len(nums) - 1; i >= 0; i-- {
+        res[i] *= right
+        right *= nums[i]
     }
     return res
 }
