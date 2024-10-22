beaf13d8535bfbfe9b88b7213c28b86c34b093b5
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-10-21 20:18:46 -0700

    Update 1657_determine_if_two_strings_are_close.go
    * hash map

--- go/medium/1657_determine_if_two_strings_are_close.go
+++ go/medium/1657_determine_if_two_strings_are_close.go
@@ -7,8 +7,13 @@ func closeStrings(word1 string, word2 string) bool {
         return false
     }
 
-    if getFreqCount(word1) != getFreqCount(word2) {
-        return false
+    fcfc1 := getFreqCount(word1) 
+    fcfc2 := getFreqCount(word2)
+
+    for k, v := range fcfc1 {
+        if fcfc2[k] != v {
+            return false
+        }
     }
     return true
 }
@@ -21,13 +26,13 @@ func getSet(s string) [26]bool {
     return byteSet
 }
 
-func getFreqCount (s string) [1e5+1]int {
+func getFreqCount (s string) map[int]int {
     byteCount := [26]int{}
-    freqCount := [1e5+1]int{}
     for _, v := range s {
         byteCount[v - 'a']++
     }
 
+    freqCount := map[int]int{}
     for _, v := range byteCount {
         freqCount[v]++
     }
