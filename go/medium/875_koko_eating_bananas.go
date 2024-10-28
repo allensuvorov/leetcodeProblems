78e353f9c65d257e268350a77e4ef17c3425d55e
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-10-28 14:52:56 -0700

    Update 875_koko_eating_bananas.go
    * min speed (left boundary) to 1 banana
    * time calculation: implement math.Ceil

--- go/medium/875_koko_eating_bananas.go
+++ go/medium/875_koko_eating_bananas.go
@@ -1,13 +1,13 @@
 func minEatingSpeed(piles []int, h int) int {
-    l := len(piles)
+    l := 1
     r := 1_000_000_000
-    for l < r {
-        m := l + (r - l)/2 + 1
+    for l <= r {
+        m := l + (r - l)/2
         time := eatTime(piles, m)
         if time <= h {
             r = m-1
         } else {
-            l = m
+            l = m+1
         }
     }
     return l
@@ -15,9 +15,12 @@ func minEatingSpeed(piles []int, h int) int {
 
 
 func eatTime(piles []int, k int) int {
-    count := 0
+    hours := 0
     for _, v := range piles {
-        count += v / k + 1
+        if v % k != 0 {
+            hours++
+        }
+        hours += v / k
     }
-    return count
+    return hours
 }
