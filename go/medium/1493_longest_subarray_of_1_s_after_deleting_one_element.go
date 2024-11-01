3d42076286a20f0ed7843ddb12b80d02b00d52c4
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-11-01 14:06:09 -0700

    Update 1493_longest_subarray_of_1_s_after_deleting_one_element.go
    * make consistent names for count vars

--- go/medium/1493_longest_subarray_of_1_s_after_deleting_one_element.go
+++ go/medium/1493_longest_subarray_of_1_s_after_deleting_one_element.go
@@ -1,35 +1,32 @@
 func longestSubarray(nums []int) int {
     res := 0
     onesCount := 0
-    zeroCount := 0
+    zerosCount := 0
     l := 0
     r := 0
-
-    zerosExist := false
 
     for r < len(nums) {
         if nums[r] == 1 {
             onesCount++
         } else {
-            zeroCount++
-            zerosExist = true
+            zerosCount++
         }
         
-        if zeroCount > 1 {
+        if zerosCount > 1 {
             if nums[l] == 1 {
                 onesCount--
             } else {
-                zeroCount--
+                zerosCount--
             }
             l++
         }
 
-        if zeroCount <= 1 {
+        if zerosCount <= 1 {
             res = max(res, onesCount)
         }
         r++
     }
-    if !zerosExist {
+    if res == len(nums) {
         res--
     }
     return res
