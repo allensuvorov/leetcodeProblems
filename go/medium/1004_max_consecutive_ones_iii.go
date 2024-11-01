07434c77b922145f1f312bbea7df5611e3c09b20
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-11-01 00:09:28 -0700

    Update 1004_max_consecutive_ones_iii.go

--- go/medium/1004_max_consecutive_ones_iii.go
+++ go/medium/1004_max_consecutive_ones_iii.go
@@ -1,32 +1,22 @@
 func longestOnes(nums []int, k int) int {
 	zeros := 0
-    if nums[0] == 0 {
-        zeros = 1
-    }
 	l, r := 0, 0
-	
-    res := 1
-    if k == 0 &&  nums[0] == 0 {
-        res = 0
-    }
+    res := 0
 
-	for l <= r && r < len(nums) {
-
-		if (l == r || zeros <= k) && r < len(nums)-1 {
-			r++
-			if nums[r] == 0 {
-				zeros++
-			}
-		} else {
-			if nums[l] == 0 {
+	for r < len(nums) {
+        if nums[r] == 0 {
+            zeros++
+        }
+		if zeros > k{
+            if nums[l] == 0 {
 				zeros--
 			}
 			l++
 		}
 		if zeros <= k {
 			res = max(res, r-l+1)
-			fmt.Println(res)
 		}
+        r++
 	}
 	return res
 }
