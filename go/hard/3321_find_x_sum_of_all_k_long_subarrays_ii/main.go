c90a44543c552750cce8e612a6634c9c23d99082
Allen Suvorov <allen.suvorov@gmail.com>
2024-11-10 13:01:17 -0800

    move maxHeap back to one file and it works

--- go/hard/3321_find_x_sum_of_all_k_long_subarrays_ii/main.go
+++ go/hard/3321_find_x_sum_of_all_k_long_subarrays_ii/main.go
@@ -11,6 +11,35 @@ type Item struct {
 	priority int    // The priority of the item in the queue.
 }
 
+// A MaxHeap implements heap.Interface and holds Items.
+type MaxHeap []*Item
+
+func (pq MaxHeap) Len() int { return len(pq) }
+
+func (pq MaxHeap) Less(i, j int) bool {
+	if pq[i].priority == pq[j].priority {
+		return pq[i].value > pq[j].value
+	}
+	return pq[i].priority > pq[j].priority
+}
+
+func (pq MaxHeap) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *MaxHeap) Push(x any) {
+	*pq = append(*pq, x.(*Item))
+}
+
+func (pq *MaxHeap) Pop() any {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil // don't stop the GC from reclaiming the item eventually
+	*pq = old[0 : n-1]
+	return item
+}
+
 func findXSum(nums []int, k int, x int) []int64 {
 	// two heaps, minHeap and maxHeap, with rebalancing
 	// binary search
--- go/hard/3321_find_x_sum_of_all_k_long_subarrays_ii/maxheap.go
+++ go/hard/3321_find_x_sum_of_all_k_long_subarrays_ii/maxheap.go
@@ -1,30 +1,30 @@
 package main
 
-// A MaxHeap implements heap.Interface and holds Items.
-type MaxHeap []*Item
+// // A MaxHeap implements heap.Interface and holds Items.
+// type MaxHeap []*Item
 
-func (pq MaxHeap) Len() int { return len(pq) }
+// func (pq MaxHeap) Len() int { return len(pq) }
 
-func (pq MaxHeap) Less(i, j int) bool {
-	if pq[i].priority == pq[j].priority {
-		return pq[i].value > pq[j].value
-	}
-	return pq[i].priority > pq[j].priority
-}
+// func (pq MaxHeap) Less(i, j int) bool {
+// 	if pq[i].priority == pq[j].priority {
+// 		return pq[i].value > pq[j].value
+// 	}
+// 	return pq[i].priority > pq[j].priority
+// }
 
-func (pq MaxHeap) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-}
+// func (pq MaxHeap) Swap(i, j int) {
+// 	pq[i], pq[j] = pq[j], pq[i]
+// }
 
-func (pq *MaxHeap) Push(x any) {
-	*pq = append(*pq, x.(*Item))
-}
+// func (pq *MaxHeap) Push(x any) {
+// 	*pq = append(*pq, x.(*Item))
+// }
 
-func (pq *MaxHeap) Pop() any {
-	old := *pq
-	n := len(old)
-	item := old[n-1]
-	old[n-1] = nil // don't stop the GC from reclaiming the item eventually
-	*pq = old[0 : n-1]
-	return item
-}
+// func (pq *MaxHeap) Pop() any {
+// 	old := *pq
+// 	n := len(old)
+// 	item := old[n-1]
+// 	old[n-1] = nil // don't stop the GC from reclaiming the item eventually
+// 	*pq = old[0 : n-1]
+// 	return item
+// }
