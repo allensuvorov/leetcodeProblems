0faea5505bf9f781eb92f08222a9dac31bb90e99
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-11-11 15:25:36 -0800

    Create 12_integer_to_roman.go
    * recursion, hash map and 2 arrays

---
+++ go/medium/12_integer_to_roman.go
@@ -0,0 +1,24 @@
+func intToRoman(num int) string {
+    if num == 0 {
+        return ""
+    }
+    symbolToValue := map[string]int{
+        "I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, 
+                "XL": 40, "L": 50, "XC": 90, "C": 100,
+                "CD": 400, "D": 500, "CM": 900, "M": 1000,
+    }
+
+    value := []int{    1,   4,    5,   9,    10,  40,   50,  90,   100, 400,  500, 900,  1000}
+    symbol := []string{"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"}
+    
+    ans := ""
+    for i := len(value) - 1; i >= 0; i-- {
+        if value[i] <= num {
+            ans = symbol[i]
+            break
+        }
+    }
+    fmt.Println(ans)
+
+    return ans + intToRoman(num - symbolToValue[ans])
+}
