6dcee863a6806c6b919ad6b8d06345c69d9a10c0
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-11-11 15:35:58 -0800

    Update 12_integer_to_roman.go
    * add iterative solution
    * it's based on two arrays

--- go/medium/12_integer_to_roman.go
+++ go/medium/12_integer_to_roman.go
@@ -1,4 +1,23 @@
 func intToRoman(num int) string {
+	value := []int{1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000}
+	symbol := []string{"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"}
+
+	ans := ""
+	for num > 0 {
+		i := 0
+		for i = len(value) - 1; i >= 0; i-- {
+			if value[i] <= num {
+				break
+			}
+		}
+		num -= value[i]
+		ans += symbol[i]
+	}
+
+	return ans
+}
+
+func intToRoman(num int) string {
     if num == 0 {
         return ""
     }
