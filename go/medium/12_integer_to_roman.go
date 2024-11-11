f3ca276d35271b130f66fae7a99fb273857b5843
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-11-11 15:38:40 -0800

    Update 12_integer_to_roman.go
    * update recursive solution
    * remove map, keep two arrays

--- go/medium/12_integer_to_roman.go
+++ go/medium/12_integer_to_roman.go
@@ -18,25 +18,19 @@ func intToRoman(num int) string {
 }
 
 func intToRoman(num int) string {
-    if num == 0 {
-        return ""
-    }
-    symbolToValue := map[string]int{
-        "I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, 
-                "XL": 40, "L": 50, "XC": 90, "C": 100,
-                "CD": 400, "D": 500, "CM": 900, "M": 1000,
-    }
+	if num == 0 {
+		return ""
+	}
 
-    value := []int{    1,   4,    5,   9,    10,  40,   50,  90,   100, 400,  500, 900,  1000}
-    symbol := []string{"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"}
-    
-    ans := ""
-    for i := len(value) - 1; i >= 0; i-- {
-        if value[i] <= num {
-            ans = symbol[i]
-            break
-        }
-    }
+	value := []int{1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000}
+	symbol := []string{"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"}
 
-    return ans + intToRoman(num - symbolToValue[ans])
+	i := 0
+	for i = len(value) - 1; i >= 0; i-- {
+		if value[i] <= num {
+			break
+		}
+	}
+
+	return symbol[i] + intToRoman(num-value[i])
 }
