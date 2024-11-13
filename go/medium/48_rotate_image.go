b0dd3da6b539ad08af71d07daf24a137e09eb136
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-11-12 17:55:31 -0800

    Update 48_rotate_image.go
    * quadruple swap

--- go/medium/48_rotate_image.go
+++ go/medium/48_rotate_image.go
@@ -1,14 +1,11 @@
 func rotate(matrix [][]int)  {
-    
     for left := range len(matrix)/2 {
-        for i := 0; i < len(matrix) - 1 - left; i++ {
-            right := len(matrix) - 1 - left
+        right := len(matrix) - 1 - left
+        for i := range right - left {
             r1, r2, r3, r4 := left,     left + i,     right,        right - i
             c1, c2, c3, c4 := left + i, right,        right - i,    left
-            // quadrop (quadro swap)
             matrix[r2][c2], matrix[r3][c3], matrix[r4][c4], matrix[r1][c1] = matrix[r1][c1], matrix[r2][c2], matrix[r3][c3], matrix[r4][c4]
         }
-        
         left++
     }
 }
