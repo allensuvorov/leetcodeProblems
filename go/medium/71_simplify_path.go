7ccea486701e3793f5300c877311ac4e648edaaa
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-11-13 19:38:02 -0800

    Update 71_simplify_path.go
    * use strings.Split and refactor

--- go/medium/71_simplify_path.go
+++ go/medium/71_simplify_path.go
@@ -1,21 +1,14 @@
 func simplifyPath(path string) string {
-    path += "/"
     stack := make([]string, 0)
-    name := make([]byte, 0)
-    for i := range path {
-        if path[i] == '/' {
-            switch string(name) {
-            case "", ".": // do nothing
-            case "..": // pop
-                if len(stack) > 0 {
-                    stack = stack[:len(stack)-1]
-                }
-            default: // push
-                stack = append(stack, string(name))
+    for _, name := range strings.Split(path, "/") {
+        switch name {
+        case "", ".": // do nothing
+        case "..": // pop
+            if len(stack) > 0 {
+                stack = stack[:len(stack)-1]
             }
-            name = name[:0]
-        } else {
-            name = append(name, path[i])
+        default: // push
+            stack = append(stack, name)
         }
     }
     return "/" + strings.Join(stack, "/")
