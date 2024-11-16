8cf70262ca4e12a1fd73372e850d73cef7050c3f
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-11-15 20:45:05 -0800

    Update 224_basic_calculator.go
    * update var names
    * minus -> sign
    * numExists -> numReady

--- go/hard/224_basic_calculator.go
+++ go/hard/224_basic_calculator.go
@@ -1,38 +1,38 @@
 func calculate(s string) int {
-    sum, _ := getSum([]rune(s))
+    sum, _ := evaluate([]rune(s))
     return sum
 }
 
-func getSum(s []rune) (int, int) {
+func evaluate(s []rune) (int, int) {
     sum, num := 0, 0
-    minus := 1
-    numExists := false
+    sign := 1
+    numReady := false
     for i := 0; i < len(s); i++{
-        if !unicode.IsNumber(s[i]) && numExists {
-            sum += num * minus
-            num, numExists, minus = 0, false, 1
+        if !unicode.IsNumber(s[i]) && numReady {
+            sum += num * sign
+            num, numReady, sign = 0, false, 1
+        }
+        
+        switch s[i] {
+        case ' ', '+': // skip
+        case '(':
+            num, i = evaluate(s[i+1:])
+            i = len(s) - i
+            sum += num * sign
+            num, numReady, sign = 0, false, 1
+        case ')':
+            return sum, len(s) - i
+        case '-':
+            sign = -1
         }
 
         if unicode.IsNumber(s[i]) {
             num = num*10 + int(s[i] - '0')
-            numExists = true
-        }
-
-        switch s[i] {
-        case ' ', '+': // skip
-        case '(':
-            num, i = getSum(s[i+1:])
-            i = len(s) - i
-            sum += num * minus
-            num, numExists, minus = 0, false, 1
-        case ')':
-            return sum, len(s) - i
-        case '-':
-            minus = -1
+            numReady = true
         }
     }
     if num > 0 {
-        sum += num * minus
+        sum += num * sign
     }
     return sum, 0
 }
