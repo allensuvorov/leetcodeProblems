503967aa9257ca7b6588cd2968a7dea9349aa99e
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-11-16 13:54:55 -0800

    Update 2517_maximum_tastiness_of_candy_basket.go
    * refactor - fix naming to countCandyAtTastiness
    * show logic

--- go/medium/2517_maximum_tastiness_of_candy_basket.go
+++ go/medium/2517_maximum_tastiness_of_candy_basket.go
@@ -1,12 +1,27 @@
+// binary search and simple iterative count 
+
+/*
+Tastiness
+              l m  r
+1 2 3 4 5 6 7 8 9 10
+
+Distanses
+*                    *                             *
+1 2     5     8            13                      21
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21
+
+*/
+
 func maximumTastiness(price []int, k int) int {
     slices.Sort(price)
 
+    // range of possible tastiness
     l := 0
     r := (price[len(price) - 1] - price[0]) / (k - 1)
 
     for l < r {
-        m := l + (r-l)/2 + 1
-        candyCount := fillsBasket(price, m)
+        m := l + (r-l)/2 + 1 // a guess tastiness
+        candyCount := countCandyAtTastiness(price, m)
 
         if candyCount >= k {
             l = m
@@ -17,7 +32,7 @@ func maximumTastiness(price []int, k int) int {
     return l
 }
 
-func fillsBasket(price []int, dist int) int {
+func countCandyAtTastiness(price []int, dist int) int {
     candyCount := 1
     lastPrice := price[0]
     for i := range price {
