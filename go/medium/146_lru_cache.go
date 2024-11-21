d83369fcc5145b09f5f74350e48b61bb4fffe778
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-11-20 21:10:14 -0800

    Update 146_lru_cache.go
    * use "container/list"

--- go/medium/146_lru_cache.go
+++ go/medium/146_lru_cache.go
@@ -1,78 +1,49 @@
-type Element struct {
+import "container/list"
+
+type Node struct {
     Key int
     Val int
-    Next *Element
-    Prev *Element
 }
 
 
 type LRUCache struct {
-    Capacity int
-    Elements map[int] *Element
-    Front *Element
-    Back *Element
+    capacity int
+    cache map[int] *list.Element
+    list *list.List
 }
 
 
 func Constructor(capacity int) LRUCache {
-    return LRUCache{ Capacity: capacity, Elements: make(map[int]*Element) }
-}
-
-
-func (this *LRUCache) MoveToBack(element *Element) {
-    if element.Next == nil {
-        return
+    return LRUCache { 
+        capacity: capacity, 
+        cache: make(map[int]*list.Element), 
+        list: list.New(),
     }
-
-    if element.Prev == nil {
-        this.Front = element.Next
-        this.Front.Prev = nil
-    } else {
-        element.Prev.Next, element.Next.Prev = element.Next, element.Prev
-    }
-
-    element.Next = nil
-    element.Prev = this.Back
-    this.Back.Next = element
-    this.Back = element
 }
 
 
 func (this *LRUCache) Get(key int) int {
-    if element, ok := this.Elements[key]; ok {
-        this.MoveToBack(element)
-        return element.Val
+    if element, ok := this.cache[key]; ok {
+        this.list.MoveToBack(element)
+        return element.Value.(Node).Val
     }
     return -1
 }
 
 
 func (this *LRUCache) Put(key int, value int)  {
-    if element, ok := this.Elements[key]; ok {
-        element.Val = value
-        this.MoveToBack(element)
+    node := Node {Key: key, Val: value}
+    if element, ok := this.cache[key]; ok {
+        element.Value = node
+        this.list.MoveToBack(element)
     } else {
-        element = &Element{
-            Key: key,
-            Val: value,
+        if this.list.Len() == this.capacity {
+            oldest := this.list.Front()
+            delete(this.cache, oldest.Value.(Node).Key)
+            this.list.Remove(this.list.Front())
         }
-
-        if len(this.Elements) == 0 {
-            this.Back = element
-            this.Front = element 
-        } else {
-            element.Prev = this.Back
-            this.Back.Next = element
-            this.Back = element
-        }
-
-        this.Elements[key] = element
-        if len(this.Elements) > this.Capacity {
-            delete(this.Elements, this.Front.Key)
-
-            this.Front = this.Front.Next
-            this.Front.Prev = nil
-        }
+        element = this.list.PushBack(node)
+        this.cache[key] = element
     }
 }
 
