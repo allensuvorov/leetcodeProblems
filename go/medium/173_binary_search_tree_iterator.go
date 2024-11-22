39d6d9497f7852d8ea692237b42c5b2ca4578ede
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-11-21 21:52:24 -0800

    Create 173_binary_search_tree_iterator.go
    * iterative DFS

---
+++ go/medium/173_binary_search_tree_iterator.go
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+type BSTIterator struct {
+    stack []*TreeNode
+}
+
+
+func Constructor(root *TreeNode) BSTIterator {
+    stack := make([]*TreeNode, 0)
+    for root != nil {
+        stack = append(stack, root)
+        root = root.Left
+    }
+    return BSTIterator{stack: stack}
+}
+
+
+func (this *BSTIterator) Next() int {
+    top := len(this.stack) - 1
+    topNode := this.stack[top]
+    this.stack = this.stack[:top]
+    for node := topNode.Right; node != nil; node = node.Left {
+        this.stack = append(this.stack, node)
+    }
+    return topNode.Val
+}
+
+
+func (this *BSTIterator) HasNext() bool {
+    return len(this.stack) != 0
+}
+
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * obj := Constructor(root);
+ * param_1 := obj.Next();
+ * param_2 := obj.HasNext();
+ */
