5b83422cca65ebe3fc64d4d888cac7deacddba95
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-11-22 19:04:29 -0800

    Update 117_populating_next_right_pointers_in_each_node_ii.go
    * rename q to list
    * those are more like lists then queues

--- go/medium/117_populating_next_right_pointers_in_each_node_ii.go
+++ go/medium/117_populating_next_right_pointers_in_each_node_ii.go
@@ -9,22 +9,22 @@
  */
 
 func connect(root *Node) *Node {
-    q1 := root // currentQueue
-    for q1 != nil {
-        dummy := &Node{} // nextQueue
-        q2 := dummy
-        for q1 != nil {
-            if q1.Left != nil {
-                q2.Next = q1.Left
-                q2 = q2.Next
+    list1 := root
+    for list1 != nil {
+        dummy := &Node{}
+        list2 := dummy
+        for list1 != nil {
+            if list1.Left != nil {
+                list2.Next = list1.Left
+                list2 = list2.Next
             }
-            if q1.Right != nil {
-                q2.Next = q1.Right
-                q2 = q2.Next
+            if list1.Right != nil {
+                list2.Next = list1.Right
+                list2 = list2.Next
             }
-            q1 = q1.Next
+            list1 = list1.Next
         }
-        q1 = dummy.Next
+        list1 = dummy.Next
     }
     return root
 }
