4b0ed2081189fe34d79a9afaefa270ebe0ff9f67
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-11-24 22:08:07 -0800

    Update 3122_minimum_number_of_operations_to_satisfy_conditions.go
    * add explanation

--- go/medium/3122_minimum_number_of_operations_to_satisfy_conditions.go
+++ go/medium/3122_minimum_number_of_operations_to_satisfy_conditions.go
@@ -1,17 +1,36 @@
+/*
+Medium - 3122. Minimum Number of Operations to Satisfy Conditions
+
+Time: Big O (n)
+Space: Big O (1)
+
+Approach: Let's imagine this is a story about a lazy gardner.
+
+* There are n gardens on a street. Each garden has m different flowers of garden variaty (10).
+* Each owner asked: I want all my m flowers to be the same, but different from my neighbors.
+* What's the minimum total number of flowers the gardner needs to change?
+
+Solution: We are trying to keep each neighbor happy, whily being lazy. 
+* Let's count cost of each solution in the current garden and add best option from previous garden.
+* That will show best solution for each flower type so far.
+*/
+
 func minimumOperations(grid [][]int) int {
     m := len(grid)
     n := len(grid[0])
 
-    prev := [10]int{}
-    curr := [10]int{}
+    const digits = 10 // number of possible digits
+
+    prev := [digits]int{} // prev best solution for each number
 
     for c := range n {
+        curr := [digits]int{}
         for r := range m { // curr count 
             curr[grid[r][c]]++
         }
-        for i := range 10 { // curr count + what was best in prev for any other number
+        for i := range digits { // curr count + best in prev for any other number
             maxPrev := 0
-            for j := range 10 {
+            for j := range digits {
                 if i != j {
                     maxPrev = max(maxPrev, prev[j])
                 }
@@ -19,25 +38,11 @@ func minimumOperations(grid [][]int) int {
             curr[i] += maxPrev
         }
         prev = curr
-        curr = [10]int{}
     }
 
     maxPrev := 0
-    for i := range 10 {
+    for i := range digits {
         maxPrev = max(maxPrev, prev[i])
     }
-    fmt.Println(prev)
     return m * n - maxPrev
 }
-
-
-/*
-
-1 0 2
-1 0 2
-
-0 [0, 4, 4]
-1 [2, 0, 4]
-2 [0, 2, 6]
-
-*/
