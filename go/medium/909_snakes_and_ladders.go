6de1601e764fd3e3788eaba234513666872b5e48
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-11-25 22:00:36 -0800

    Update 909_snakes_and_ladders.go
    * refactor ifs, to remove continue

--- go/medium/909_snakes_and_ladders.go
+++ go/medium/909_snakes_and_ladders.go
@@ -1,9 +1,8 @@
 func snakesAndLadders(board [][]int) int {
     n := len(board)
-    moves := 0
     q := []int{1}
 
-    for len(q) > 0 {
+    for moves := 0; len(q) > 0; moves++ {
         size := len(q)
         for range size {
             now := q[0]
@@ -11,20 +10,18 @@ func snakesAndLadders(board [][]int) int {
             if now == n*n {
                 return moves
             }
-            for next := now + 1; next <= n*n && next <= now + 6; next++ {
+            for next := now + 1; next <= min(n*n, now + 6); next++ {
                 r, c := getCoordinates(next, n)
-                switch board[r][c] {
-                case -1: 
-                    q = append(q, next)
-                case -2:
-                    continue
-                default:
-                    q = append(q, board[r][c])
+                if board[r][c] != -2 {
+                    if board[r][c] == -1 {
+                        q = append(q, next)
+                    } else {
+                        q = append(q, board[r][c])
+                    }
+                    board[r][c] = -2 // mark visited with -2
                 }
-                board[r][c] = -2 // mark visited with -2
             }
         }
-        moves++
     }
     return -1
 }
