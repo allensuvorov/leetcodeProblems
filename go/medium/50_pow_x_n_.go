a3f4c67b10448d6994c5aa00433d2164ee001053
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-11-29 20:41:48 -0800

    Update 50_pow_x_n_.go
    * recursive
    * nice and simple

--- go/medium/50_pow_x_n_.go
+++ go/medium/50_pow_x_n_.go
@@ -1,40 +1,33 @@
 func myPow(x float64, n int) float64 {
-    if n == 0 {
-        return 1
-    }
-    
     if x == 0 {
         return 0
     }
     
-    if n == 1 {
-        return x
+    if n == 0 {
+        return 1
     }
-
+    
     if x == 1 {
         return 1
     }
 
-    negExp := false
+    ans := myPow(x, abs(n) / 2)
+    ans *= ans
+    
+    if abs(n) % 2 == 1 {
+        ans *= x
+    }
+
     if n < 0 {
-        negExp = true
-        n = -n
-    }
-
-    a := x
-    ans := float64(1)
-
-    for n > 0 {
-        if n % 2 == 1 {
-            ans *= a
-        }
-        a *= a
-        n /= 2
-    }
-
-    if negExp {
-        ans = 1/ans
+        ans = 1 / ans
     }
 
     return ans
+}
+
+func abs(a int) int {
+    if a < 0 {
+        return -a
+    }
+    return a
 }
