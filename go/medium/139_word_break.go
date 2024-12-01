97e33f5a3fc1a6c24083b5e4c86b303f46ced8ca
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-11-30 19:12:56 -0800

    Update 139_word_break.go
    * DP
    * bottom - up

--- go/medium/139_word_break.go
+++ go/medium/139_word_break.go
@@ -1,45 +1,20 @@
-type Trie struct {
-    Root *TrieNode
-}
-
-type TrieNode struct {
-    Children [26]*TrieNode
-    IsWord bool
-}
+* DP
+* bottom - up
 
 func wordBreak(s string, wordDict []string) bool {
+	n := len(s)
+	dp := make([]bool, n+1)
+	dp[n] = true
 
-    // trie - prefix tree
-    t := Trie{&TrieNode{}}
-
-    for _, word := range wordDict {
-        now := t.Root
-        for _, v := range word {
-            if now.Children[v - 'a'] == nil {
-                now.Children[v - 'a'] = &TrieNode{}
-            }
-            now = now.Children[v - 'a']
-        }
-        now.IsWord = true
-    }
-    
-    // backtracking
-    var backtrack func(node *TrieNode, s string) bool
-    backtrack = func(node *TrieNode, s string) bool {
-        if len(s) == 0 {
-            return true
-        }
-        for i, v := range s{
-            if node.Children[v - 'a'] == nil {
-                return false
-            }
-            node = node.Children[v - 'a']
-            if node.IsWord && backtrack(t.Root, s[i + 1:]) {
-                return true
-            }
-        }
-        return false
-    }
-    return backtrack(t.Root, s)
-
+	for i := n - 1; i >= 0; i-- {
+		for _, word := range wordDict {
+			if word[0] == s[i] &&
+				i+len(word) <= n &&
+				word == s[i:i+len(word)] &&
+				dp[i+len(word)] {
+				dp[i] = true
+			}
+		}
+	}
+	return dp[0]
 }
