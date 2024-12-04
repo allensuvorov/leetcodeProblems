a0421be204e7b2724a9f511b26e16896be7ca86c
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-04 15:00:42 -0800

    Update 221_maximal_square.go
    * add recursion with cache solution

--- go/medium/221_maximal_square.go
+++ go/medium/221_maximal_square.go
@@ -26,3 +26,32 @@ func maximalSquare(matrix [][]byte) int {
     }
     return res*res
 }
+
+
+func maximalSquare(matrix [][]byte) int {
+    rows, cols := len(matrix), len(matrix[0])
+    res := 0
+    cache := make(map[[2]int]int, rows * cols)
+    
+    var helper func(r, c int) int 
+    helper = func(r, c int) int {
+        if r >= rows || c >= cols {
+            return 0
+        }
+
+        if _, ok := cache[[2]int{r,c}]; !ok {
+            right := helper(r,c+1)
+            down := helper(r+1,c)
+            diag := helper(r+1,c+1)
+            cache[[2]int{r,c}] = 0
+            if matrix[r][c] == '1' {
+                cache[[2]int{r,c}] = 1 + min(right, down, diag)
+                res = max(res, cache[[2]int{r,c}])
+            }
+        }
+        return cache[[2]int{r,c}]
+    }
+
+    helper(0,0)
+    return res*res
+}
