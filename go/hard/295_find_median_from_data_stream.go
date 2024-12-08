5204db60205b1b94e0dd82fa80794a8b227bcf8a
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-07 20:08:00 -0800

    Update 295_find_median_from_data_stream.go
    * solve with two heaps
    * max-heap and min-heap

--- go/hard/295_find_median_from_data_stream.go
+++ go/hard/295_find_median_from_data_stream.go
@@ -1,41 +1,44 @@
 type MedianFinder struct {
-    
+    minH *minHeap
+    maxH *maxHeap
 }
 
 
 func Constructor() MedianFinder {
-    
+    return MedianFinder {&minHeap{}, &maxHeap{}}
 }
 
 
 func (this *MedianFinder) AddNum(num int)  {
-    
+    if len(*this.minH) > 0 && num >= (*this.minH)[0] { // (*this.maxH)[0]
+        heap.Push(this.minH, num)
+    } else {
+        heap.Push(this.maxH, num)
+    }
+    this.rebalance()
 }
 
 
 func (this *MedianFinder) FindMedian() float64 {
-    
+    if len(*this.minH) == len(*this.maxH) {
+        return float64(float64((*this.minH)[0] + (*this.maxH)[0])/2)
+    } else {
+        return float64((*this.maxH)[0])
+    }
 }
 
 
-func (this *MedianFinder) rebalance() {
-
+func (this *MedianFinder) rebalance() { // len of max has to be equal of longer by one
+    if len(*this.minH) - len(*this.maxH) == 1 { // min is longer by one
+        num := heap.Pop(this.minH).(int)
+        heap.Push(this.maxH, num)
+    } else if len(*this.maxH) - len(*this.minH) > 1 { // max is longer by more than one
+        num := heap.Pop(this.maxH).(int)
+        heap.Push(this.minH, num)
+    }
 }
 
-
-// This example inserts several ints into an maxHeap, checks the minimum,
-// and removes them in order of priority.
-// func main() {
-// 	h := &maxHeap{2, 1, 5}
-// 	heap.Init(h)
-// 	heap.Push(h, 3)
-// 	fmt.Printf("minimum: %d\n", (*h)[0])
-// 	for h.Len() > 0 {
-// 		fmt.Printf("%d ", heap.Pop(h))
-// 	}
-// }
-
-// An maxHeap is a min-heap of ints.
+// An maxHeap is a max-heap of ints.
 type maxHeap []int
 
 func (h maxHeap) Len() int           { return len(h) }
