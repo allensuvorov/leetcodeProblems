e1d062a6c06d7806ef4ed51a0da230a01efb6e4c
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-12 17:57:58 -0800

    Update 80_remove_duplicates_from_sorted_array_ii.go
    * rename repCount to occurance

--- go/medium/80_remove_duplicates_from_sorted_array_ii.go
+++ go/medium/80_remove_duplicates_from_sorted_array_ii.go
@@ -1,15 +1,15 @@
 func removeDuplicates(nums []int) int {
     plant := 1
-    repCount := 0
+    occurance := 1
 
     for get := 1; get < len(nums); get++ {
         if nums[get] == nums[get - 1] {
-            repCount++
+            occurance++
         } else {
-            repCount = 0
+            occurance = 1
         }
 
-        if repCount < 2 {
+        if occurance <= 2 {
             nums[plant] = nums[get]
             plant++
         }
