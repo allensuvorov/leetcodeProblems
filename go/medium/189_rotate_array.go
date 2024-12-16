44c1b88ee7b4a12031540101eb5a1af2a6b69739
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-15 21:50:12 -0800

    Update 189_rotate_array.go
    * add O(n) extra memory solution

--- go/medium/189_rotate_array.go
+++ go/medium/189_rotate_array.go
@@ -1,6 +1,23 @@
+// O(1) extra memory
 func rotate(nums []int, k int)  {
     k = k % len(nums)
     slices.Reverse(nums)
     slices.Reverse(nums[:k])
     slices.Reverse(nums[k:])
+}
+
+// O(n) extra memory
+func rotate(nums []int, k int)  {
+    n := len(nums)
+    k = k % n
+    a := nums[:n - k]
+    b := nums[n - k:]
+    c := make([]int, 0, n)
+    for _, v := range b {
+        c = append(c, v)
+    }
+    for _, v := range a {
+        c = append(c, v)
+    }
+    copy(nums, c)
 }
