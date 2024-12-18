f6cf2a94ccde5d08670ff83a1aedecae10d19993
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-17 21:12:17 -0800

    Update 238_product_of_array_except_self.go
    * add second try

--- go/medium/238_product_of_array_except_self.go
+++ go/medium/238_product_of_array_except_self.go
@@ -12,3 +12,25 @@ func productExceptSelf(nums []int) []int {
     }
     return res
 }
+
+// second try
+func productExceptSelf(nums []int) []int {
+    n := len(nums)
+
+    suff := make([]int, n)
+    suff[n-1] = nums[n-1]
+    for i := n - 2; i >= 0; i-- {
+        suff[i] = nums[i]*suff[i+1]
+    }
+    
+    for i := 1; i < n; i++ {
+        nums[i] *= nums[i-1]
+    }
+
+    suff[0] = suff[1]
+    for i := 1; i < n - 1; i++ {
+        suff[i] = nums[i-1] * suff[i+1]
+    }
+    suff[n-1] = nums[n-2]
+    return suff
+}
