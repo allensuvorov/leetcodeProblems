c36d4915740017bbebf2d3ae67fed3d9c00b6d1c
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-17 16:38:06 -0800

    Update 274_h_index.go
    * code golf it all the way

--- go/medium/274_h_index.go
+++ go/medium/274_h_index.go
@@ -1,16 +1,10 @@
 func hIndex(citations []int) int {
-    const maxCitation = 1000
-    counts := make([]int, maxCitation + 1)
+    counts := make([]int, 1000 + 1)
     for _, v := range citations {
         counts[v]++
     }
-    sum := 0
-    c := maxCitation
-    for c >= 0 && c > sum  {
-        sum += counts[c]
-        if c <= sum {
-            return c
-        }
+    c := 1000
+    for sum := counts[c]; c > sum; sum += counts[c] {
         c--
     }
     return c
