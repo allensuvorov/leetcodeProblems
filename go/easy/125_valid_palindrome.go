bd5de28aa6ed736522eb96f81b860acfac183f44
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-19 12:09:11 -0800

    Update 125_valid_palindrome.go
    * refactor

--- go/easy/125_valid_palindrome.go
+++ go/easy/125_valid_palindrome.go
@@ -2,16 +2,12 @@ func isPalindrome(s string) bool {
 
     isAlphaNumeric := func(b byte) bool {
         r := rune(b)
-        if unicode.IsLetter(r) || unicode.IsDigit(r) {
-            return true
-        }
-        return false
+        return unicode.IsLetter(r) || unicode.IsDigit(r)
     }
 
-    toLower := func(b byte) byte {
+    toLower := func(b byte) rune {
         r := rune(b)
-        r = unicode.ToLower(r)
-        return byte(r)
+        return unicode.ToLower(r)
     }
 
     for l, r := 0, len(s) - 1; l < r; {
