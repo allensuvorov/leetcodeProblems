60114ad0a38cf984b7f5cbc891a3af5b7129f33c
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-18 17:28:04 -0800

    Update 12_integer_to_roman.go
    * reduce i if we are done this this i

--- go/medium/12_integer_to_roman.go
+++ go/medium/12_integer_to_roman.go
@@ -1,20 +1,19 @@
 func intToRoman(num int) string {
-	value := []int{1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000}
-	symbol := []string{"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"}
+    symbols := []string {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"}
+    values := []int     {1,    4,    5,   9,    10,  40,   50,  90,   100, 400, 500, 900, 1000}
 
-	ans := ""
-	for num > 0 {
-		i := 0
-		for i = len(value) - 1; i >= 0; i-- {
-			if value[i] <= num {
-				break
-			}
-		}
-		num -= value[i]
-		ans += symbol[i]
-	}
+    roman := ""
 
-	return ans
+    for i := len(values) - 1; num > 0 && i >= 0; {
+        if values[i] <= num {
+            roman += symbols[i]
+            num -= values[i]
+        } else {
+            i--
+        }
+    }
+
+    return roman
 }
 
 func intToRoman(num int) string {
