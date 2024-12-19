a118dca2d2072e9bf35da607f4a27824d4731a17
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-19 13:22:07 -0800

    Update 167_two_sum_ii_input_array_is_sorted.go
    * refactor

--- go/medium/167_two_sum_ii_input_array_is_sorted.go
+++ go/medium/167_two_sum_ii_input_array_is_sorted.go
@@ -1,13 +1,13 @@
 func twoSum(numbers []int, target int) []int {
-    l := 0
-    r := len(numbers)-1
-    for l<r {
-        sum := numbers[l] + numbers[r]
-        switch{
-            case sum == target: return []int{l+1, r+1}
-            case sum > target: r--
-            case sum < target: l++
+    l, r := 0, len(numbers) - 1
+    for l < r {
+        if numbers[l] + numbers[r] > target {
+            r--
+        } else if numbers[l] + numbers[r] < target {
+            l++
+        } else {
+            return []int{l+1, r+1}
         }
     }
-    return []int{}
+    return nil
 }
