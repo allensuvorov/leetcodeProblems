f8e20803716d369ff0d47073e1bc1c3ec57e68e0
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-18 16:25:00 -0800

    Update 134_gas_station.go
    * reset tank at negative

--- go/medium/134_gas_station.go
+++ go/medium/134_gas_station.go
@@ -1,23 +1,19 @@
 func canCompleteCircuit(gas []int, cost []int) int {
-    // answer is right after the lowest deficit point, if we ever get out of deficit
-    worstDeficitPoint := 0
-    worstDeficit := 10_000
-    totalDeficit := 0
-    for i := range gas {
-        totalDeficit += gas[i] - cost[i] 
-        if totalDeficit <= worstDeficit {
-            worstDeficit = totalDeficit
-            worstDeficitPoint = i
+    res, tank, total := 0, 0, 0
+
+    for i := range len(gas) {
+        diff := gas[i] - cost[i]
+        tank += diff
+        total += diff
+        if tank < 0 {
+            tank = 0
+            res = i + 1
         }
     }
 
-    if totalDeficit < 0 {
+    if total < 0 {
         return -1
     }
 
-    if worstDeficitPoint == len(gas) - 1 {
-        return 0
-    }
-
-    return worstDeficitPoint + 1
+    return res
 }
