12a94bc3ebda3b07bc0403284518e3994aae0806
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-19 16:45:25 -0800

    Update 11_container_with_most_water.go
    * practice

--- go/medium/11_container_with_most_water.go
+++ go/medium/11_container_with_most_water.go
@@ -1,16 +1,14 @@
 func maxArea(height []int) int {
-    ans := 0
     l, r := 0, len(height) - 1
-
+    res := 0
     for l < r {
         area := min(height[l], height[r]) * (r - l)
-        ans = max(ans, area)
-        if height[l] <= height[r] {
+        res = max(res, area)
+        if height[l] < height[r] {
             l++
         } else {
             r--
         }
     }
-
-    return ans
+    return res
 }
