8298d0a112decbbdee90362fd904c8ea8fb7e0fd
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-19 19:52:18 -0800

    Update 15_3sum.go
    * redo for practice

--- go/medium/15_3sum.go
+++ go/medium/15_3sum.go
@@ -1,23 +1,21 @@
-package medium
-
-func threeSum(nums []int) [][]int {    
-    res := [][]int{}
-    sort.Ints(nums)
+func threeSum(nums []int) [][]int {
+    res := make([][]int,0)
+    slices.Sort(nums)
 
     for i := 0; i < len(nums) && nums[i] <= 0; i++ {
         if i > 0 && nums[i] == nums[i-1] {
             continue
         }
-        l := i + 1
-        r := len(nums) - 1
+
+        l, r := i + 1, len(nums) - 1
         for l < r {
             sum := nums[i] + nums[l] + nums[r]
-            switch {
-            case sum < 0:
-                l++
+            switch{
             case sum > 0:
                 r--
-            case sum == 0:
+            case sum < 0:
+                l++
+            default:
                 res = append(res, []int{nums[i], nums[l], nums[r]})
                 l++
                 for nums[l] == nums[l-1] && l < r {
