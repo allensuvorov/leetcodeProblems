fa81e2f04e3a3b012cec9efe954d00d7cd6a1126
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-20 18:29:13 -0800

    Update 3_longest_substring_without_repeating_characters.go
    * redo with bool

--- go/medium/3_longest_substring_without_repeating_characters.go
+++ go/medium/3_longest_substring_without_repeating_characters.go
@@ -1,13 +1,12 @@
 func lengthOfLongestSubstring(s string) int {
-    set := make(map[byte]int)
+    set := make(map[byte]bool)
     res := 0
     for l, r := 0, 0; r < len(s); r++ {
-        set[s[r]]++
-
-        for set[s[r]] > 1 {
-            set[s[l]]--
+        for set[s[r]] {
+            set[s[l]] = false
             l++
         }
+        set[s[r]] = true
         res = max(res, r - l + 1)
     }
     return res
