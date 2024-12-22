7f60b2b6cab913b1bf311be776783bcdca03f041
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-21 18:53:44 -0800

    Update 290_word_pattern.go
    * redo for practice

--- go/easy/290_word_pattern.go
+++ go/easy/290_word_pattern.go
@@ -1,22 +1,21 @@
 func wordPattern(pattern string, s string) bool {
-    bm := map[byte]string{}
-    wm := map[string]byte{}
     words := strings.Fields(s)
-    if len(pattern) != len(words) {
+    if len(words) != len(pattern) {
         return false
     }
-    for i := 0; i < len(pattern); i++ {
-        if w, ok := bm[pattern[i]]; ok {
-            if w != words[i] {
-                return false
-            }
-        } else if b, ok := wm[words[i]]; ok{
-            if b != pattern[i] {
-                return false
-            }
+    lw := make(map[byte]string)
+    wl := make(map[string]byte)
+
+    for i := range words {
+        if v, ok := lw[pattern[i]]; ok && v != words[i] {
+            return false
         } else {
-            bm[pattern[i]] = words[i]
-            wm[words[i]] = pattern[i]
+            lw[pattern[i]] = words[i]
+        }
+        if v, ok := wl[words[i]]; ok && v != pattern[i] {
+            return false
+        } else {
+            wl[words[i]] = pattern[i]
         }
     }
     return true
