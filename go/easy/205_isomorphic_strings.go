c2c80a195e21a8530ee78d9081643418089abc40
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-21 18:30:37 -0800

    Update 205_isomorphic_strings.go
    * refactor into two functions

--- go/easy/205_isomorphic_strings.go
+++ go/easy/205_isomorphic_strings.go
@@ -1,18 +1,15 @@
 func isIsomorphic(s string, t string) bool {
+    return isMapping(s,t) && isMapping(t,s)
+}
+
+func isMapping(s, t string) bool {
     st := make(map[byte]byte)
-    ts := make(map[byte]byte)
     for i := range s {
         if v, ok := st[s[i]]; ok && v != t[i]{
             return false
         } else {
             st[s[i]] = t[i]
         }
-        if v, ok := ts[t[i]]; ok && v != s[i] {
-            return false
-        } else {
-            ts[t[i]] = s[i]
-        }
     }
     return true
 }
-
