046ec03631e7b3a607d8e14093b1c72be3a49694
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-22 13:48:05 -0800

    Update 228_summary_ranges.go
    * Big O (n) time
    * index the end of range

--- go/easy/228_summary_ranges.go
+++ go/easy/228_summary_ranges.go
@@ -1,22 +1,17 @@
 func summaryRanges(nums []int) []string {
-    ranges := []string{}
-    rng := ""
-    l, r := 0, 0
-    for i := 0; i < len(nums); i++ {
-        // range start
-        if rng == "" {
-            l = nums[i]
-            rng = rng + strconv.Itoa(l)
+    start, end := 0, 0
+	res := []string{}
+
+	for i := 0; i < len(nums); i++ {
+		end = i
+        if i == len(nums) - 1 || nums[i+1]-nums[i] != 1 {
+			rng := strconv.Itoa(nums[start])
+			if start != end {
+				rng += "->" + strconv.Itoa(nums[end])
+			}
+			res = append(res, rng)
+			start = i + 1
         }
-        // range end
-        if i == len(nums) - 1 || nums[i] + 1 != nums[i+1] {
-            r = nums[i]
-            if l != r {
-                rng = strconv.Itoa(l) + "->" + strconv.Itoa(r)
-            }
-            ranges = append(ranges, rng)
-            rng = ""
-        }
-    }
-    return ranges
+	}
+	return res
 }
