8de3c07ced6b46a2937fea34bc701f6ee9456db2
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-21 19:09:47 -0800

    Update 242_valid_anagram.go
    * redo for practice

--- go/easy/242_valid_anagram.go
+++ go/easy/242_valid_anagram.go
@@ -1,18 +1,19 @@
-package easy
-
 func isAnagram(s string, t string) bool {
     if len(s) != len(t) {
         return false
     }
-    cf := [26]int{}
-    for i := 0; i < len(s); i++ {
-        cf[s[i]-'a']++
-        cf[t[i]-'a']--
+
+    counts := make([]int, 26)
+    for i := range s {
+        counts[s[i] - 'a']++
+        counts[t[i] - 'a']--
     }
-    for _, v := range cf {
+
+    for _, v := range counts {
         if v != 0 {
             return false
         }
     }
+
     return true
 }
