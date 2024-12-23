ee985bae1b02f36827899d4c8c3b9fac027722d0
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-23 15:44:52 -0800

    Update 155_min_stack.go
    * refactor 
    * use nodes to store val and minVal
    * simplify pop operation

--- go/medium/155_min_stack.go
+++ go/medium/155_min_stack.go
@@ -1,8 +1,11 @@
 type MinStack struct {
-    vs []int
-    ms []int
+    stack []node
 }
 
+type node struct {
+    val int
+    minVal int
+}
 
 func Constructor() MinStack {
     return MinStack{}
@@ -10,29 +13,27 @@ func Constructor() MinStack {
 
 
 func (this *MinStack) Push(val int)  {
-    this.vs = append(this.vs, val)
-
-    if len(this.ms) == 0 || val <= this.ms[len(this.ms) - 1] {
-        this.ms = append(this.ms, val)
+    newNode := node{val:val, minVal:val}
+    
+    if len(this.stack) > 0 {
+        newNode.minVal = min(val, this.stack[len(this.stack)-1].minVal)
     }
+    this.stack = append(this.stack, newNode)
 }
 
 
 func (this *MinStack) Pop()  {
-    if this.vs[len(this.vs)-1] == this.ms[len(this.ms)-1] {
-        this.ms = this.ms[:len(this.ms)-1]
-    }
-    this.vs = this.vs[:len(this.vs)-1]
+    this.stack = this.stack[:len(this.stack)-1]
 }
 
 
 func (this *MinStack) Top() int {
-    return this.vs[len(this.vs)-1]
+    return this.stack[len(this.stack)-1].val
 }
 
 
 func (this *MinStack) GetMin() int {
-    return this.ms[len(this.ms)-1]
+    return this.stack[len(this.stack)-1].minVal
 }
 
 
