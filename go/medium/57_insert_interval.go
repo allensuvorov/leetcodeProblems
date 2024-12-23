6d81712a1bac928491e771ca4f94d1baa06d3c63
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-22 19:36:35 -0800

    Update 57_insert_interval.go
    * redo for practice

--- go/medium/57_insert_interval.go
+++ go/medium/57_insert_interval.go
@@ -1,29 +1,29 @@
 func insert(intervals [][]int, newInterval []int) [][]int {
-    n := len(intervals)
+    n := len(intervals) 
+    res := [][]int{}
     i := 0
-    res := make([][]int, 0)
-        
-    for i < n && intervals[i][1] < newInterval[0] {
+
+    // Case 1: No overlappint before merging intervals
+    for i < n && intervals[i][1] < newInterval[0]{
         res = append(res, intervals[i])
         i++
-    } 
-    
+    }
+
+    // Case 2: Overlapping and merging intervals
+
     for i < n && intervals[i][0] <= newInterval[1] {
-        newInterval[0] = min(newInterval[0], intervals[i][0])
-        newInterval[1] = max(newInterval[1], intervals[i][1])
+        newInterval[0] = min(intervals[i][0], newInterval[0]) 
+        newInterval[1] = max(intervals[i][1], newInterval[1])
         i++
     }
 
     res = append(res, newInterval)
-        
-    for i < n && intervals[i][0] > newInterval[1] {
+
+    // Case 3: No overlappint after merging intervals
+    for i < n {
         res = append(res, intervals[i])
         i++
     }
+    
     return res
 }
-
-
-// 1---3   6---9
-//   2---5
-// 1-----5
