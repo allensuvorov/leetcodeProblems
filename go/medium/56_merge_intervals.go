f9f31ee8542aed13eca156fff3a1968c1053d060
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-22 17:58:42 -0800

    Update 56_merge_intervals.go
    * redo for practice

--- go/medium/56_merge_intervals.go
+++ go/medium/56_merge_intervals.go
@@ -1,14 +1,16 @@
 func merge(intervals [][]int) [][]int {
-   sort.Slice(intervals, func(i, j int) bool { return intervals[i][0] < intervals[j][0] })
-   merged := [][]int{}
+    slices.SortFunc(intervals, func(a, b []int) int {
+        return a[0] - b[0]
+    })
 
-   for _, interval := range intervals {
-       last := len(merged) - 1
-       if len(merged) == 0 || merged[last][1] < interval[0] {
-           merged = append(merged, interval)
-       } else {
-           merged[last][1] = max(merged[last][1], interval[1])
-       }
-   }
-   return merged
+    merged := make([][]int, 0)
+    for _, interval := range intervals {
+        last := len(merged) - 1
+        if len(merged) == 0 || merged[last][1] < interval[0] {
+            merged = append(merged, interval)
+        } else {
+            merged[last][1] = max(merged[last][1], interval[1])
+        }
+    }
+    return merged
 }
