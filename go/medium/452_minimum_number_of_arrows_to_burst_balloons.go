fbf4af95accdf76dfb8897658692ea094ee98ef2
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-22 20:06:07 -0800

    Update 452_minimum_number_of_arrows_to_burst_balloons.go
    * redo for practice

--- go/medium/452_minimum_number_of_arrows_to_burst_balloons.go
+++ go/medium/452_minimum_number_of_arrows_to_burst_balloons.go
@@ -1,11 +1,11 @@
 func findMinArrowShots(points [][]int) int {
     slices.SortFunc(points, func(a, b []int) int {
-        return a[1]-b[1] 
+        return a[1] - b[1]
     })
-    lastEnd := math.MinInt
-    arrowCount := 0
+    arrowCount := 1
+    lastEnd := points[0][1]
     for _, v := range points {
-        if lastEnd < v[0] {
+        if v[0] > lastEnd {
             arrowCount++
             lastEnd = v[1]
         }
