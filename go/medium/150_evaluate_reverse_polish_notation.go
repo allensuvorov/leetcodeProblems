fffcdff8651a2e55c7feb209e209f2222dad7f33
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-23 16:27:50 -0800

    Update 150_evaluate_reverse_polish_notation.go
    * redo for practice

--- go/medium/150_evaluate_reverse_polish_notation.go
+++ go/medium/150_evaluate_reverse_polish_notation.go
@@ -1,24 +1,37 @@
 func evalRPN(tokens []string) int {
+    ops := map[string]func(int, int)int{
+        "+": func(a, b int) int{
+            return a + b
+        },
+        "-": func(a, b int) int{
+            return a - b
+        },
+        "*": func(a, b int) int{
+            return a * b
+        },
+        "/": func(a, b int) int{
+            if b == 0 {
+                return -1
+            }
+            return a / b
+        },
+    }
+
     stack := []int{}
     for _, v := range tokens {
-        top := len(stack) - 1
-        if _, ok := ops[v]; ok {
-            stack[top-1] = ops[v](stack[top-1], stack[top])
-            stack = stack[:top]
-        } else {
-            num, err := strconv.Atoi(v)
-            if err != nil {
+        if _, ok := ops[v]; !ok {
+            if num, err := strconv.Atoi(v); err != nil {
                 fmt.Println(err)
+            } else {
+                stack = append(stack, num)
             }
-            stack = append(stack, num)
+        } else {
+            top := len(stack) - 1
+            b := stack[top]
+            a := stack[top - 1]
+            stack = stack[:top]
+            stack[len(stack) - 1] = ops[v](a, b)
         }
     }
     return stack[0]
-}
-
-var ops = map[string]func(int, int) int {
-    "*": func(a, b int) int { return a * b },
-    "/": func(a, b int) int { return a / b },
-    "+": func(a, b int) int { return a + b },
-    "-": func(a, b int) int { return a - b },
 }
