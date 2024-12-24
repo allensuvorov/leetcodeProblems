a3c7ea59e409888178de2d3dcc50d951cfc1e08d
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-24 15:48:35 -0800

    Update 1071_greatest_common_divisor_of_strings.go
    * tc: Big O(m+n) 
    * sc: Big O(m+n)

--- go/easy/1071_greatest_common_divisor_of_strings.go
+++ go/easy/1071_greatest_common_divisor_of_strings.go
@@ -1,27 +1,12 @@
 func gcdOfStrings(str1 string, str2 string) string {
-    res := ""
-    for i := 0; i < len(str1) && i < len(str2); i++ {
-        if str1[:i+1] == str2[:i+1] {
-            pref := str1[:i+1]
-            if len(str1) % len(pref) == 0 && len(str2) % len(pref) == 0 {
-                isDevisor := true
-                for i := 0; i <= len(str1) - len(pref); i += len(pref) {
-                    if str1[i:i+len(pref)] != pref {
-                        isDevisor = false
-                        break
-                    }
-                }
-                for i := 0; i <= len(str2) - len(pref); i += len(pref) {
-                    if str2[i:i+len(pref)] != pref {
-                        isDevisor = false
-                        break
-                    }
-                }
-                if isDevisor {
-                    res = pref
-                }
-            }
+    if str1 + str2 != str2 + str1 {
+        return ""
+    }
+    maxLen := 0
+    for i := range min(len(str1), len(str2)) {
+        if len(str1) % (i+1) == 0 && len(str2) % (i+1) == 0 {
+            maxLen = i + 1
         }
     }
-    return res
+    return str1[:maxLen]
 }
