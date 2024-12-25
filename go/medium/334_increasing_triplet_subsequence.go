82588fc82d32aa10a1fae55fad773d8f1f67d074
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-25 13:53:19 -0800

    Update 334_increasing_triplet_subsequence.go
    * this one works - min1, min2, min3

--- go/medium/334_increasing_triplet_subsequence.go
+++ go/medium/334_increasing_triplet_subsequence.go
@@ -1,36 +1,13 @@
-// two mins
 func increasingTriplet(nums []int) bool {
-    min1 := math.MaxInt
-    min2 := math.MaxInt
-
+    min1, min2 := math.MaxInt, math.MaxInt
     for _, v := range nums {
         if v <= min1 {
-            min1 = v
+            min1 = v 
         } else if v <= min2 {
             min2 = v
         } else {
             return true
         }
-    }
-    return false
-}
-
-// min and max
-func increasingTriplet(nums []int) bool {
-    l, r := 0, len(nums) - 1
-    minSoFar := nums[0]
-    maxSoFar := nums[len(nums) - 1]
-    for l < r {
-        if minSoFar < nums[l] && nums[l] < maxSoFar {
-            return true
-        }
-        if minSoFar < nums[r] && nums[r] < maxSoFar {
-            return true
-        }
-        minSoFar = min(minSoFar, nums[l])
-        maxSoFar = max(maxSoFar, nums[r])
-        l++
-        r--
     }
     return false
 }
