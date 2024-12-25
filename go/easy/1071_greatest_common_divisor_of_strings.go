cfa44042f71a137a6c1e65763e0caf769bad9b92
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-24 16:02:43 -0800

    Update 1071_greatest_common_divisor_of_strings.go
    * extract gcd() function

--- go/easy/1071_greatest_common_divisor_of_strings.go
+++ go/easy/1071_greatest_common_divisor_of_strings.go
@@ -2,11 +2,13 @@ func gcdOfStrings(str1 string, str2 string) string {
     if str1 + str2 != str2 + str1 {
         return ""
     }
-    maxLen := 0
-    for i := range min(len(str1), len(str2)) {
-        if len(str1) % (i+1) == 0 && len(str2) % (i+1) == 0 {
-            maxLen = i + 1
-        }
-    }
+    maxLen := gcd(len(str1), len(str2))
     return str1[:maxLen]
+}
+
+func gcd(x, y int) int {
+    for y != 0 {
+        x, y = y, x%y
+    }
+    return x
 }
