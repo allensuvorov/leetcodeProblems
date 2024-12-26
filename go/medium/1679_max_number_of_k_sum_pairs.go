cad2e203a0506a83cdcf11c046c321b35cc9367d
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-25 20:47:13 -0800

    Update 1679_max_number_of_k_sum_pairs.go
    * redo for practice

--- go/medium/1679_max_number_of_k_sum_pairs.go
+++ go/medium/1679_max_number_of_k_sum_pairs.go
@@ -12,3 +12,25 @@ func maxOperations(nums []int, k int) int {
     }
     return res
 }
+
+// practice 2
+func maxOperations(nums []int, k int) int {
+    counts := map[int]int{}
+    for _, v := range nums {
+        counts[v]++
+    }
+    res := 0
+    for num, count := range counts {
+        numOps := 0
+        if num * 2 == k {
+            numOps = count/2
+            counts[num] = 0
+        } else {
+            numOps = min(count, counts[k-num])
+            counts[num] = 0
+            counts[k-num] = 0
+        }
+        res += numOps
+    }
+    return res
+}
