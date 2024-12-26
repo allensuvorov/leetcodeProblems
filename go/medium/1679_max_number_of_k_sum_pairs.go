3acaea0721b0c6c455137167e64c7012158ed511
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-25 20:51:03 -0800

    Update 1679_max_number_of_k_sum_pairs.go
    * hash map of pair nums

--- go/medium/1679_max_number_of_k_sum_pairs.go
+++ go/medium/1679_max_number_of_k_sum_pairs.go
@@ -1,36 +1,13 @@
 func maxOperations(nums []int, k int) int {
     counts := map[int]int{}
     res := 0
-    for _, num := range nums {   
-        diff := k - num
-        if counts[diff] > 0 {
-            res++
-            counts[diff]--
-        } else {
-            counts[num]++
-        }
-    }
-    return res
-}
-
-// practice 2
-func maxOperations(nums []int, k int) int {
-    counts := map[int]int{}
     for _, v := range nums {
-        counts[v]++
-    }
-    res := 0
-    for num, count := range counts {
-        numOps := 0
-        if num * 2 == k {
-            numOps = count/2
-            counts[num] = 0
+        if counts[k-v] > 0 {
+            res++
+            counts[k-v]--
         } else {
-            numOps = min(count, counts[k-num])
-            counts[num] = 0
-            counts[k-num] = 0
+            counts[v]++
         }
-        res += numOps
     }
     return res
 }
