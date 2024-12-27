c2d6f2083141cf210192c64bc2d561e88c38a64c
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-26 23:33:29 -0800

    Update 1456_maximum_number_of_vowels_in_a_substring_of_given_length.go

--- go/medium/1456_maximum_number_of_vowels_in_a_substring_of_given_length.go
+++ go/medium/1456_maximum_number_of_vowels_in_a_substring_of_given_length.go
@@ -1,35 +1,15 @@
 func maxVowels(s string, k int) int {
+    vowels := map[byte]bool{'a':true, 'e':true, 'i':true, 'o':true, 'u':true}
     maxCount := 0
-    count := 0
-    vowels := map[byte]bool{'a':true, 'e':true, 'i':true, 'o':true, 'u':true}
-    for head := 0; head < len(s); head++ {
-        if vowels[s[head]] {
-            count++
-        }
-        if head >= k && vowels[s[head-k]] {
-            count--
-        }
-        maxCount = max(maxCount, count)
-    }
-    return maxCount
-}
-
-// 
-func maxVowels(s string, k int) int {
-    vowels := map[byte]bool{'a':true, 'e':true, 'i':true, 'o':true, 'u':true}
-    res := 0
     count := 0
     for i := range s {
         if vowels[s[i]] {
             count++
         }
-
-        if i >= k - 1 {
-            res = max(res, count)
-            if vowels[s[i-(k-1)]] {
-                count--
-            }
+        if i >= k && vowels[s[i-k]] {
+            count--
         }
+        maxCount = max(maxCount, count)
     }
-    return res
+    return maxCount   
 }
