7cfec3a338fa272c06f2fe190b9b94b6f8bb28f1
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-26 23:31:40 -0800

    Update 1456_maximum_number_of_vowels_in_a_substring_of_given_length.go

--- go/medium/1456_maximum_number_of_vowels_in_a_substring_of_given_length.go
+++ go/medium/1456_maximum_number_of_vowels_in_a_substring_of_given_length.go
@@ -13,3 +13,23 @@ func maxVowels(s string, k int) int {
     }
     return maxCount
 }
+
+// 
+func maxVowels(s string, k int) int {
+    vowels := map[byte]bool{'a':true, 'e':true, 'i':true, 'o':true, 'u':true}
+    res := 0
+    count := 0
+    for i := range s {
+        if vowels[s[i]] {
+            count++
+        }
+
+        if i >= k - 1 {
+            res = max(res, count)
+            if vowels[s[i-(k-1)]] {
+                count--
+            }
+        }
+    }
+    return res
+}
