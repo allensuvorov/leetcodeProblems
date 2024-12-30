eaac75c77ae3a78bafbfbd5ad1136a6945696222
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2024-12-29 19:58:38 -0800

    Update 2390_removing_stars_from_a_string.go
    * stack
    * push and pop

--- go/medium/2390_removing_stars_from_a_string.go
+++ go/medium/2390_removing_stars_from_a_string.go
@@ -1,33 +1,11 @@
 func removeStars(s string) string {
-    stack := make([]byte, 0, len(s))
-    for i := range s {
-        if s[i] == '*' {
-            stack = stack[:len(stack) - 1] // pop
+    stack := []rune{}
+    for _, v := range s {
+        if v != '*' {
+            stack = append(stack, v)
         } else {
-            stack = append(stack, s[i]) // push
+            stack = stack[:len(stack)-1]
         }
     }
     return string(stack)
-}
-
-func removeStars(s string) string {
-    // reverse solution
-    rev := make([]byte, 0, len(s))
-    count := 0
-    for i := len(s) - 1; i >= 0; i-- {
-        if s[i] == '*' {
-            count++
-        } else {
-            if count > 0 {
-                count --
-            } else {
-                rev = append(rev, s[i])
-            }
-        }
-    }
-    res := make([]byte, 0, len(rev))
-    for i := len(rev) - 1; i >= 0; i-- {
-        res = append(res, rev[i])
-    }
-    return string(res)
 }
