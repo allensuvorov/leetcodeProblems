53517088ce98672867d1914b79d3fd6c51eab242
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-03 15:30:18 -0800

    Update 1372_longest_zigzag_path_in_a_binary_tree.go
    * redo for practice

--- go/medium/1372_longest_zigzag_path_in_a_binary_tree.go
+++ go/medium/1372_longest_zigzag_path_in_a_binary_tree.go
@@ -8,21 +8,16 @@
  */
 func longestZigZag(root *TreeNode) int {
     res := 0
-    var dfs func(node *TreeNode, zig bool) int
-    dfs = func(node *TreeNode, zig bool) int {
-        if node == nil {
-            return 0
+    var dfs func(root *TreeNode, l, r int)
+    dfs = func(root *TreeNode, l, r int) {
+        if root == nil {
+            return
         }
-        left := dfs(node.Left, false)
-        right := dfs(node.Right, true)
-        if zig {
-            res = max(res, right, left + 1)
-            return left + 1
-        }
-        res = max(res, left, right + 1)
-        return right + 1
+        res = max(res, l, r)
+        dfs(root.Left, r+1, 0)
+        dfs(root.Right, 0, l+1)
     }
-    dfs(root.Left, false)
-    dfs(root.Right, true)
-    return res 
+
+    dfs(root, 0, 0)
+    return res
 }
