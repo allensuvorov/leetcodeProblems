a404633eaf9b8ca2f3111044e13c0e411f3a5d64
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-03 13:31:38 -0800

    Update 1448_count_good_nodes_in_binary_tree.go

--- go/medium/1448_count_good_nodes_in_binary_tree.go
+++ go/medium/1448_count_good_nodes_in_binary_tree.go
@@ -21,3 +21,24 @@ func dfs(node *TreeNode, max int) int {
     }
     return good + dfs(node.Left, max) + dfs(node.Right, max)
 }
+
+// redo 3 Jan 2025
+func goodNodes(root *TreeNode) int {
+    res := 0
+    var dfs func(root *TreeNode, greatest int)
+    dfs = func(root *TreeNode, greatest int) {
+        if root == nil {
+            return
+        }
+        if root.Val >= greatest {
+            res++
+            greatest = root.Val
+        }
+        
+        dfs(root.Left, greatest)
+        dfs(root.Right, greatest)
+    }
+
+    dfs(root, -1e4)
+    return res
+}
