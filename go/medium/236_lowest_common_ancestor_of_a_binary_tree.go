943fabb5ed4958c38d2289b305d7c5b986fd2845
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-03 16:29:37 -0800

    Update 236_lowest_common_ancestor_of_a_binary_tree.go
    * redo

--- go/medium/236_lowest_common_ancestor_of_a_binary_tree.go
+++ go/medium/236_lowest_common_ancestor_of_a_binary_tree.go
@@ -6,48 +6,24 @@
  *     Right *TreeNode
  * }
  */
- func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-    if root == nil {
-        return nil
-    }    
-    if root == p || root == q {
-        return root
-    }
-    l := lowestCommonAncestor(root.Left, p, q)
-    r := lowestCommonAncestor(root.Right, p, q)
-    if l != nil && r != nil {
-        return root
-    }
-    if l != nil {
-        return l
-    }
-    return r
-}
 
-// redo
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
     if root == nil {
         return nil
     }
 
+    if root == p || root == q {
+        return root
+    }
+
     l := lowestCommonAncestor(root.Left, p, q)
     r := lowestCommonAncestor(root.Right, p, q)
 
-    switch { 
-    case l == p && r == q, l == q && r == p:
+    switch {
+    case l != nil && r != nil: // lca case
         return root
-    case root == p && (l == q || r == q):
-        return root
-    case root == q && (l == p || r == p):
-        return root
-    case root == p, l == p, r == p: 
-        return p
-    case root == q, l == q, r == q:
-        return q
     case l != nil:
         return l
-    case r != nil:
-        return r
     }
-    return nil
+    return r
 }
