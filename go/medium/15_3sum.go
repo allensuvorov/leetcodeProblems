b0e0af2a25a67966df21e23cc2c977b7e14d8152
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-04 14:52:49 -0800

    Update 15_3sum.go
    - redo

--- go/medium/15_3sum.go
+++ go/medium/15_3sum.go
@@ -1,28 +1,27 @@
 func threeSum(nums []int) [][]int {
-    res := make([][]int,0)
-    slices.Sort(nums)
+	n := len(nums)
+	slices.Sort(nums)
+	tripletSet := map[[3]int]bool{}
+    triplets := [][]int{}
 
-    for i := 0; i < len(nums) && nums[i] <= 0; i++ {
-        if i > 0 && nums[i] == nums[i-1] {
-            continue
-        }
-
-        l, r := i + 1, len(nums) - 1
-        for l < r {
-            sum := nums[i] + nums[l] + nums[r]
-            switch{
-            case sum > 0:
-                r--
-            case sum < 0:
-                l++
-            default:
-                res = append(res, []int{nums[i], nums[l], nums[r]})
-                l++
-                for nums[l] == nums[l-1] && l < r {
-                    l++
-                }
-            }
-        }
+	for i := range nums {
+		j := i + 1
+		k := len(nums) - 1
+		for j < k && j < n-1 && k > i {
+			sum := nums[i] + nums[j] + nums[k]
+			if sum > 0 {
+				k--
+			} else if sum < 0 {
+				j++
+			} else {
+				tripletSet[[3]int{nums[i], nums[j], nums[k]}] = true
+				j++
+                k--
+			}
+		}
+	}
+    for triplet := range tripletSet{
+        triplets = append(triplets, triplet[:])
     }
-    return res
+	return triplets
 }
