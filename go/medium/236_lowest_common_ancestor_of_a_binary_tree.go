b102f7862d037c8272a3dff9bee30d3f6c066916
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-03 16:22:06 -0800

    Update 236_lowest_common_ancestor_of_a_binary_tree.go
    * redo

--- go/medium/236_lowest_common_ancestor_of_a_binary_tree.go
+++ go/medium/236_lowest_common_ancestor_of_a_binary_tree.go
@@ -23,3 +23,39 @@
     }
     return r
 }
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+    if root == nil {
+        return nil
+    }
+
+    l := lowestCommonAncestor(root.Left, p, q)
+    r := lowestCommonAncestor(root.Right, p, q)
+
+    switch { 
+    case l == p && r == q, l == q && r == p:
+        return root
+    case root == p && (l == q || r == q):
+        return root
+    case root == q && (l == p || r == p):
+        return root
+    case root == p, l == p, r == p: 
+        return p
+    case root == q, l == q, r == q:
+        return q
+    case l != nil:
+        return l
+    case r != nil:
+        return r
+    }
+    return nil
+}
