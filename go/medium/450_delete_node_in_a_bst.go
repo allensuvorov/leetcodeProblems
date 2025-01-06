99f2963b81456893762c6caa4e7c4734685780b4
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-06 15:18:25 -0800

    Update 450_delete_node_in_a_bst.go

--- go/medium/450_delete_node_in_a_bst.go
+++ go/medium/450_delete_node_in_a_bst.go
@@ -10,24 +10,23 @@ func deleteNode(root *TreeNode, key int) *TreeNode {
     if root == nil {
         return root
     }
-
-    if key > root.Val {
-        root.Right = deleteNode(root.Right, key)
-    } else if key < root.Val {
+    // search
+    if key < root.Val {
         root.Left = deleteNode(root.Left, key)
-    } else {
+    } else if key > root.Val {
+        root.Right = deleteNode(root.Right, key)
+    } else { // delete
         if root.Left == nil {
             return root.Right
         } else if root.Right == nil {
             return root.Left
         }
-
         cur := root.Right
         for cur.Left != nil {
             cur = cur.Left
         }
         root.Val = cur.Val
-        root.Right = deleteNode(root.Right, root.Val)
+        root.Right = deleteNode(root.Right, root.Val) // recursive delete
     }
     return root
 }
