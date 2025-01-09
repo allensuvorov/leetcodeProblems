af56054c45ce67f3958ca0193b79d2a4f1140d5b
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-08 19:33:32 -0800

    Update 17_letter_combinations_of_a_phone_number.go
    * redo

--- go/medium/17_letter_combinations_of_a_phone_number.go
+++ go/medium/17_letter_combinations_of_a_phone_number.go
@@ -16,3 +16,33 @@ func letterCombinations(digits string) []string {
     }
     return res
 }
+
+// redo
+func letterCombinations(digits string) []string {
+    if len(digits) == 0 {
+        return nil
+    }
+    numLetters := map[byte]string{
+        '2': "abc",
+        '3': "def",
+        '4': "ghi",
+        '5': "jkl",
+        '6': "mno",
+        '7': "pqrs",
+        '8': "tuv",
+        '9': "wxyz",
+    }
+    res := []string{}
+    var dfs func(digits, letters string)
+    dfs = func(digits, letters string){
+        if len(digits) == 0 {
+            res = append(res, letters)
+            return
+        }
+        for _, letter := range numLetters[digits[0]] {
+            dfs(digits[1:], letters + string(letter))
+        }
+    }
+    dfs(digits,"")
+    return res
+}
