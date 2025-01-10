d9be1cf4ab09992a7885262e6df29cfcf8842211
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-09 19:01:23 -0800

    Update 1466_reorder_routes_to_make_all_paths_lead_to_the_city_zero.go

--- go/medium/1466_reorder_routes_to_make_all_paths_lead_to_the_city_zero.go
+++ go/medium/1466_reorder_routes_to_make_all_paths_lead_to_the_city_zero.go
@@ -1,10 +1,10 @@
 func minReorder(n int, connections [][]int) int {
-    origins := make(map[int]map[int]int, n)
+    origins := make([]map[int]int, n)
     for i, v := range connections {
-        if _, ok := origins[v[0]]; !ok {
+        if origins[v[0]] == nil {
             origins[v[0]] = make(map[int]int)
         }
-        if _, ok := origins[v[1]]; !ok {
+        if origins[v[1]] == nil {
             origins[v[1]] = make(map[int]int)
         }
         origins[v[0]][v[1]] = i
@@ -15,15 +15,14 @@ func minReorder(n int, connections [][]int) int {
 
     var dfs func(destination int)
     dfs = func(destination int) {
-        if visited[destination] {
-            return
-        }
         visited[destination] = true
         for origin, i := range origins[destination] {
-            if !visited[origin] && origin != connections[i][0] {
-                reorderCount++
+            if !visited[origin] {
+                if origin != connections[i][0] {
+                    reorderCount++
+                }
+                dfs(origin)
             }
-            dfs(origin)
         }
     }
     dfs(0)
