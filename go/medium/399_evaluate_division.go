0465f7b9a0f524039762e2b35ad0fad9ff90b5f4
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-10 17:44:16 -0800

    Update 399_evaluate_division.go
    * redo

--- go/medium/399_evaluate_division.go
+++ go/medium/399_evaluate_division.go
@@ -1,41 +1,36 @@
 func calcEquation(equations [][]string, values []float64, queries [][]string) []float64 {
-    adj := map[string][]int{}
-
-    for i := range equations {
-        adj[equations[i][0]] = append(adj[equations[i][0]],i)
-        adj[equations[i][1]] = append(adj[equations[i][1]],i)
+    adj := make(map[string][]int)
+    for i, v := range equations {
+        adj[v[0]] = append(adj[v[0]], i)
+        adj[v[1]] = append(adj[v[1]], i)
     }
 
-    res := make([]float64, len(queries))
     visited := make([]bool, len(equations))
-
     var dfs func(a, b string) float64
-    dfs = func(a, b string) float64{
+    dfs = func(a, b string) float64 {
         if a == b {
             return 1.0
         }
-
-        for _, v := range adj[a] {
-            if !visited[v] {
-                visited[v] = true
-                if a == equations[v][0] {
-                    res := dfs(equations[v][1], b) * values[v]
-                    if res > 0 {
-                        return res
-                    }
+        for _, i := range adj[a] {
+            if !visited[i] {
+                visited[i] = true
+                res := 1.0
+                if a == equations[i][0] {
+                    res = dfs(equations[i][1], b) * values[i]
                 } else {
-                    res := dfs(equations[v][0], b) / values[v]
-                    if res > 0 {
-                        return res
-                    }
+                    res = dfs(equations[i][0], b) / values[i]
+                }
+                if res > 0 {
+                    return res
                 }
             }
-        }
+        } 
         return -1.0
     }
 
-    for i, v:= range queries{
-        if adj[v[0]] == nil || adj[v[1]] == nil {
+    res := make([]float64, len(queries))
+    for i, v := range queries {
+        if adj[v[0]] == nil || adj[v[1]] == nil{
             res[i] = -1.0
         } else {
             visited = make([]bool, len(equations))
