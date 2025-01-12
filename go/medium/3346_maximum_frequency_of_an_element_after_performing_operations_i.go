d3ac1fa33ece021bec6b970741ecdc421ec196ba
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-11 18:46:37 -0800

    Create 3346_maximum_frequency_of_an_element_after_performing_operations_i.go
    * sorting
    * two pointers

---
+++ go/medium/3346_maximum_frequency_of_an_element_after_performing_operations_i.go
@@ -0,0 +1,34 @@
+func maxFrequency(nums []int, k int, numOperations int) int {
+    slices.Sort(nums) // sorting
+    counts := make(map[int]int) // frequency map
+    for _, v := range nums {
+        counts[v]++
+    }
+    // some unchanged
+    res := 0
+    
+    for i, l, r := 0, 0, 0; i < len(nums); i++ {
+        // keep left pointer within nums[i] - k
+        for l <= i && nums[l] < nums[i] - k {
+            l++
+        }
+        // keep right pointer within nums[i] + k
+        for r < len(nums) && nums[r] <= nums[i] + k{
+            spread := r - l + 1
+            res = max(res, min(spread, counts[nums[i]] + numOperations))
+            r++
+        }
+        r--
+    }
+    // change everything
+    for l, r := 0, 0; r < len(nums); r++ {
+        for l <= r && nums[l] < nums[r] - 2 * k {
+            l++
+        }
+        spread := r - l + 1
+        if spread <= numOperations {
+            res = max(res, spread)
+        }
+    }
+    return res
+}
