9e07f48d9c440e81e5c4c8fc23569b0e0584b822
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-14 11:21:12 -0800

    Update 2300_successful_pairs_of_spells_and_potions.go
    * all in one function

--- go/medium/2300_successful_pairs_of_spells_and_potions.go
+++ go/medium/2300_successful_pairs_of_spells_and_potions.go
@@ -2,21 +2,17 @@ func successfulPairs(spells []int, potions []int, success int64) []int {
     slices.Sort(potions)
     res := make([]int, len(spells))
     for i, spell := range spells {
-        res[i] = len(potions) - binarySearch(potions, spell, success)
+        l, r := 0, len(potions) - 1
+        for l <= r {
+            m := l + (r-l)/2
+            guess := int64(spell * potions[m])
+            if guess >= success {
+                r = m - 1
+            } else {
+                l = m + 1
+            }
+        }
+        res[i] = len(potions) - l
     }
     return res
-}
-
-func binarySearch(potions []int, spell int, success int64) int {
-    l, r := 0, len(potions) - 1
-    for l <= r {
-        m := l + (r-l)/2
-        guess := int64(spell * potions[m])
-        if guess >= success {
-            r = m - 1
-        } else {
-            l = m + 1
-        }
-    }
-    return l
 }
