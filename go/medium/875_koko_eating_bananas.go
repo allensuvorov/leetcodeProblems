08cf2508a103b4e975869446e1a4eb16e8ad3c79
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-14 12:46:27 -0800

    Update 875_koko_eating_bananas.go
    * redo

--- go/medium/875_koko_eating_bananas.go
+++ go/medium/875_koko_eating_bananas.go
@@ -1,26 +1,34 @@
 func minEatingSpeed(piles []int, h int) int {
-    l := 1
-    r := 1_000_000_000
-    for l <= r {
-        m := l + (r - l)/2
-        time := eatTime(piles, m)
-        if time <= h {
-            r = m-1
+    l, r := 1, 1_000_000_000
+    for l <= r { 
+        m := l + (r-l)/2
+        t := getEatingTime(piles, m) 
+        if t <= h {
+            r = m - 1 // slow down
         } else {
-            l = m+1
+            l = m + 1 // speed up
         }
     }
     return l
 }
 
-
-func eatTime(piles []int, k int) int {
-    hours := 0
+func getEatingTime(piles []int, k int) int {
+    t := 0
     for _, v := range piles {
-        if v % k != 0 {
-            hours++
+        t += v / k
+        if v % k > 0 {
+            t++
         }
-        hours += v / k
     }
-    return hours
+    return t
 }
+
+// n * log n
+    // max time <= h
+    // h = 1,2,3,4,5,6,7,8,9,10
+
+    // k - h               
+    // 3 - 10
+    // 4 - 8
+    // 5 - 8
+    // 6 - 6
