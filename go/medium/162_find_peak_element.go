f62dde0e83af9d6769e96d8a5a58e8f361aec95c
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-14 11:47:54 -0800

    Update 162_find_peak_element.go
    * redo

--- go/medium/162_find_peak_element.go
+++ go/medium/162_find_peak_element.go
@@ -11,3 +11,19 @@ func findPeakElement(nums []int) int {
     }
     return l
 }
+
+func findPeakElement(nums []int) int {
+	n := len(nums)
+	l, r := 0, n-1
+	for l < r {
+		m := l + (r-l)/2
+		if m != n-1 && nums[m] < nums[m+1] {
+			l = m + 1
+		} else if m != 0 && nums[m] < nums[m-1] {
+			r = m - 1
+		} else {
+			return m
+		}
+	}
+	return l
+}
