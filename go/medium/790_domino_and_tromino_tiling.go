9f9843e8c73cc4419f2389396d2c7a5e0d067f7f
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-14 14:29:59 -0800

    Update 790_domino_and_tromino_tiling.go

--- go/medium/790_domino_and_tromino_tiling.go
+++ go/medium/790_domino_and_tromino_tiling.go
@@ -1,13 +1,18 @@
 func numTilings(n int) int {
+    // try to find the pattern
+    // use the pattern
     if n < 3 {
         return n
     }
-
     a, b, c := 1, 2, 5
-
     for range n - 3 {
-        a, b, c = b, c, (c*2 + a)
-        c %= (1e9 + 7)
+        a, b, c = b, c, 2*c + a
+        c %= 1e9 + 7
     }
     return c
 }
+// 1 - 1
+// 2 - 2
+// 3 - 5
+// 4 - 11 (2*5 + 1)
+// 5 - 24 (2*11 + 2)
