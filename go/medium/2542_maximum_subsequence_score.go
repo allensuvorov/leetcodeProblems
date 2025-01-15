c64368209ea4e3c5ba7de2e712102d8d8b56d4a5
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-15 13:36:22 -0800

    Update 2542_maximum_subsequence_score.go
    * redo

--- go/medium/2542_maximum_subsequence_score.go
+++ go/medium/2542_maximum_subsequence_score.go
@@ -1,50 +1,44 @@
-import "container/heap"
+import (
+	"container/heap"
+)
 
 func maxScore(nums1 []int, nums2 []int, k int) int64 {
-    pairs := make([][]int, len(nums1))
+    n := len(nums1)
+    pairs := make([][]int, n)
     for i := range pairs {
         pairs[i] = []int{nums1[i], nums2[i]}
     }
 
-    slices.SortFunc(pairs, func(a, b []int)int {
-        return b[1] - a[1]
-    })
-    
-    ans := 0
-    sum := 0
-    h := &IntHeap{}
+    slices.SortFunc(pairs, func(a, b []int) int { return b[1] - a[1]} )
+
+    h := &minHeap{}
     heap.Init(h)
-
+    var sum, res int64
     for _, v := range pairs {
-        
-        sum += v[0]
         heap.Push(h, v[0])
-
-        if len(*h) > k {
-            sum -= heap.Pop(h).(int)
+        sum += int64(v[0])
+        if h.Len() > k {
+            sum -= int64(heap.Pop(h).(int))
         }
 
-        if len(*h) == k {
-            ans = max(ans, sum * v[1])
+        if h.Len() == k {
+            res = max(res, sum * int64(v[1]))
         }
     }
-    return int64(ans)
+    return res
 }
 
-// An IntHeap is a min-heap of ints.
-type IntHeap []int
+type minHeap []int
 
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
-func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h minHeap) Len() int           { return len(h) }
+func (h minHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h minHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 
-func (h *IntHeap) Push(x any) {
-	// Push and Pop use pointer receivers because they modify the slice's length,
-	// not just its contents.
+func (h *minHeap) Push(x any) {
 	*h = append(*h, x.(int))
 }
 
-func (h *IntHeap) Pop() any {
+func (h *minHeap) Pop() any {
 	old := *h
 	n := len(old)
 	x := old[n-1]
