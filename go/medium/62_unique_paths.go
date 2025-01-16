77a25461d60a4e64c54033d1da2cacfbaebc522e
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-16 11:28:40 -0800

    Update 62_unique_paths.go
    * redo

--- go/medium/62_unique_paths.go
+++ go/medium/62_unique_paths.go
@@ -1,13 +1,10 @@
 func uniquePaths(m int, n int) int {
-    dp := make([]int, n)
-
-    dp[0] = 1
-
+    row := make([]int, n)
+    row[0] = 1
     for range m {
         for c := 1; c < n; c++ {
-            dp[c] += dp[c-1]
+            row[c] += row[c-1]
         }
     }
-
-    return dp[len(dp) - 1]
+    return row[len(row)-1]
 }
