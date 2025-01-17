bae0e359e31dac9a1a76c7b51fae2197dff416f8
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-16 16:18:13 -0800

    Update 1143_longest_common_subsequence.go
    * redo

--- go/medium/1143_longest_common_subsequence.go
+++ go/medium/1143_longest_common_subsequence.go
@@ -1,10 +1,10 @@
 func longestCommonSubsequence(text1 string, text2 string) int {
-    dp := make([][]int, len(text2))
-    for r := range text2 {
-        dp[r] = make([]int, len(text1))
-        for c := range text1 {
-            if text2[r] == text1[c] {
-                if c > 0 && r > 0 {
+    dp := make([][]int, len(text1))
+    for r := range text1 {
+        dp[r] = make([]int, len(text2))
+        for c := range text2 {
+            if text1[r] == text2[c] {
+                if r > 0 && c > 0 {
                     dp[r][c] = dp[r-1][c-1] + 1
                 } else {
                     dp[r][c] = 1
@@ -19,8 +19,15 @@ func longestCommonSubsequence(text1 string, text2 string) int {
                     left = dp[r][c-1]
                 }
                 dp[r][c] = max(top, left)
-            }   
+            }
         }
     }
-    return dp[len(dp) - 1][len(dp[0])-1]
+    return dp[len(dp)-1][len(dp[0])-1]
 }
+
+//   t e x t 2
+// t
+// e
+// x
+// t
+// 1
