3ea98822da45f9311f47734da3e96d1b9a61f3df
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-17 16:37:26 -0800

    Update 2683_neighboring_bitwise_xor.go
    * use xorSum or all elements

--- go/medium/2683_neighboring_bitwise_xor.go
+++ go/medium/2683_neighboring_bitwise_xor.go
@@ -1,21 +1,7 @@
 func doesValidArrayExist(derived []int) bool {
-    first := 0
-    curr, next := 0, 0
+    xorSum := 0
     for _, v := range derived {
-        if v == 1 {
-            if curr == 1 {
-                next = 0
-            } else {
-                next = 1
-            }
-        } else {
-            if curr == 1 {
-                next = 1
-            } else {
-                next = 0
-            }
-        }
-        curr = next
+        xorSum ^= v
     }
-    return next == first 
+    return xorSum == 0 
 }
