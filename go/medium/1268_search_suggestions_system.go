aee46b7f607f36b3dcf4f52f785bb50a0fca99e9
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-19 16:59:15 -0800

    Update 1268_search_suggestions_system.go
    * redo

--- go/medium/1268_search_suggestions_system.go
+++ go/medium/1268_search_suggestions_system.go
@@ -1,73 +1,58 @@
+type trie struct {
+    children [26]*trie
+    isWord bool
+}
+
+func (t *trie) insert(word string) {
+    now := t
+    for _, c := range word {
+        if now.children[c - 'a'] == nil {
+            now.children[c - 'a'] = new(trie)
+        }
+        now = now.children[c - 'a']
+    }
+    now.isWord = true
+}
+
+func (t *trie) searchThree(prefix string) []string {
+    res := make([]string, 0)
+    word := []byte(prefix)
+
+    var dfs func(t *trie) 
+    dfs = func(t *trie) {
+        if len(res) == 3 {
+            return
+        }
+        
+        if t.isWord {
+            res = append(res, string(word))
+        }
+        
+        for i, node := range t.children {
+            if node != nil {
+                word = append(word, byte(i + 'a'))
+                dfs(node)
+                word = word[:len(word)-1]
+            } 
+        }
+    }
+    dfs(t)
+    return res
+}
+
 func suggestedProducts(products []string, searchWord string) [][]string {
-    t := newTrie()
-    res := [][]string{}
-
-    for _, p := range products {
-        t.Insert(p)
+    t := new(trie)
+    for _, v := range products {
+        t.insert(v)
     }
 
-    pref := []rune{}
-    for _, c := range searchWord {
-        pref = append(pref, c)
-        res = append(res, t.getWordsStartingWith(pref))
-    }
-    return res
-}
-
-
-type Trie struct {
-    Root *Node
-}
-
+    res := make([][]string, len(searchWord))
     
-type Node struct {
-    Children [26] *Node // array for edges - to sort lexicographically
-    IsWord bool
-}
-
-
-func newTrie() Trie {
-    return Trie{new(Node)}
-}
-
-
-func (this *Trie) Insert(product string) {
-    now := this.Root
-    for _, c := range product {
-        i := c - 'a'
-        if now.Children[i] == nil {
-            now.Children[i] = new(Node)
-        }
-        now = now.Children[i]
+    now := t
+    for i := 0; i < len(searchWord) && now.children[searchWord[i] - 'a'] != nil; i++ {
+        now = now.children[searchWord[i] - 'a']
+        res[i] = now.searchThree(searchWord[:i+1])
     }
-    now.IsWord = true
-}
 
-func (this *Trie) getWordsStartingWith(pref []rune) []string{
-    now := this.Root
-    res := []string{}
-    // Check: are there any words with that prefix?
-    for _, c := range pref {
-        if now.Children[c - 'a'] == nil { 
-            return res
-        }
-        now = now.Children[c - 'a']
-    }
-    this.dfsWithPrefix(now, pref, &res)
     return res
-}
-
-func (this *Trie) dfsWithPrefix(now *Node, pref []rune, result *[]string) {
-    if len(*result) == 3 {
-        return
-    }
-    if now.IsWord {
-        *result = append(*result, string(pref))
-    }
-    for i, node := range now.Children {
-        if node != nil {
-            this.dfsWithPrefix(node, append(pref, rune(i + 'a')), result)
-            
-        }
-    }
 }
