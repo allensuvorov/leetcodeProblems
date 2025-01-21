7693989657e4b957df57e84e68bec1e7b183cf1f
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-21 13:10:20 -0800

    Update 435_non_overlapping_intervals.go
    * redo

--- go/medium/435_non_overlapping_intervals.go
+++ go/medium/435_non_overlapping_intervals.go
@@ -15,3 +15,23 @@ func eraseOverlapIntervals(intervals [][]int) int {
     }
     return res
 }
+
+
+// neetCode version
+func eraseOverlapIntervals(intervals [][]int) int {
+    slices.SortFunc(intervals, func(a, b []int) int {
+        return a[0] - b[0]
+    })
+    res := 0
+    prevEnd := intervals[0][1]
+    for i := 1; i < len(intervals); i++ {
+        start, end := intervals[i][0], intervals[i][1]
+        if start >= prevEnd {
+            prevEnd = end
+        } else {
+            res++
+            prevEnd = min(prevEnd, end)
+        }
+    }
+    return res
+}
