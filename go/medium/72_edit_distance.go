e6d9c56a2ca9e145717f291f31029358bdac034f
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-20 17:50:45 -0800

    Update 72_edit_distance.go
    * re do

--- go/medium/72_edit_distance.go
+++ go/medium/72_edit_distance.go
@@ -13,10 +13,6 @@ func minDistance(word1 string, word2 string) int {
 			prevMinVal := 0
 			if c > 0 && r > 0 {
 				prevMinVal = min(dp[r-1][c-1], dp[r-1][c], dp[r][c-1])
-			} else if r > 0 {
-				prevMinVal = dp[r-1][c]
-			} else if c > 0 {
-				prevMinVal = dp[r][c-1]
 			}
 
 			dp[r][c] = prevMinVal + 1
@@ -28,4 +24,41 @@ func minDistance(word1 string, word2 string) int {
 		}
 	}
 	return dp[len(dp)-1][len(dp[0])-1]
+}
+
+// redo
+func minDistance(word1 string, word2 string) int {
+    if len(word1) == 0 || len(word2) == 0 {
+        return max(len(word1),len(word2))
+    }
+    dp := make([][]int, len(word2))
+    for r := range word2 {
+        dp[r] = make([]int, len(word1))
+        for c := range word1 {
+            l, t, d := r+1, c+1, 0
+            
+            if r > 0 && c == 0{
+                t = dp[r-1][c]
+                d = r
+            }
+
+            if c > 0 && r == 0{
+                l = dp[r][c-1]
+                d = c
+            }
+            
+            if r > 0 && c > 0 {
+                l = dp[r][c-1]
+                t = dp[r-1][c]
+                d = dp[r-1][c-1]
+            }
+
+            if word1[c] != word2[r] {
+                dp[r][c] = 1 + min(l, t, d)
+            } else {
+                dp[r][c] = d
+            }
+        }
+    }
+    return dp[len(dp)-1][len(dp[0])-1]
 }
