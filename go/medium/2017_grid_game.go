6c6248867c2eedc0010c40fac5f47e692705e2a0
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-21 10:19:24 -0800

    Update and rename math_maxint64.go to 2017_grid_game.go

--- go/medium/2017_grid_game.go
+++ go/medium/2017_grid_game.go
@@ -1,17 +1,14 @@
 func gridGame(grid [][]int) int64 {
     n := len(grid[0])
     
-    rowSum := make([]int64, 2)
+    var rowZeroSum int64
 
     for i := range n {
-        rowSum[0] += int64(grid[0][i])
-        rowSum[1] += int64(grid[1][i])
+        rowZeroSum += int64(grid[0][i])
     }
     
+    rowRem := []int64 {rowZeroSum, 0}
     res := int64(math.MaxInt64)
-
-    rowRem := make([]int64, 2)
-    rowRem[0] = rowSum[0]
 
     for i := range n {
         rowRem[0] -= int64(grid[0][i])
