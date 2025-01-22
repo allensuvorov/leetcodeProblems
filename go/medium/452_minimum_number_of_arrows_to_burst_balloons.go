aed9e86e4a2bb5bc89262438109d042f584bfbb8
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-21 16:03:34 -0800

    Update 452_minimum_number_of_arrows_to_burst_balloons.go

--- go/medium/452_minimum_number_of_arrows_to_burst_balloons.go
+++ go/medium/452_minimum_number_of_arrows_to_burst_balloons.go
@@ -12,3 +12,47 @@ func findMinArrowShots(points [][]int) int {
     }
     return arrowCount
 }
+
+// redo
+func findMinArrowShots(points [][]int) int {
+    slices.SortFunc(points, func(a,b []int)int{
+        return a[1]-b[1]
+    })
+    arrowCount := 0
+    prevEnd := math.MinInt
+    for _, v := range points {
+        start, end := v[0], v[1]
+        if prevEnd < start {
+            arrowCount++
+            prevEnd = end
+        }
+    }
+    return arrowCount
+}
+
+// [[3,9],[7,12],[3,8],[6,8],[9,10],[2,9],[0,9],[3,9],[0,6],[2,8]]
+
+// 0-1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18
+
+// 0-----------6
+//       3---------8
+//             6---8
+//     2-----------8
+//       3-----------9
+//     2-------------9
+// 0-----------------9
+//       3-----------9
+//                   9-10
+//               7-----------12
+
+
+
+// Input: points = [[10,16],[2,8],[1,6],[7,12]]
+
+// 1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18
+// 1---------6
+//   2-----------8
+//             7-----------12
+//                   10----------------16
+
+//  1           2             
