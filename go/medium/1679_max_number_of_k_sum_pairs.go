cd0988f849f1bef10deda0be6b33388131e3fd86
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-28 14:40:54 -0800

    Update 1679_max_number_of_k_sum_pairs.go
    * add sorting solution

--- go/medium/1679_max_number_of_k_sum_pairs.go
+++ go/medium/1679_max_number_of_k_sum_pairs.go
@@ -1,3 +1,4 @@
+// hash map
 func maxOperations(nums []int, k int) int {
     counts := map[int]int{}
     res := 0
@@ -7,6 +8,25 @@ func maxOperations(nums []int, k int) int {
             counts[k-v]--
         } else {
             counts[v]++
+        }
+    }
+    return res
+}
+
+
+// sorting
+func maxOperations(nums []int, k int) int {
+    res := 0
+    slices.Sort(nums)
+    for l, r := 0, len(nums)-1; l<r; {
+        if nums[l] + nums[r] < k {
+            l++
+        } else if nums[l] + nums[r] > k {
+            r--
+        } else {
+            res++
+            l++
+            r--
         }
     }
     return res
