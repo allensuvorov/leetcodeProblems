2b2b89dda09eec1ebebd62fec4339b9615f64c0f
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-01-29 11:24:43 -0800

    Update 345_reverse_vowels_of_a_string.go
    * redo

--- go/easy/345_reverse_vowels_of_a_string.go
+++ go/easy/345_reverse_vowels_of_a_string.go
@@ -1,15 +1,13 @@
 func reverseVowels(s string) string {
+    vowels := []byte("aeiouAEIOU")
     res := []byte(s)
-    vowels := map[byte]bool {
-        'a':true, 'e':true, 'i':true, 'o':true, 'u':true,
-        'A':true, 'E':true, 'I':true, 'O':true, 'U':true,
-    }
-    for l, r := 0, len(res)-1; l<r; {
-        if !vowels[res[l]] {
+    l, r := 0, len(s)-1
+    for l < r {
+        if !slices.Contains(vowels, res[l]) {
             l++
             continue
         }
-        if !vowels[res[r]] {
+        if !slices.Contains(vowels, res[r]) {
             r--
             continue
         }
