581ce2b47f309bc0a30f7b94d6ddde5c995ef062
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-02-10 15:16:24 -0800

    Update 2095_delete_the_middle_node_of_a_linked_list.go
    * one special case
    * prev, slow, fast

--- go/medium/2095_delete_the_middle_node_of_a_linked_list.go
+++ go/medium/2095_delete_the_middle_node_of_a_linked_list.go
@@ -9,47 +9,15 @@ func deleteMiddle(head *ListNode) *ListNode {
     if head.Next == nil {
         return nil
     }
-    fast := head
-    slow := head
-    prev := head
+
+    prev, slow, fast := head, head, head
     for fast != nil && fast.Next != nil {
         prev = slow
         slow = slow.Next
-        if fast.Next != nil {
-            fast = fast.Next.Next
-        }
+        fast = fast.Next.Next
     }
+
     prev.Next = slow.Next
-    return head
-}
 
-// redo
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
-func deleteMiddle(head *ListNode) *ListNode {
-    if head.Next == nil {
-        return nil
-    }
-
-    listLen := 0
-    for runner := head; runner != nil; runner = runner.Next {
-        listLen++
-    }
-
-    half := listLen / 2
-
-    runner := head
-    for range half - 1 {
-        runner = runner.Next
-    }
-
-
-    runner.Next = runner.Next.Next 
-        
     return head
 }
