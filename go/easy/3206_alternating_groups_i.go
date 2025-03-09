e82fe5e88b9db9c6b29f459b5e99c68c4bd373f3
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-03-09 14:13:42 -0700

    Update 3206_alternating_groups_i.go
    * time O(n)
    * space O(1)

--- go/easy/3206_alternating_groups_i.go
+++ go/easy/3206_alternating_groups_i.go
@@ -1,8 +1,18 @@
 func numberOfAlternatingGroups(colors []int) int {
+    n := len(colors)
     ans := 0
-    double := append(colors, colors...)
-    for i := range colors {
-        if double[i] != double[i+1] && double[i+1] != double[i+2] {
+    for i1 := range colors {
+        i2 := i1 + 1
+        if i2 >= n {
+            i2 = i2 - n
+        }
+
+        i3 := i1 + 2
+        if i3 >= n {
+            i3 = i3 - n
+        }
+
+        if colors[i1] != colors[i2] && colors[i2] != colors[i3] {
             ans++
         }
     }
