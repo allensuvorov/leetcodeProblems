7d92949414db23faea8b47dbfae74da338611d3d
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-03-09 14:59:58 -0700

    Update 3206_alternating_groups_i.go
    * wrap around with modulo

--- go/easy/3206_alternating_groups_i.go
+++ go/easy/3206_alternating_groups_i.go
@@ -1,18 +1,8 @@
 func numberOfAlternatingGroups(colors []int) int {
     n := len(colors)
     ans := 0
-    for i1 := range colors {
-        i2 := i1 + 1
-        if i2 >= n {
-            i2 = i2 - n
-        }
-
-        i3 := i1 + 2
-        if i3 >= n {
-            i3 = i3 - n
-        }
-
-        if colors[i1] != colors[i2] && colors[i2] != colors[i3] {
+    for i := range colors {
+        if colors[i] != colors[(i+1)%n] && colors[(i+1)%n] != colors[(i+2)%n] {
             ans++
         }
     }
