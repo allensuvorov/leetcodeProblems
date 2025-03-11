8ae5d7beaed54a113eb181c8b9169b01b5240e95
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-03-11 16:43:51 -0700

    Update 1539_kth_missing_positive_number.go

--- go/easy/1539_kth_missing_positive_number.go
+++ go/easy/1539_kth_missing_positive_number.go
@@ -16,3 +16,21 @@ func findKthPositive(arr []int, k int) int {
     }
     return n - 1
 }
+
+// log n: (k+m) magic + binary search
+
+func findKthPositive(arr []int, k int) int {
+    l, r := 0, len(arr) - 1
+    res := len(arr) + k
+    for l <= r {
+        m := l + (r-l)/2
+        
+        if arr[m] > k + m {
+            res = k + m
+            r = m - 1
+        } else {
+            l = m + 1
+        }
+    }
+    return res
+}
