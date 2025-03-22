974d23ec45cc2c03cf634c31f857e773802156d6
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-03-22 15:48:26 -0700

    Update 2564_substring_xor_queries.go
    * map of int

--- go/medium/2564_substring_xor_queries.go
+++ go/medium/2564_substring_xor_queries.go
@@ -1,18 +1,23 @@
 func substringXorQueries(s string, queries [][]int) [][]int {
-    endpoints := make(map[string][]int)
+    endpoints := make(map[int][]int)
     // time Big O(32*32*n) = O(n)
     for i := range s {
-        for j := i+1; j <= len(s) && j <= i + 30; j++ {
-            subStr := s[i:j]
-            if _, exists := endpoints[subStr]; !exists {
-                endpoints[subStr] = []int{i,j-1}
+        num := 0
+        for j := i; j < len(s) && j <= i + 30; j++ {
+            bit := int(s[j] - '0')
+            num = num << 1
+            num = num | bit
+            if _, exists := endpoints[num]; !exists {
+                if num == 0 || s[i] != '0' {
+                    endpoints[num] = []int{i,j}
+                }
             }
         }
     }
     res := [][]int{}
     for _, v := range queries {
         target := v[0] ^ v[1]
-        if v, exists := endpoints[strconv.FormatInt(int64(target), 2)]; exists {
+        if v, exists := endpoints[target]; exists {
             res = append(res, v)
         } else {
             res = append(res, []int{-1,-1})
