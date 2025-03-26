0f7f3b25075728449cff77d913e6e35ef999a890
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-03-25 18:50:55 -0700

    Update 853_car_fleet.go
    * refactor

--- go/medium/853_car_fleet.go
+++ go/medium/853_car_fleet.go
@@ -10,16 +10,14 @@ func carFleet(target int, position []int, speed []int) int {
     })
 
     // time of arrival to the target, decreasing stack
-    stack := []float64{}
+    times := []float64{}
 
-    for _, cur := range cars {
-        curTime := float64(target - cur[0])/float64(cur[1])
-        if len(stack) > 0 {
-            for len(stack) > 0 && stack[len(stack)-1] <= curTime {
-                stack = stack[:len(stack)-1] // pop
-            }
+    for _, car := range cars {
+        time := float64(target - car[0])/float64(car[1])
+        for len(times) > 0 && times[len(times)-1] <= time { // catch-up
+            times = times[:len(times)-1] // pop
         }
-        stack = append(stack, curTime)
+        times = append(times, time)
     }
-    return len(stack)
+    return len(times)
 }
