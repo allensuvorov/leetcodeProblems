4be4d77c48f4a71d7abf8b9f1474ded967a462c4
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-03-31 17:49:37 -0700

    Update 2657_find_the_prefix_common_array_of_two_arrays.go
    * one frequency array

--- go/medium/2657_find_the_prefix_common_array_of_two_arrays.go
+++ go/medium/2657_find_the_prefix_common_array_of_two_arrays.go
@@ -1,19 +1,18 @@
 func findThePrefixCommonArray(A []int, B []int) []int {
     n := len(A)
-    setA := make([]int, n+1) // 0,1,1,1
-    setB := make([]int, n+1) // 0,1,1,1
-    
-    res := make([]int, n) // 0
+    seen := make([]int, n+1)
+    res := make([]int, n)
 
     for i := range res {
-        setA[A[i]]++
-        setB[B[i]]++
 
         if A[i] == B[i] {
             res[i]++    
         } else {
-            res[i] += setA[B[i]] + setB[A[i]]
+            res[i] += seen[B[i]] + seen[A[i]]
         }
+
+        seen[A[i]] |= 1
+        seen[B[i]] |= 1
 
         if i > 0 {
             res[i] += res[i-1]
@@ -21,12 +20,3 @@ func findThePrefixCommonArray(A []int, B []int) []int {
     }
     return res
 }
-
-// idx  0
-// A = [2,3,1], 
-// B = [3,1,2]
-
-// res  0,1,3
-
-
-
