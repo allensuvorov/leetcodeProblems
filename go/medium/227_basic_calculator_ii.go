c7df93ad9dc4d7d604a7c8abbe3040fe8f7f8cc8
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-04-15 18:39:54 -0700

    Update 227_basic_calculator_ii.go
    * refactor: make it clear and concise

--- go/medium/227_basic_calculator_ii.go
+++ go/medium/227_basic_calculator_ii.go
@@ -1,81 +1,30 @@
 func calculate(s string) int {
-    curNum, lastNum, result := 0, 0, 0
-    operation := '+'
-    for i, curChar := range s {
-        if unicode.IsDigit(curChar) {
-            curNum = (curNum * 10) + int(curChar - '0')
-        }
-        if !unicode.IsDigit(curChar) && !unicode.IsSpace(curChar) || i == len(s) - 1 {
-            switch operation {
-            case '*':
-                lastNum *= curNum
-            case '/':
-                lastNum /= curNum
-            case '+':
-                result += lastNum
-                lastNum = curNum
-            case '-':
-                result += lastNum
-                lastNum = -curNum
-            }
-            operation = curChar
-            curNum = 0
-        }
-    }
-    result += lastNum
-    return result
-}
-
-// 2 hour long and over complicated solution
-func calculate(s string) int {
-    // space O(1) solution
-    // parse the string
-    s = strings.ReplaceAll(s, " ", "")
     n := len(s)
-    res := 0
-    prod := 1
-    var prodOp, currOp, nextOp byte = '+', '+', '+'
-    for i := 0; i < n; {
-        // read number
-        num := 0
-        for i < n && unicode.IsDigit(rune(s[i])) {
-            num *= 10
-            num += int(s[i] - '0')
-            i++
-        }
-    
-        if i < n {
-            nextOp = s[i]
-            i++
-        }
-
-        // use number
-        if currOp == '*' || currOp == '/' { // num is a factor
-            prod = doMath(prod, num, currOp) // factor-in the num
-            if i == n || nextOp == '+' || nextOp == '-' { 
-                res = doMath(res, prod, prodOp) //
-            }
-        } else if currOp == '+' || currOp == '-' {
-            if i == n || nextOp == '+' || nextOp == '-' { // it's a term
-                res = doMath(res, num, currOp)
-            } else if nextOp == '*' || nextOp == '/' { // new product
+    res, prod, num := 0, 0, 0
+    op := '+'
+    for i, c := range s {
+        // build number
+        if unicode.IsDigit(c) {
+            num = (num * 10) + int(c - '0')
+        } 
+        // do operation
+        if i == n - 1 || c == '+' || c == '-' || c == '*' || c == '/' {
+            switch op {
+            case '+':
+                res += prod
                 prod = num
-                prodOp = currOp
-            } 
+            case '-':
+                res += prod
+                prod = -num
+            case '*':
+                prod *= num
+            case '/':
+                prod /= num
+            }
+            op = c
+            num = 0
         }
-        
-        num = 0 // reset num
-        currOp = nextOp // save op to prodOp
     }
+    res += prod
     return res
-}
-
-func doMath(a, b int, op byte) int {
-    switch op {
-    case '+': return a + b
-    case '-': return a - b
-    case '*': return a * b
-    case '/': return a / b
-    }
-    return 0
 }
