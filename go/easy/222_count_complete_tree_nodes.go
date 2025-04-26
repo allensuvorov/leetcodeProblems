8968e58408ab463467dc7806eb4939468fb8bed3
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-04-26 16:24:51 -0700

    Update 222_count_complete_tree_nodes.go
    * practice

--- go/easy/222_count_complete_tree_nodes.go
+++ go/easy/222_count_complete_tree_nodes.go
@@ -7,20 +7,26 @@
  * }
  */
 func countNodes(root *TreeNode) int {
-    if root == nil {
+    if root == nil { // root.Val = 6
         return 0
     }
-    if left, right := getDepth(root.Left), getDepth(root.Right); left == right {
-        return 1 << left + countNodes(root.Right)
+    
+    lh, rh := getHeight(root.Left), getHeight(root.Right)
+
+    if lh == rh {
+        return 1 << lh + countNodes(root.Right) // left with math
     } else {
-        return 1 << right + countNodes(root.Left)
+        return 1 << rh + countNodes(root.Left) //  right with math
     }
+
     return 0
 }
 
-func getDepth(node *TreeNode) int {
-    if node == nil {
-        return 0
+func getHeight (root *TreeNode) int {
+    h := 0
+    for root != nil {
+        h++
+        root = root.Left
     }
-    return getDepth(node.Left) + 1
+    return h
 }
