7ff90d55e4d3dd089dc5c75321db1bdc8e7ab9a3
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-04-30 18:10:21 -0700

    Update 34_find_first_and_last_position_of_element_in_sorted_array.go
    * full control solution

--- go/medium/34_find_first_and_last_position_of_element_in_sorted_array.go
+++ go/medium/34_find_first_and_last_position_of_element_in_sorted_array.go
@@ -1,45 +1,34 @@
 func searchRange(nums []int, target int) []int {
-    res := []int{-1, -1}
-    if len(nums) == 0 {
-        return res
-    }
-    res[0] = findFirstMatch(nums, target)
-    res[1] = findLastMatch(nums, target)
-    return res
-}
-
-func findFirstMatch(nums[]int, target int) int {
-    l := 0
-    r := len(nums) - 1
-    for l < r {
+    n := len(nums)
+    // bin search to find starting position
+    start := -1
+    l, r := 0, n - 1
+    for l <= r {
         m := l + (r - l)/2
-        if nums[m] >= target {
-            r = m
-        } else {
+        if nums[m] == target {
+            r = m - 1
+            start = m
+        } else if nums[m] > target {
+            r = m - 1
+        } else if nums[m] < target {
             l = m + 1
         }
     }
-    if nums[l] == target {
-        return l
-    } else {
-        return -1
-    }
-}
 
-func findLastMatch(nums[]int, target int) int {
-    l := 0
-    r := len(nums) - 1
-    for l < r {
-        m := l + (r - l + 1)/2
-        if nums[m] <= target {
-            l = m
-        } else {
+    // bin search to find ending position
+    end := -1
+    l, r = 0, n - 1
+    for l <= r {
+        m := l + (r - l)/2
+        if nums[m] == target {
+            end = m
+            l = m + 1
+        } else if nums[m] > target {
             r = m - 1
+        } else if nums[m] < target {
+            l = m + 1
         }
+
     }
-    if nums[l] == target {
-        return l
-    } else {
-        return -1
-    }
+    return []int{start, end}
 }
