8679528d1109c22f4890bf9c19b15ecee14a9469
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-05-02 19:23:37 -0700

    Update 611_valid_triangle_number.go
    * third pointer

--- go/medium/611_valid_triangle_number.go
+++ go/medium/611_valid_triangle_number.go
@@ -1,26 +1,15 @@
 func triangleNumber(nums []int) int {
-    // n^2 * log n
+    // n^2
     n := len(nums)
     slices.Sort(nums)
-    binSearch := func(b int, target int) int {
-        l, r := b+1, n-1
-        c := b
-        for l <= r {
-            m := l + (r - l)/2
-            if nums[m] >= target {
-                r = m - 1
-            } else {
-                c = m
-                l = m + 1
-            }
-        }
-        return c - b
-    }
 
     res := 0
     for a := range nums {
-        for b := a + 1; nums[a] > 0 && b < n - 1; b++ {
-            res += binSearch(b, nums[a] + nums[b])
+        for b, c := a + 1, a + 2; nums[a] > 0 && b < n - 1; b++ {
+            for c < n && nums[a] + nums[b] > nums[c] {
+                c++
+            }
+            res += c - b - 1
         }
     }
     return res
