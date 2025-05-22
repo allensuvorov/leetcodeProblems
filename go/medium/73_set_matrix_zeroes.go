5b6cfa1e85d8c7b9471d25942192bfe596100de6
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-05-21 20:02:25 -0700

    Update 73_set_matrix_zeroes.go
    * constant space solution

--- go/medium/73_set_matrix_zeroes.go
+++ go/medium/73_set_matrix_zeroes.go
@@ -1,54 +1,56 @@
-func setZeroes(matrix [][]int)  {
-    // mark zeros with MaxInt
-    // Set zeros to the marked
-    zeroCal0 := false
-    zeroRow0 := false
+func setZeroes(matrix [][]int) {
+	rows, cols := len(matrix), len(matrix[0])
+	// look for a zero, save row and col
+	cacheRow, cacheCol := -1, -1
+	for r := range rows {
+		for c := range cols {
+			if matrix[r][c] == 0 {
+				cacheRow = r
+				cacheCol = c
+			}
+		}
+	}
 
-    for c := range matrix[0] {
-        if matrix[0][c] == 0 {
-            zeroRow0 = true
-        }
-    }
+    // if no zero, return
+	if cacheRow == -1 {
+		return
+	}
 
-    for r := range matrix {
-        if matrix[r][0] == 0 {
-            zeroCal0 = true
-        }
-    }
+	// use those cache fields as memory
+	for r := range rows {
+		for c := range cols {
+			if matrix[r][c] == 0 {
+				matrix[r][cacheCol] = 0
+				matrix[cacheRow][c] = 0
+			}
+		}
+	}
 
-    for r := 1; r < len(matrix); r++ {
-        for c := 1; c < len(matrix[0]); c++ {
-            if matrix[r][c] == 0 {
-                matrix[r][0] = 0
-                matrix[0][c] = 0
-            }
-        }
+	// zero target rows, except cache row
+	for r := range rows {
+		if r != cacheRow && matrix[r][cacheCol] == 0 {
+			for c := range cols {
+				matrix[r][c] = 0
+			}
+		}
+	}
+    
+	// zero target cols, except cache col
+	for c := range cols {
+		if c != cacheCol && matrix[cacheRow][c] == 0 {
+			for r := range rows {
+				matrix[r][c] = 0
+			}
+		}
+	}
+    
+    // zero cache row
+    for c := range cols {
+        matrix[cacheRow][c] = 0
     }
-
-    for c := 1; c < len(matrix[0]); c++ {
-        if matrix[0][c] == 0 {
-            for r := range matrix{
-                matrix[r][c] = 0
-            }
-        }
-    }
-
-    for r := 1; r < len(matrix); r++ {
-        if matrix[r][0] == 0 {
-            for c := range matrix[0] {
-                matrix[r][c] = 0
-            }
-        }
-    }
-    if zeroCal0 {
-        for r := range matrix {
-            matrix[r][0] = 0
-        }
-    }
-
-    if zeroRow0 {
-        for c := range matrix[0] {
-            matrix[0][c] = 0
-        }
+    
+    // zero cache col
+    for r := range rows {
+        matrix[r][cacheCol] = 0
     }
 }
