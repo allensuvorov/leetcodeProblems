0dda821679d51188c835a2a2e4186a663c6e4ae3
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-05-22 17:08:52 -0700

    Update 96_unique_binary_search_trees.go
    re-do

--- go/medium/96_unique_binary_search_trees.go
+++ go/medium/96_unique_binary_search_trees.go
@@ -1,14 +1,16 @@
 func numTrees(n int) int {
-    dp := []int{0:1, 1:1, 20: 0}
-
-    for i := 2; i < n + 1; i++ {
-        sum := 0
-        for r := range i {
-            l := i - (r + 1)
-            sum += dp[l] * dp[r]
+    dp := make([]int, n + 1)
+    dp[0] = 1 // to support combinations with zero nodes
+    
+    for nodeCount := 1; nodeCount <= n; nodeCount++ {
+        totalTreeCount := 0
+        for root := 1; root <= nodeCount; root++ {
+            leftSubTreeSize := root - 1
+            rightSubTreeSize := nodeCount - root
+            totalTreeCount += dp[leftSubTreeSize] * dp[rightSubTreeSize]
         }
-        dp[i] = sum
+        dp[nodeCount] = totalTreeCount
     }
-
+    
     return dp[n]
 }
