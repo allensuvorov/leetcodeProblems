d07dad9aabf72c2d6efba4cbfe121cc33a8c128a
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-05-23 23:12:11 -0700

    Create 95_unique_binary_search_trees_ii.go
    * logic

---
+++ go/medium/95_unique_binary_search_trees_ii.go
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func generateTrees(n int) []*TreeNode {
+        // solve for 1 node and for 2 nodes
+        // build trees recurcively
+}
+
+1
+1 2
