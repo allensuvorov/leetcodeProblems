c0e2d571cadf2fb6aae3ff7f351443ac79011010
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-05-27 20:31:25 -0700

    Update 1071_greatest_common_divisor_of_strings.go
    redo

--- go/easy/1071_greatest_common_divisor_of_strings.go
+++ go/easy/1071_greatest_common_divisor_of_strings.go
@@ -2,32 +2,11 @@ func gcdOfStrings(str1 string, str2 string) string {
     if str1 + str2 != str2 + str1 {
         return ""
     }
-    maxLen := gcd(len(str1), len(str2))
-    return str1[:maxLen]
-}
 
-func gcd(x, y int) int {
-    for y != 0 {
-        x, y = y, x%y
-    }
-    return x
-}
-
-
-// more intuitive
-
-func gcdOfStrings(str1 string, str2 string) string {
-    n, m := len(str1), len(str2)
-
-    if str1 + str2 != str2 + str1 {
-        return ""
+    gcdLen := min(len(str1), len(str2))
+    for gcdLen > 1 && (len(str1) % gcdLen != 0 || len(str2) % gcdLen != 0) {
+        gcdLen--
     }
 
-    for d := min(n, m); d > 0; d-- {
-        if n % d == 0 && m % d == 0 {
-            return str1[:d]
-        }
-    }
-
-    return ""
+    return str1[:gcdLen]
 }
