8d173104aec4ce57057c3a2bb907425352d70d85
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-05-29 19:20:01 -0700

    Update 392_is_subsequence.go

--- go/easy/392_is_subsequence.go
+++ go/easy/392_is_subsequence.go
@@ -1,9 +1,23 @@
 func isSubsequence(s string, t string) bool {
     j := 0
-    for i := 0; i < len(t) && j < len(s); i++ {
-        if t[i] == s[j] {
-            j++
+    for i := 0; i < len(t) && j < len(s); i++ { // this is what we have, one by one
+        if t[i] == s[j] { // is it required?
+            j++ // good, check next
         }
     }
-    return j == len(s)
+    return j == len(s) // did we find all required?
+}
+
+
+func isSubsequence(s string, t string) bool {
+    for i, j := 0, 0; i < len(s); i++{
+        for j < len(t) && t[j] != s[i] {
+            j++
+        }
+        if j == len(t) {
+            return false
+        }
+        j++
+    }
+    return true
 }
