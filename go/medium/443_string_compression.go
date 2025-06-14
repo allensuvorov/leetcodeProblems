8f7f5cf312a88095af65fe5e10f8964c18342cae
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-06-14 16:43:38 -0700

    Update 443_string_compression.go
    * redo

--- go/medium/443_string_compression.go
+++ go/medium/443_string_compression.go
@@ -1,22 +1,22 @@
 func compress(chars []byte) int {
-    w := 0
-    for l := 0; l < len(chars); {
-        // read, measure 
-        r := l + 1
-        for r < len(chars) && chars[r] == chars[l] {
+    w := 0 // writer index
+    r := 0 // reader index
+    for r < len(chars) {
+        chars[w] = chars[r] // new group
+        groupLen := 0
+        for r < len(chars) && chars[w] == chars[r] {
+            groupLen++
             r++
         }
-        // write
-        chars[w] = chars[l]
         w++
-        if (r - l) > 1 {
-            s := strconv.Itoa(r - l)
+
+        if groupLen > 1 {
+            s := strconv.Itoa(groupLen)
             for i := range s {
                 chars[w] = s[i]
                 w++
             }
         }
-        l = r
     }
     return w
 }
