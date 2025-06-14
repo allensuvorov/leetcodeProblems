1da2ca5a644209309641eee58d19776b7a304b38
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-06-14 15:44:23 -0700

    Update 1861_rotating_the_box.go
    * two pointers

--- go/medium/1861_rotating_the_box.go
+++ go/medium/1861_rotating_the_box.go
@@ -1,33 +1,26 @@
 func rotateTheBox(boxGrid [][]byte) [][]byte {
-    rows := len(boxGrid[0]) // 3
-    cols := len(boxGrid) // 1
-    rotatedBox := make([][]byte, rows)
+    rows, cols := len(boxGrid[0]), len(boxGrid)
+    // rotate the box
+    res := make([][]byte, rows)
     for r := range rows {
-        row := make([]byte, cols)
+        res[r] = make([]byte, cols)
         for c := range cols {
-            row[c] = boxGrid[cols-1-c][r]
+            res[r][c] = boxGrid[cols-1-c][r]
         }
-        rotatedBox[r] = row
     }
-
+   
     for c := range cols { // for each column
-        for p := rows - 1; p >= 0; p-- { // p - plant index
-            if rotatedBox[p][c] != '.' { 
-                continue
+        p := rows - 1 // p - plant index begins from bottom    
+        for s := rows - 1; s >= 0; s-- { // s - scout index
+            if res[s][c] == '#' { 
+                res[s][c], res[p][c] = '.', '#'
+                p--
             }
-
-            // for each empty space
-            for s := p - 1; s >= 0; s-- { // s - scout index
-                if rotatedBox[s][c] == '*' {
-                    p = s
-                    break
-                }
-                if rotatedBox[s][c] == '#' { 
-                    rotatedBox[s], rotatedBox[p] = rotatedBox[p], rotatedBox[s]
-                    p--
-                }
+            if res[s][c] == '*' {
+                p = s - 1
             }
         }
     }
-    return rotatedBox
+    
+    return res
 }
