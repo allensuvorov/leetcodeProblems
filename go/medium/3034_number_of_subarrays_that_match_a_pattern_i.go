5897b23e8ae46fdaf5ca94e1ea1248e0f5be162d
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-06-14 18:13:25 -0700

    Update 3034_number_of_subarrays_that_match_a_pattern_i.go

--- go/medium/3034_number_of_subarrays_that_match_a_pattern_i.go
+++ go/medium/3034_number_of_subarrays_that_match_a_pattern_i.go
@@ -6,10 +6,10 @@ func countMatchingSubarrays(nums []int, pattern []int) int {
         match := true
         k := 0
         for ; k < m && i < n - m; k++ {
-            inc := pattern[k] == 1 && nums[i + k + 1] > nums[i + k]
-            non := pattern[k] == 0 && nums[i + k + 1] == nums[i + k]
-            dec := pattern[k] == -1 && nums[i + k + 1] < nums[i + k]
-            if !inc && !non && !dec {
+            c1 := pattern[k] == 1 && nums[i + k + 1] > nums[i + k]
+            c2 := pattern[k] == 0 && nums[i + k + 1] == nums[i + k]
+            c3 := pattern[k] == -1 && nums[i + k + 1] < nums[i + k]
+            if !(c1 || c2 || c3) {
                 match = false
             }
         }
