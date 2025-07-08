e0961d335f9543567f28a8da77e7186a4c3cb0a5
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-07-07 21:15:24 -0700

    Update 1353_maximum_number_of_events_that_can_be_attended.go
    TODO - review this heap solution

--- go/medium/1353_maximum_number_of_events_that_can_be_attended.go
+++ go/medium/1353_maximum_number_of_events_that_can_be_attended.go
@@ -1,31 +1,55 @@
 func maxEvents(events [][]int) int {
-    // preprocessing
-    slices.SortFunc(events, func(a, b []int) int{
-        // sort by end
-        return a[1] - b[1]
-    })
-
-    attendedCount := 0
-    today := 1 // the day we have
-
-    for _, event := range events {
-        start, end := event[0], event[1]
-        if end >= today {
-            attendedCount++
-            if start <= today {
-                today++
-            } else {
-                today = start + 1
-            }
-        }        
-    }
-    return attendedCount
+	n := len(events)
+	maxDay := 0
+	for _, event := range events {
+		if event[1] > maxDay {
+			maxDay = event[1]
+		}
+	}
+	sort.Slice(events, func(i, j int) bool {
+		return events[i][0] < events[j][0]
+	})
+	pq := &IntHeap{}
+	heap.Init(pq)
+	ans := 0
+	for i, j := 1, 0; i <= maxDay; i++ {
+		for j < n && events[j][0] <= i {
+			heap.Push(pq, events[j][1])
+			j++
+		}
+		for pq.Len() > 0 && (*pq)[0] < i {
+			heap.Pop(pq)
+		}
+		if pq.Len() > 0 {
+			heap.Pop(pq)
+			ans++
+		}
+	}
+	return ans
 }
 
+type IntHeap []int
 
-// 12345
-//  --
-//  --
-// -----
-// -----
-// -----
+func (h IntHeap) Len() int {
+	return len(h)
+}
+
+func (h IntHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+
+func (h IntHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *IntHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
