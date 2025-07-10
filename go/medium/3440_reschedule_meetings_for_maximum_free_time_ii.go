67b30258fde5e7b8b7ee5905ba853c730419b828
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-07-10 16:21:10 -0700

    Update 3440_reschedule_meetings_for_maximum_free_time_ii.go
    * time complexity Big O(n)
    * space complexity Big O(1)

--- go/medium/3440_reschedule_meetings_for_maximum_free_time_ii.go
+++ go/medium/3440_reschedule_meetings_for_maximum_free_time_ii.go
@@ -1,56 +1,42 @@
 func maxFreeTime(eventTime int, startTime []int, endTime []int) int {
     n := len(startTime)
-    maxBreakFromL := make([]int, n)
-    maxBreak := 0
-    for i := range n {
-        currentBreak := startTime[i]
+    res := 0
+
+    getLR := func(i int) (int, int) {
+        l := 0
         if i > 0 {
-            currentBreak -= endTime[i-1]
+            l = endTime[i-1]
         }
-        maxBreak = max(maxBreak, currentBreak)
-        maxBreakFromL[i] = maxBreak
+        r := eventTime
+        if i < n - 1 {
+            r = startTime[i+1]
+        }
+        return l, r
+    }
+
+    maxSlotSoFar := 0
+    for i := range n {
+        l, r := getLR(i)
+        meeting := endTime[i] - startTime[i]
+        if maxSlotSoFar >= meeting {
+            res = max(res, r - l)
+        } else {
+            res = max(res, r - l - meeting)
+        }
+        maxSlotSoFar = max(maxSlotSoFar, startTime[i] - l)
     }
     
-    maxBreakFromR := make([]int, n)
-    maxBreak = 0
+    maxSlotSoFar = 0
     for i := n-1; i >=0; i-- {
-        currentBreak := eventTime - endTime[i]
-        if i < n-1 {
-            currentBreak = startTime[i+1] - endTime[i]
-        }
-        maxBreak = max(maxBreak, currentBreak)
-        maxBreakFromR[i] = maxBreak
-    }
-    // fmt.Println(maxBreakFromL)
-    // fmt.Println(maxBreakFromR)
-    res := 0
-    for i := range n {
-        leftBreak := startTime[i]
-        if i > 0 {
-            leftBreak = startTime[i] - endTime[i-1] 
-        }
-
-        rightBreak := eventTime - endTime[i]
-        if i < n - 1 {
-            rightBreak = startTime[i+1] - endTime[i]
-        }
-
-        // check free space on left
-        maxBreak = 0
-        if i > 0 {
-            maxBreak = max(maxBreak, maxBreakFromL[i-1])
-        }
-
-        if i < n - 1 {
-            maxBreak = max(maxBreak, maxBreakFromR[i+1])
-        }
-
-        meetingDuration := endTime[i] - startTime[i]
-        if maxBreak >= meetingDuration {
-            res = max(res, leftBreak + rightBreak + meetingDuration)
+        l, r := getLR(i)
+        meeting := endTime[i] - startTime[i]
+        if maxSlotSoFar >= meeting {
+            res = max(res, r - l)
         } else {
-            res = max(res, leftBreak + rightBreak)
+            res = max(res, r - l - meeting)
         }
+        maxSlotSoFar = max(maxSlotSoFar, r - endTime[i])
     }
+
     return res
 }
