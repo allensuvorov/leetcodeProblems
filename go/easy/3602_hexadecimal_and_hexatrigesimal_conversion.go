8cf193a32d0532c4f1dd0572acf789a078b60993
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-07-12 12:16:58 -0700

    Update 3602_hexadecimal_and_hexatrigesimal_conversion.go

--- go/easy/3602_hexadecimal_and_hexatrigesimal_conversion.go
+++ go/easy/3602_hexadecimal_and_hexatrigesimal_conversion.go
@@ -3,3 +3,24 @@ func concatHex36(n int) string {
     ht := strings.ToUpper((strconv.FormatInt(int64(n*n*n), 36)))
     return hd + ht
 }
+
+
+// custom formatInt() function:
+func concatHex36(n int) string {
+    hd := formatInt(n*n, 16)
+    ht := formatInt(n*n*n, 36)
+    return hd + ht
+}
+
+func formatInt(n, base int) string {
+    s := ""
+    for a := n; a > 0; a = a / base {
+        d := a % base
+        c := string(byte(d) + '0')
+        if d > 9 {
+            c = string(byte(d-10) + 'A')
+        }
+        s = c + s
+    }
+    return s
+}
