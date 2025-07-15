d652d0b5c734cd4cd7fc2a43852cb8c91f5ebc16
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-07-14 18:56:45 -0700

    Update 3136_valid_word.go

--- go/easy/3136_valid_word.go
+++ go/easy/3136_valid_word.go
@@ -1,26 +1,22 @@
 func isValid(word string) bool {
-    // It contains a minimum of 3 characters.
     if len(word) < 3 {
         return false
     }
-    includesVowel := false
-    includesConsonant := false
+    
+    hasVowel := false
+    hasConsonant := false
     isVowel := map[rune]bool{'a':true, 'e':true, 'i':true, 'o':true, 'u':true}
-    for _, v := range word {
-        // It contains only digits (0-9), and English letters (uppercase and lowercase).
-        if !unicode.IsLetter(v) && !unicode.IsDigit(v) {
-            return false
-        }
 
+    for _, v := range word {
         if unicode.IsLetter(v) {
             if isVowel[unicode.ToLower(v)] {
-                // It includes at least one vowel.
-                includesVowel = true
+                hasVowel = true
             } else {
-                // It includes at least one consonant.
-                includesConsonant = true
+                hasConsonant = true
             }
+        } else if !unicode.IsDigit(v) {
+            return false
         }
     }
-    return includesVowel && includesConsonant
+    return hasVowel && hasConsonant
 }
