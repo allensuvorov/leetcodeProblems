338af6ce7acca4bf862a0a3c8d8ccc8126facff9
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-07-14 18:53:32 -0700

    Update 3136_valid_word.go

--- go/easy/3136_valid_word.go
+++ go/easy/3136_valid_word.go
@@ -1,38 +1,26 @@
 func isValid(word string) bool {
+    // It contains a minimum of 3 characters.
     if len(word) < 3 {
         return false
     }
-  
-    invalidSymbols := map[rune]bool {
-        '@': true, 
-        '#': true,
-        '$': true,
-    }
-  
-    vowels := map[rune]bool {
-        'a': true,
-        'e': true,
-        'i': true,
-        'o': true,
-        'u': true,
-    }
-
-    var includesVowel, includesConsonant bool
-
+    includesVowel := false
+    includesConsonant := false
+    isVowel := map[rune]bool{'a':true, 'e':true, 'i':true, 'o':true, 'u':true}
     for _, v := range word {
-        v = unicode.ToLower(v)
-        if invalidSymbols[v] {
+        // It contains only digits (0-9), and English letters (uppercase and lowercase).
+        if !unicode.IsLetter(v) && !unicode.IsDigit(v) {
             return false
         }
 
-        if vowels[v] {
-            includesVowel = true
-        }
-
-        if unicode.IsLetter(v) && !vowels[v] {
-            includesConsonant = true
+        if unicode.IsLetter(v) {
+            if isVowel[unicode.ToLower(v)] {
+                // It includes at least one vowel.
+                includesVowel = true
+            } else {
+                // It includes at least one consonant.
+                includesConsonant = true
+            }
         }
     }
-
     return includesVowel && includesConsonant
 }
