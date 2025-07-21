c7579aeb4d191a2ef234042f9e589f09667a54f6
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-07-20 21:37:48 -0700

    Update 1957_delete_characters_to_make_fancy_string.go

--- go/easy/1957_delete_characters_to_make_fancy_string.go
+++ go/easy/1957_delete_characters_to_make_fancy_string.go
@@ -1,13 +1,8 @@
 func makeFancyString(s string) string {
-    limit := 2
-    res := []byte{}
-    l := 0
-    for r := range s {
-        if s[r] != s[l] {
-            l = r 
-        }
-        if r - l < limit {
-            res = append(res, s[r])
+    res := make([]byte, 0)
+    for i := 0; i < len(s); i++ {
+        if i == 0 || s[i] != s[i-1] || i == len(s) - 1 || s[i] != s[i+1] {
+            res = append(res, s[i])
         }
     }
     return string(res)
