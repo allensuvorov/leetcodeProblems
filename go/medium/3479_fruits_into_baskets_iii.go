d65d9f260a9aa1e0c13eaab2ccaa8328461df303
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-08-06 20:52:27 -0700

    Create 3479_fruits_into_baskets_iii.go
    * TODO - find better idea

---
+++ go/medium/3479_fruits_into_baskets_iii.go
@@ -0,0 +1,27 @@
+func numOfUnplacedFruits(fruits []int, baskets []int) int {
+    // Big O (n * log(n)) - for binary search, but just for search, 
+        // maintaining the sorted list is O(n)
+    // Big O (1/2 * n * n) - linked list 
+
+}
+
+Input: 
+
+fruits = [4,2,5], 
+          ^
+baskets = [3,5,4]
+
+
+
+3, 7, 4, 5, 4
+
+1  0  1  1  0
+
+
+
+
+  *
+  * *
+* * *
+* * *
+* * *
