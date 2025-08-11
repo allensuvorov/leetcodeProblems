477e05467a47a974d111e2913b7c4a7516affcf4
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-08-10 19:13:55 -0700

    Update 869_reordered_power_of_2.go
    * permutations + bitwise

--- go/medium/869_reordered_power_of_2.go
+++ go/medium/869_reordered_power_of_2.go
@@ -1,23 +1,29 @@
 func reorderedPowerOf2(n int) bool {
-    // try each order Big O((Log(10)N)!)
-    digitCounts := make(map[int]int)
+    // try each permutation - Big O((Log(10)N)!)
+    digits := make([]int, 0)
     for x := n; x > 0; x = x / 10 {
-        digitCounts = append(digits, x % 10)
+        digits = append(digits, x % 10)
     }
-
-    return dfs(digitCounts, num)
+    return dfs(digits, 0)
 }
 
-func dfs(digitCounts map[int]int, num int) bool {
-    if len(digitCounts) == 0 {
+
+func dfs(digits []int, num int) bool {
+    if len(digits) == 0 {
         return isPowerOf2(num)
     }
-
-    for i := range len(digits) {
-        if digits[i] != 0 {
-            used := make([]bool, len(digits))
-            num := digits[i]
-            used[i] = true
+    
+    for i, digit := range digits {
+        if digit != 0 || num != 0  {
+            nextDigits := make([]int, 0, len(digits) - 1)
+            for j, v := range digits {
+                if j != i {
+                    nextDigits = append(nextDigits, v)
+                }
+            }
+            if dfs(nextDigits, num * 10 + digit) {
+                return true
+            }
         }
     }
 
@@ -27,7 +33,3 @@ func dfs(digitCounts map[int]int, num int) bool {
 func isPowerOf2(n int)bool {
     return n > 0 && (n & (n-1) == 0)
 }
-
-1    2    3
-2 3  1 3
-3 2  3 1
