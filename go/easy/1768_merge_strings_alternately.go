1c5de35f3fe420407ec0ffcc7d6d51a523fbcbb1
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-08-15 15:11:26 -0700

    Update 1768_merge_strings_alternately.go
    * redo

--- go/easy/1768_merge_strings_alternately.go
+++ go/easy/1768_merge_strings_alternately.go
@@ -1,30 +1,16 @@
 func mergeAlternately(word1 string, word2 string) string {
-    res := make([]byte, 0, len(word1) + len(word2))
-    for i := range max(len(word1), len(word2)) {
+    n1, n2 := len(word1), len(word2)
+    res := make([]byte, 0, n1 + n2)
+    i, j := 0, 0
+    for range max(n1, n2) {
         if i < len(word1) {
             res = append(res, word1[i])
+            i++
         }
-        if i < len(word2) {
-            res = append(res, word2[i])
+        if j < len(word2) {
+            res = append(res, word2[j])
+            j++
         }
     }
     return string(res)
-}
-
-// Big O (n^2) In Go strings are immutable. Every concat is a O(n).
-func mergeAlternately(word1 string, word2 string) string {
-    res := ""
-    
-    for i := range max(len(word1), len(word2)) {
-        
-        if i < len(word1) {
-            res += string(word1[i])
-        }
-        
-        if i < len(word2) {
-            res += string(word2[i])
-        }
-    }
-    return res
-        
 }
