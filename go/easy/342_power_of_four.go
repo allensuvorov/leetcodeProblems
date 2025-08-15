22c10bca4d2580e2157323a5166926173d54f204
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-08-14 20:36:21 -0700

    Update 342_power_of_four.go
    * add log(n) solution

--- go/easy/342_power_of_four.go
+++ go/easy/342_power_of_four.go
@@ -7,3 +7,11 @@ func isPowerOfFour(n int) bool {
 
     return n > 0 && intSqRt & (intSqRt - 1) == 0
 }
+
+// Big O (log(n))
+func isPowerOfFour(n int) bool {
+    for n % 4 == 0 {
+        n = n / 4
+    }
+    return n == 1
+}
