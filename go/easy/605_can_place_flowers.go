6ea3fe5f75b12b278906f72412bcf4f09d203664
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-08-15 16:21:04 -0700

    Update 605_can_place_flowers.go

--- go/easy/605_can_place_flowers.go
+++ go/easy/605_can_place_flowers.go
@@ -12,3 +12,25 @@ func canPlaceFlowers(flowerbed []int, n int) bool {
 
     return n <= 0
 }
+
+// early exit
+func canPlaceFlowers(flowerbed []int, n int) bool {
+    for i := range flowerbed {
+        if flowerbed[i] != 0 { // current
+            continue
+        }
+
+        if i != 0 && flowerbed[i - 1] == 1 { // prev
+            continue
+        }
+
+        if i != len(flowerbed) - 1 && flowerbed[i+1] == 1 { // next
+            continue
+        }
+
+        flowerbed[i] = 1
+        n--
+    }
+
+    return n <= 0
+}
