5e8ccc7f58ae37c4dd698956445a358280978dba
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-08-16 19:24:02 -0700

    Update 334_increasing_triplet_subsequence.go
    * redo

--- go/medium/334_increasing_triplet_subsequence.go
+++ go/medium/334_increasing_triplet_subsequence.go
@@ -1,8 +1,10 @@
 func increasingTriplet(nums []int) bool {
-    min1, min2 := math.MaxInt, math.MaxInt
+    min1 := math.MaxInt
+    min2 := math.MaxInt
+
     for _, v := range nums {
         if v <= min1 {
-            min1 = v 
+            min1 = v
         } else if v <= min2 {
             min2 = v
         } else {
