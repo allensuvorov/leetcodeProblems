27bcde1daad14132f46cf9ba4e50a55ae8310d6c
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-08-18 18:59:39 -0700

    Update 1493_longest_subarray_of_1_s_after_deleting_one_element.go

--- go/medium/1493_longest_subarray_of_1_s_after_deleting_one_element.go
+++ go/medium/1493_longest_subarray_of_1_s_after_deleting_one_element.go
@@ -1,18 +1,20 @@
 func longestSubarray(nums []int) int {
-    maxLen := 0
+    maxSize := 0
     zeroCount := 0
-    for l, r := 0, 0; r < len(nums); r++ {
+    l := 0
+    for r := range nums {
         if nums[r] == 0 {
             zeroCount++
         }
-
         for zeroCount > 1 {
             if nums[l] == 0 {
                 zeroCount--
             }
             l++
         }
-        maxLen = max(maxLen, r - l)
-    }
-    return maxLen   
+        maxSize = max(maxSize, r - l) // (r - l) is windowSize minus 1
+            // when there is one a more zeros in the array, the maxSize window will include one of them
+            // when there no zeros in the array, the maxSize be reduced by one, since we have to delete one
+    }   
+    return maxSize
 }
