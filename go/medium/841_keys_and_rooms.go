650ad5f6b8c331f918935c853a3cea2105bb735d
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-08-19 16:41:01 -0700

    Update 841_keys_and_rooms.go
    * add a variation with separate DFS

--- go/medium/841_keys_and_rooms.go
+++ go/medium/841_keys_and_rooms.go
@@ -1,3 +1,4 @@
+// nested DFS
 func canVisitAllRooms(rooms [][]int) bool {
 	visited := make([]bool, len(rooms))
 	visitedCount := 0
@@ -14,4 +15,22 @@ func canVisitAllRooms(rooms [][]int) bool {
     }
     dfs(0)
     return visitedCount == len(rooms)
+}
+
+// separate DFS
+func canVisitAllRooms(rooms [][]int) bool {
+    return dfs(0, rooms, map[int]bool{0:true})
+}
+
+func dfs(room int, rooms [][]int, seen map[int]bool) bool {
+    seen[room] = true
+    if len(seen) == len(rooms) {
+        return true
+    }
+    for _, key := range rooms[room] {
+        if !seen[key] && dfs(key, rooms, seen) {
+            return true
+        }
+    }
+    return false
 }
