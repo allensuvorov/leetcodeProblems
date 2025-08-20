163c694c0cc3c13fc130d5bf22bc5bda14806822
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-08-20 12:28:26 -0700

    Update 1137_n_th_tribonacci_number.go
    * redo

--- go/easy/1137_n_th_tribonacci_number.go
+++ go/easy/1137_n_th_tribonacci_number.go
@@ -1,15 +1,18 @@
 func tribonacci(n int) int {
-    if n < 2 {
-        return n
+    if n == 0 {
+        return 0 
+    }
+    if n == 1 {
+        return 1 
     }
     if n == 2 {
         return 1
     }
 
     a, b, c := 0, 1, 1
-
     for range n - 2 {
         a, b, c = b, c, a + b + c
     }
+
     return c
 }
