40c2ba5fbee596acb6f6f303584ded292f67e17b
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-09-06 20:58:27 -0700

    Update 1304_find_n_unique_integers_sum_up_to_zero.go
    * redo

--- go/easy/1304_find_n_unique_integers_sum_up_to_zero.go
+++ go/easy/1304_find_n_unique_integers_sum_up_to_zero.go
@@ -1,10 +1,11 @@
 func sumZero(n int) []int {
-    ans := make([]int, 0, n)
-    if n%2 != 0 {
-        ans = append(ans, 0)
+    res := make([]int, 0, n)
+    if n % 2 == 1 {
+        res = append(res, 0)
     }
-    for i := 1; i <= n/2; i ++ {
-        ans = append(ans, []int{i, -i}...)
+
+    for i := 1; i <= n/2; i++ {
+        res = append(res, i, -i)
     }
-    return ans
+    return res
 }
