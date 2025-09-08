c1fdc9ca93e24d36154c6851b431ab0fdd537fc0
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-09-07 18:42:43 -0700

    Update 1317_convert_integer_to_the_sum_of_two_no_zero_integers.go
    * simple solution

--- go/easy/1317_convert_integer_to_the_sum_of_two_no_zero_integers.go
+++ go/easy/1317_convert_integer_to_the_sum_of_two_no_zero_integers.go
@@ -1,20 +1,18 @@
 func getNoZeroIntegers(n int) []int {
-    a := 1
-    m := n
-    level := 1
-    carry := 0
-    if m > 9 {
-        for a = 0; m / 10 != 0; m /= 10 {
-            d := m % 10 - carry
-            if d < 2 {
-                a += 2 * level
-                carry = 1
-            } else {
-                a += 1 * level
-                carry = 0
-            }
-            level *= 10
+    for a := 1; a < n; a++ {
+        if isNoZero(a) && isNoZero(n - a) {
+            return []int{a, n-a}
         }
     }
-    return []int{a, n - a}
+    return nil
+}
+
+func isNoZero(a int) bool {
+    for x := a; x != 0; x = x / 10 {
+        d := x % 10
+        if d % 10 == 0 {
+            return false
+        }
+    }
+    return true
 }
