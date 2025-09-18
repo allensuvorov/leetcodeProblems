b6051f2a8313dc24a15a7cb0730fe3d5845114e9
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-09-17 20:51:25 -0700

    Update 2200_find_all_k_distant_indices_in_an_array.go
    * constant space

--- go/easy/2200_find_all_k_distant_indices_in_an_array.go
+++ go/easy/2200_find_all_k_distant_indices_in_an_array.go
@@ -1,29 +1,14 @@
 func findKDistantIndices(nums []int, key int, k int) []int {
-    // 0 ... n - 1
     n := len(nums)
-
-    keyIndexList := []int{}
+    var result []int
+    r := -1
     for i := range nums {
         if nums[i] == key {
-            keyIndexList = append(keyIndexList, i)
-        }
-    }
-    
-    result := []int{}
-
-    for _, i := range keyIndexList {
-        l := i - k
-        l = max(0, l)
-        if len(result) > 0 {
-            topVal := result[len(result) - 1]
-            l = max(l, topVal + 1)
-        }
-
-        r := i + k
-        r = min(n-1, r)
-
-        for j := l; j <= r; j++ {
-            result = append(result, j)
+            l := max(i - k, r + 1)
+            r = min(n-1, i + k)
+            for j := l; j <= r; j++ {
+                result = append(result, j)
+            }
         }
     }
     return result
