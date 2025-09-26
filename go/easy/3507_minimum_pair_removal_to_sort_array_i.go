14b5f643ef5a89e6dead351b41e347ff2cb194bc
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-09-26 10:10:50 -0700

    Update 3507_minimum_pair_removal_to_sort_array_i.go
    * solved

--- go/easy/3507_minimum_pair_removal_to_sort_array_i.go
+++ go/easy/3507_minimum_pair_removal_to_sort_array_i.go
@@ -1,11 +1,32 @@
 func minimumPairRemoval(nums []int) int {
-
 // simulation
-    // check for dec
-    // find min pair
-  
+    opsCnt := 0
+    for len(nums) > 1 {
+        dec := false
+        // check for dec
+        for i := 1; i < len(nums); i++ {
+            if nums[i-1] > nums[i] {
+                dec = true
+                break
+            }
+        }
+        if !dec {
+            return opsCnt
+        }
+        opsCnt++
+
+        // find min sum pair
+        minSum := nums[1] + nums[0]
+        minSumIdx := 1
+        for i := 1; i < len(nums); i++ {
+            if nums[i] + nums[i-1] < minSum {
+                minSum = nums[i] + nums[i-1]
+                minSumIdx = i
+            }
+        }
+        // sum the min sum pair, remove extra item
+        nums[minSumIdx-1] += nums[minSumIdx]
+        nums = append(nums[:minSumIdx], nums[minSumIdx + 1:]...)
+    }
+    return opsCnt
 }
-
-5,2,3,1,4
-
-6454
