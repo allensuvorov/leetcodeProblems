dc7c0323e907f5f02714c2f339412f0c9ab3304b
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-09-27 10:48:32 -0700

    Update 2331_evaluate_boolean_binary_tree.go

--- go/easy/2331_evaluate_boolean_binary_tree.go
+++ go/easy/2331_evaluate_boolean_binary_tree.go
@@ -7,27 +7,16 @@
  * }
  */
 func evaluateTree(root *TreeNode) bool {
-    if root == nil {
-        return false
+    if root.Val < 2 {
+        return root.Val == 1
     }
-    numToBool := map[int]bool{0:false, 1:true}
     
-    // leaf - convert to bool
-    if root.Left == nil {
-        return numToBool[root.Val]
-    }
-
-    // non leaf - do operation
-    numToOperation := map[int] func(a,b bool) bool {
-        2: func(a,b bool) bool {
-            return a || b
-        },
-        3: func(a,b bool) bool {
-            return a && b
-        },
-    }
-
     left := evaluateTree(root.Left)
     right := evaluateTree(root.Right)
-    return numToOperation[root.Val](left, right)
+
+    if root.Val == 2 {
+        return left || right
+    }
+
+    return left && right    
 }
