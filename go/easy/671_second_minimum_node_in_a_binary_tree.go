10cd8169992cf69195104e2d092b06cff5dabc11
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-10-04 21:48:29 -0700

    Update 671_second_minimum_node_in_a_binary_tree.go

--- go/easy/671_second_minimum_node_in_a_binary_tree.go
+++ go/easy/671_second_minimum_node_in_a_binary_tree.go
@@ -7,37 +7,25 @@
  * }
  */
 func findSecondMinimumValue(root *TreeNode) int {
-    if root == nil {
-        return -1
+    min1 := root.Val
+    min2 := math.MaxInt
+
+    var dfs func(root *TreeNode)
+    dfs = func(root *TreeNode) {
+        if root != nil {
+            if root.Val > min1 {
+                min2 = min(min2, root.Val)
+            } else {
+                dfs(root.Left)
+                dfs(root.Right)
+            }
+        }
     }
 
-    if root.Left == nil { // no children
-        return -1
-    }
-
-    var l, r int
-
-    if root.Left.Val == root.Val {
-        l = findSecondMinimumValue(root.Left)
-    } else {
-        l = root.Left.Val
-    }
+    dfs(root)
     
-    if root.Right.Val == root.Val {
-        r = findSecondMinimumValue(root.Right)
-    } else {
-        r = root.Right.Val
-    }
-
-    switch {
-    case l == -1 && r == -1:
-        return -1
-    case l != -1 && r != -1:
-        return min(l,r)
-    case l != -1:
-        return l
-    case r != -1:
-        return r
+    if min2 < math.MaxInt {
+        return min2
     }
 
     return -1
