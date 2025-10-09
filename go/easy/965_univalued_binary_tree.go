670498c52cef2c1cdc5c2a1be93218c561134fd4
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-10-08 18:38:47 -0700

    Create 965_univalued_binary_tree.go

---
+++ go/easy/965_univalued_binary_tree.go
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isUnivalTree(root *TreeNode) bool {
+    if root == nil {
+        return true
+    }
+
+    if root.Left != nil {
+        if root.Left.Val != root.Val {
+            return false
+        }
+    }
+    if root.Right != nil {
+        if root.Right.Val != root.Val {
+            return false
+        }
+    }
+
+    return isUnivalTree(root.Left) && isUnivalTree(root.Right)
+}
