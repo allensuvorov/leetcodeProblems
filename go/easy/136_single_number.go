e6910c67c725f41fa8dbba4b57d51c1d0e135ab0
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-10-16 11:42:00 -0700

    Update 136_single_number.go

--- go/easy/136_single_number.go
+++ go/easy/136_single_number.go
@@ -1,7 +1,7 @@
 func singleNumber(nums []int) int {
-    num := 0
+    single := 0 
     for _, v := range nums {
-        num ^= v
+        single = single ^ v
     }
-    return num
+    return single
 }
