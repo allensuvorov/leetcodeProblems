b8711e250a4cef03f247cc2ddcd23aa9842c9e9b
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-10-17 11:58:14 -0700

    Update 1757_recyclable_and_low_fat_products.sql

--- SQL/ easy/1757_recyclable_and_low_fat_products.sql
+++ SQL/ easy/1757_recyclable_and_low_fat_products.sql
@@ -1,5 +1,7 @@
--- Write your PostgreSQL query statement below
-SELECT product_id 
-FROM Products
-WHERE low_fats = 'Y'
-AND recyclable = 'Y'
+# Write your MySQL query statement below
+SELECT 
+    product_id 
+FROM 
+    Products 
+WHERE 
+    low_fats = 'Y' AND recyclable = 'Y';
