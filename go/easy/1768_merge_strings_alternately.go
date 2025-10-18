400b044ead108937c106a1dbc495f82dbbfdda5e
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-10-18 15:34:05 -0700

    Update 1768_merge_strings_alternately.go
    redo

--- go/easy/1768_merge_strings_alternately.go
+++ go/easy/1768_merge_strings_alternately.go
@@ -1,15 +1,11 @@
 func mergeAlternately(word1 string, word2 string) string {
-    n1, n2 := len(word1), len(word2)
-    res := make([]byte, 0, n1 + n2)
-    i, j := 0, 0
-    for range max(n1, n2) {
+    res := make([]byte, 0, len(word1) + len(word2))
+    for i := range max(len(word1), len(word2)) {
         if i < len(word1) {
             res = append(res, word1[i])
-            i++
         }
-        if j < len(word2) {
-            res = append(res, word2[j])
-            j++
+        if i < len(word2) {
+            res = append(res, word2[i])
         }
     }
     return string(res)
