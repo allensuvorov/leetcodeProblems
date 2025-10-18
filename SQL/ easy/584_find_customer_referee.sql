22596eeb8e88e55229f674493387e95afc5e31ec
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-10-18 15:44:55 -0700

    Update 584_find_customer_referee.sql
    redo

--- SQL/ easy/584_find_customer_referee.sql
+++ SQL/ easy/584_find_customer_referee.sql
@@ -1,4 +1,7 @@
--- Write your PostgreSQL query statement below
-SELECT name
-FROM Customer
-WHERE referee_id != 2 OR referee_id IS NULL
+# Write your MySQL query statement below
+SELECT 
+    name
+FROM
+    Customer
+WHERE
+    referee_id != 2 OR referee_id IS NULL
