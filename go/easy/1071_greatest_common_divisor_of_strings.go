26a1ba75db7702f9e2fff147dc6ee05371777c08
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-10-19 16:30:27 -0700

    Refactor gcdOfStrings for better performance
    Refactor gcdOfStrings function to improve efficiency and clarity.

--- go/easy/1071_greatest_common_divisor_of_strings.go
+++ go/easy/1071_greatest_common_divisor_of_strings.go
@@ -1,3 +1,6 @@
+// Time Big O(n+m)
+// Space Big O(n+m)
+
 func gcdOfStrings(str1 string, str2 string) string {
     if str1 + str2 != str2 + str1 {
         return ""
@@ -9,4 +12,30 @@ func gcdOfStrings(str1 string, str2 string) string {
     }
 
     return str1[:divLen]
+}
+
+// Time Big O(n+m)
+// Space Big O(min(n,m))
+
+func gcdOfStrings(str1 string, str2 string) string {
+    lenGcd := 0
+    for d := 1; d <= min(len(str1), len(str2)); d++ {
+        if len(str1) % d == 0 && len(str2) % d == 0 {
+            lenGcd = d
+        }
+    }
+
+    gcd := str1[:lenGcd]
+    for i := 0; i <= len(str1) - lenGcd; i += lenGcd {
+        if str1[i : i + lenGcd] != gcd {
+            return ""
+        }
+    }
+    for i := 0; i <= len(str2) - lenGcd; i += lenGcd {
+        if str2[i : i + lenGcd] != gcd {
+            return ""
+        }
+    }
+
+    return gcd
 }
