99731ca52a6e6bd8521c37707116ee3c52bb85c6
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-10-20 10:19:12 -0700

    Update 1431_kids_with_the_greatest_number_of_candies.go
    redo

--- go/easy/1431_kids_with_the_greatest_number_of_candies.go
+++ go/easy/1431_kids_with_the_greatest_number_of_candies.go
@@ -1,12 +1,12 @@
 func kidsWithCandies(candies []int, extraCandies int) []bool {
-    gnc := 0
-    for _, count := range candies {
-        gnc = max(gnc, count)
+    maxCandies := 0
+    for _, v := range candies {
+        maxCandies = max(maxCandies, v)
     }
 
     res := make([]bool, len(candies))
-    for i, count := range candies {
-        res[i] = count + extraCandies >= gnc
+    for i, v := range candies {
+        res[i] = v + extraCandies >= maxCandies
     }
     return res
 }
