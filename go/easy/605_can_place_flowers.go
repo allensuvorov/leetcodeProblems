1a8993adcdb43914d021cf3bedb705d47f6742a5
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-10-21 09:31:24 -0700

    Refactor canPlaceFlowers function for clarity

--- go/easy/605_can_place_flowers.go
+++ go/easy/605_can_place_flowers.go
@@ -1,36 +1,13 @@
 func canPlaceFlowers(flowerbed []int, n int) bool {
-    for i := range flowerbed {
-        if flowerbed[i] == 0 { // current
-            if i == 0 || flowerbed[i - 1] == 0 { // prev
-                if i == len(flowerbed) - 1 || flowerbed[i+1] == 0 { // next
+    for i := 0; n > 0 && i < len(flowerbed); i++ {
+        if flowerbed[i] == 0 {
+            if i == 0 || flowerbed[i-1] == 0 {
+                if i == (len(flowerbed) - 1) || flowerbed[i+1] == 0 {
                     flowerbed[i] = 1
-                    n--
+                    n = n - 1
                 }
             }
         }
     }
-
-    return n <= 0
-}
-
-// early exit
-func canPlaceFlowers(flowerbed []int, n int) bool {
-    for i := range flowerbed {
-        if flowerbed[i] != 0 { // current
-            continue
-        }
-
-        if i != 0 && flowerbed[i - 1] == 1 { // prev
-            continue
-        }
-
-        if i != len(flowerbed) - 1 && flowerbed[i+1] == 1 { // next
-            continue
-        }
-
-        flowerbed[i] = 1
-        n--
-    }
-
-    return n <= 0
+    return n == 0
 }
