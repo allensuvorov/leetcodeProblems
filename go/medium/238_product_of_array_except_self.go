b4f0162674023184f769062577e9725bec139c70
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-10-24 17:00:06 -0700

    Update 238_product_of_array_except_self.go

--- go/medium/238_product_of_array_except_self.go
+++ go/medium/238_product_of_array_except_self.go
@@ -1,41 +1,18 @@
 func productExceptSelf(nums []int) []int {
-    n := len(nums)
-    res := make([]int, n)
-    // right prefix products
-    right := 1
-    for i := n - 1; i >= 0; i-- {
-        res[i] = right
-        right *= nums[i]
-    }
-    // left prefix products
-    left := 1
+    // prefixes ->
+    prefixes := make([]int, len(nums))
+    prefix := 1
     for i := range nums {
-        res[i] *= left
-        left *= nums[i]
+        prefixes[i] = prefix
+        prefix = prefix * nums[i]
     }
 
-    return res
-}
-
-// linear extra space
-func productExceptSelf(nums []int) []int {
-    n := len(nums)
-    prefixProduct := make([]int, n)
-    suffixProduct := make([]int, n)
-
-    prefixProduct[0] = 1
-    for i := 1; i < n; i++ {
-        prefixProduct[i] = prefixProduct[i-1] * nums[i-1]
+    // input array - suffixes  <-
+    suffix := 1
+    for i := len(nums) - 1; i >= 0; i-- {
+        prefixes[i] *= suffix
+        suffix = suffix * nums[i]
     }
 
-    suffixProduct[n-1] = 1
-    for i := n - 2; i >= 0; i-- {
-        suffixProduct[i] = suffixProduct[i+1] * nums[i+1]
-    }
-
-    result := make([]int, n)
-    for i := range n {
-        result[i] = prefixProduct[i] * suffixProduct[i]
-    }
-    return result
+    return prefixes
 }
