97798b109dcd327eeeecf40ae6edfba7fa852333
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-10-28 09:58:14 -0700

    Update 392_is_subsequence.go
    redo

--- go/easy/392_is_subsequence.go
+++ go/easy/392_is_subsequence.go
@@ -1,9 +1,9 @@
 func isSubsequence(s string, t string) bool {
-    j := 0
-    for i := range t {
-        if j < len(s) && s[j] == t[i] {
-            j++
+    check := 0 // sequential check list
+    for scan := 0; check < len(s) && scan < len(t); scan++ {
+        if s[check] == t[scan] {
+            check++
         }
     }
-    return j == len(s)
+    return check == len(s)
 }
