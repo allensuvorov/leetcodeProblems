ab83b287e2e0fd31950087edbe72fd2f7ff963c3
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-11-01 14:02:50 -0700

    Update 643_maximum_average_subarray_i.go

--- go/easy/643_maximum_average_subarray_i.go
+++ go/easy/643_maximum_average_subarray_i.go
@@ -3,9 +3,11 @@ func findMaxAverage(nums []int, k int) float64 {
     curSum := 0
     for i := range nums {
         curSum += nums[i]
-        if i + 1 >= k { // full window
+        if i >= k { // we have a tail item - cut it
+            curSum -= nums[i - k]
+        }
+        if i + 1 >= k { // we have a k lenth subarray
             maxSum = max(maxSum, curSum)
-            curSum -= nums[i + 1 - k] // cut tail
         }
     }
     return float64(maxSum)/float64(k)
