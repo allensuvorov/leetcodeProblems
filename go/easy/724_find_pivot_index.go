d3d7a4f4659337930330ab074e2f37d8e938a12e
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-11-06 08:33:13 -0800

    Update 724_find_pivot_index.go
    redo

--- go/easy/724_find_pivot_index.go
+++ go/easy/724_find_pivot_index.go
@@ -1,15 +1,20 @@
 func pivotIndex(nums []int) int {
-    rightSum := 0
-    for _, v := range nums {
-        rightSum += v
+    totalSum := 0
+    for _, v := range nums{
+        totalSum += v
     }
+
     leftSum := 0
-    for i, v := range nums {
+    rightSum := totalSum
+
+    for i, v := range nums{
         rightSum -= v
         if leftSum == rightSum {
             return i
         }
         leftSum += v
     }
+
     return -1
 }
+
