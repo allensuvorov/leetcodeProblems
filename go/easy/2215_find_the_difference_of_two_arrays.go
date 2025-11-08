ca1a1750a5f47429afc82b13e953b78d7ed5b936
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-11-07 16:08:20 -0800

    Update 2215_find_the_difference_of_two_arrays.go

--- go/easy/2215_find_the_difference_of_two_arrays.go
+++ go/easy/2215_find_the_difference_of_two_arrays.go
@@ -1,29 +1,25 @@
 func findDifference(nums1 []int, nums2 []int) [][]int {
     set1 := make(map[int]bool)
     set2 := make(map[int]bool)
-
     for _, v := range nums1 {
         set1[v] = true
     }
-
     for _, v := range nums2 {
         set2[v] = true
     }
-    
-    // check what is missing in nums2 (set2)
-    result1 := []int{}
-    for v := range set1 {
-        if !set2[v] {
-            result1 = append(result1, v)
-        }
-    }
 
-    // check what is missing in nums1 (set1)
-    result2 := []int{}
-    for v := range set2 {
-        if !set1[v] {
-            result2 = append(result2, v)
+    diff1 := []int{}
+    for k := range set1 {
+        if !set2[k] {
+            diff1 = append(diff1, k)
         }
     }
-    return [][]int{result1, result2}
+    
+    diff2 := []int{}
+    for k := range set2 {
+        if !set1[k] {
+            diff2 = append(diff2, k)
+        }
+    }
+    return [][]int{diff1, diff2}
 }
