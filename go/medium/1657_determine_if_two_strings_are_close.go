4a3fadece10c7f8dc4f40340ded12e34ca9bf650
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-11-10 08:09:33 -0800

    Update 1657_determine_if_two_strings_are_close.go

--- go/medium/1657_determine_if_two_strings_are_close.go
+++ go/medium/1657_determine_if_two_strings_are_close.go
@@ -1,37 +1,34 @@
 func closeStrings(word1 string, word2 string) bool {
-    if len(word1) != len(word2) {
-        return false
-    }
-
-    var charSet1, charSet2 [26]bool
-    for i := range word1 {
-        charSet1[word1[i] - 'a'] = true
-        charSet2[word2[i] - 'a'] = true
-    }
-
-    if charSet1 != charSet2 {
-        return false
-    }
-
-    var charCount1, charCount2 [26]int
-
+    charCount1 := [26]int{}
     for i := range word1 {
         charCount1[word1[i] - 'a']++
+    }
+
+    charCount2 := [26]int{}
+    for i := range word2 {
         charCount2[word2[i] - 'a']++
     }
 
-    freqCount1, freqCount2 := map[int]int{}, map[int]int{}
-
-    for i := range charCount1 {
-        freqCount1[charCount1[i]]++
-        freqCount2[charCount2[i]]++
-    }
-
-    for freq1, count1 := range freqCount1 {
-        if count2, ok := freqCount2[freq1]; !ok || count1 != count2 {
+    for i := range 26 {
+        if charCount1[i] == 0 && charCount2[i] != 0 {
+            return false
+        }
+        if charCount1[i] != 0 && charCount2[i] == 0 {
             return false
         }
     }
 
-    return  true
+    freqCount1 := map[int]int{}
+    freqCount2 := map[int]int{}
+    for i := range 26 {
+        freqCount1[charCount1[i]]++
+        freqCount2[charCount2[i]]++
+    }
+
+    for k, v := range freqCount1{
+        if freqCount2[k] != v {
+            return false
+        }
+    }
+    return true
 }
