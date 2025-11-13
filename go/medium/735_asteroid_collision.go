1d611c8cd87e94e6a0acaed6cc019b50e39663e2
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-11-13 13:48:37 -0800

    Update 735_asteroid_collision.go
    redo

--- go/medium/735_asteroid_collision.go
+++ go/medium/735_asteroid_collision.go
@@ -1,47 +1,19 @@
 func asteroidCollision(asteroids []int) []int {
-    stack := make([]int, 0, len(asteroids))
-    for _, r := range asteroids {
-        top := len(stack) - 1
-        // collision cases checks: both exist and both trying to collide
-        for len(stack) > 0 && r < 0 && stack[top] > 0 {
-            switch {
-            case stack[top] > -r: // right asteroid explodes
-                r = 0
-            case stack[top] < -r: // left asteroid explodes
-                stack = stack[:top] 
-            default:                       // both explode
-                r = 0
-                stack = stack[:top]
+    stack := []int{}
+    for _, v := range asteroids {
+        for len(stack) > 0 && v < 0 && stack[len(stack) - 1] > 0 { // collision
+            if stack[len(stack) - 1] < -v {
+                stack = stack[:len(stack) - 1] // explode
+            } else if stack[len(stack) - 1] == -v {
+                stack = stack[:len(stack) - 1] // explode
+                v = 0 // explode
+            } else if stack[len(stack) - 1] > -v {
+                v = 0 // explode
             }
-            top = len(stack) - 1
         }
-        if r != 0 { // append, if right asteroid survives collisions
-            stack = append(stack, r)
+        if v != 0 {
+            stack = append(stack, v)
         }
-        
     }
-    return stack
-}
-
-// redo
-func asteroidCollision(asteroids []int) []int {
-	stack := make([]int, 0)
-	for _, v := range asteroids {
-		stack = append(stack, v)
-		if len(stack) > 1 {
-			for len(stack) > 1 && stack[len(stack)-2] > 0 && stack[len(stack)-1] < 0 {
-				a := stack[len(stack)-2]
-				b := stack[len(stack)-1]
-				if a < -b {
-					stack[len(stack)-2] = b
-					stack = stack[:len(stack)-1]
-				} else if a > -b {
-                    stack = stack[:len(stack)-1]
-				} else {
-					stack = stack[:len(stack)-2]
-                }
-			}
-		}
-	}
     return stack
 }
