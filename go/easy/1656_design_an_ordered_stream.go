41f3906422649f903b0edf19b811c588fdb0fed1
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-11-18 13:12:28 -0800

    Implement OrderedStream with Insert method

---
+++ go/easy/1656_design_an_ordered_stream.go
@@ -0,0 +1,30 @@
+type OrderedStream struct {
+    data []string
+    ptr int
+}
+
+
+func Constructor(n int) OrderedStream {
+    return OrderedStream{
+        data: make([]string, n),
+        ptr: 0,
+    }
+}
+
+
+func (this *OrderedStream) Insert(idKey int, value string) []string {
+    this.data[idKey-1] = value
+    chunk := []string{}
+    for this.ptr < len(this.data) && this.data[this.ptr] != "" {
+        chunk = append(chunk, this.data[this.ptr])
+        this.ptr++
+    }
+    return chunk
+}
+
+
+/**
+ * Your OrderedStream object will be instantiated and called as such:
+ * obj := Constructor(n);
+ * param_1 := obj.Insert(idKey,value);
+ */
