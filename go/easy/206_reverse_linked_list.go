0d19026da4f0f8a32e759ffb098a34d4510ec733
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-11-19 09:21:16 -0800

    Update 206_reverse_linked_list.go

--- go/easy/206_reverse_linked_list.go
+++ go/easy/206_reverse_linked_list.go
@@ -12,3 +12,16 @@ func reverseList(head *ListNode) *ListNode {
     }
     return prev // that dummy is also our new head
 }
+
+// full version
+func reverseList(head *ListNode) *ListNode {
+    var prev *ListNode // dummy
+    curr := head
+    for curr != nil {
+        next := curr.Next // temp
+        curr.Next = prev
+        prev = curr
+        curr = next
+    }
+    return prev
+}
