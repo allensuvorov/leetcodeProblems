fd0774e995839d5c8a2b61cf27a2665ce2c630ce
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-11-20 08:42:14 -0800

    Update 104_maximum_depth_of_binary_tree.go
    redo

--- go/easy/104_maximum_depth_of_binary_tree.go
+++ go/easy/104_maximum_depth_of_binary_tree.go
@@ -10,5 +10,6 @@ func maxDepth(root *TreeNode) int {
     if root == nil {
         return 0
     }
+    // if node exists - count it's length as 1 and pick max of left and right recursively
     return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
 }
