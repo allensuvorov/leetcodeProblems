891a6918a5eba19d801a75169a9fe04c2c43d707
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-11-24 09:36:38 -0800

    Update 2095_delete_the_middle_node_of_a_linked_list.go

--- go/medium/2095_delete_the_middle_node_of_a_linked_list.go
+++ go/medium/2095_delete_the_middle_node_of_a_linked_list.go
@@ -19,3 +19,26 @@ func deleteMiddle(head *ListNode) *ListNode {
     slow.Next = slow.Next.Next // delete middle node
     return head
 }
+
+// with prev pointer
+func deleteMiddle(head *ListNode) *ListNode {
+    // special case - one node list
+    if head.Next == nil {
+        return nil
+    }
+
+    // find middle node and prev node (find prev and next)
+    var prev *ListNode
+    slow := head // middle one
+    fast := head // finds the end of list at double speed
+
+    for fast != nil && fast.Next != nil {
+        prev = slow
+        slow = slow.Next
+        fast = fast.Next.Next
+    } 
+
+    // remove middle node
+    prev.Next = slow.Next // link
+    return head
+}
