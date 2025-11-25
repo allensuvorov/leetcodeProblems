28fa1131fb7b3baebb7e862edfd954055e412832
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-11-25 10:16:22 -0800

    Update 328_odd_even_linked_list.go

--- go/medium/328_odd_even_linked_list.go
+++ go/medium/328_odd_even_linked_list.go
@@ -24,3 +24,47 @@ func oddEvenList(head *ListNode) *ListNode {
 
     return head
 }
+
+// raw version, before refactoring
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func oddEvenList(head *ListNode) *ListNode {
+    // zero nodes
+    if head == nil {
+        return nil
+    }
+    // 1 node + // 2 nodes
+    if head.Next == nil || head.Next.Next == nil {
+        return head
+    }
+    
+    // 3 nodes
+    oddHead := head // 1
+    oddTail := head
+    
+    evenHead := head.Next // 2
+    evenTail := head.Next
+    isOdd := true
+
+    for runner := head.Next.Next; runner != nil; runner = runner.Next {
+        if isOdd {
+            oddTail.Next = runner // 1-3-5-> nil
+            oddTail = oddTail.Next //    t
+        } else {
+            evenTail.Next = runner   //  2-4->5
+            evenTail = evenTail.Next //    t
+        }
+        isOdd = !isOdd
+    }
+
+    evenTail.Next = nil
+
+    oddTail.Next = evenHead
+    return oddHead
+}
