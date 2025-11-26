f810d09d641d31bcd6a6f8a93f6f27536bafe737
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-11-26 15:38:37 -0800

    Refactor pairSum to optimize twin sum calculation
    Refactored pairSum function to reverse the first half of the linked list and calculate the maximum twin sum.

--- go/medium/2130_maximum_twin_sum_of_a_linked_list.go
+++ go/medium/2130_maximum_twin_sum_of_a_linked_list.go
@@ -5,6 +5,8 @@
  *     Next *ListNode
  * }
  */
+
+// reverse 1st half
 func pairSum(head *ListNode) int {
     var dummy *ListNode
     prev := dummy
@@ -19,4 +21,38 @@ func pairSum(head *ListNode) int {
         res = max(res, r1.Val + r2.Val)
     }
     return res
+}
+
+
+// reverse 1st half
+func pairSum(head *ListNode) int {
+    // space O(1)
+
+    // read len and reverse first half
+    fast := head // for finding the middle
+    head2 := head // reversed new head2
+    
+    var head1 *ListNode // dummy, to be the head1
+    
+    for fast != nil && fast.Next != nil {
+        // end of list runner
+        fast = fast.Next.Next
+
+        // reversal mechanics
+        temp := head2.Next
+        head2.Next = head1 // reversal operation
+        head1 = head2
+        head2 = temp
+    }
+
+    // fmt.Println(head1.Val, head2.Val)
+    // get result
+    maxSum := 0
+    for head1 != nil && head2 != nil {
+        fmt.Println(head1.Val, head2.Val)
+        maxSum = max(maxSum, head1.Val + head2.Val)
+        head1 = head1.Next
+        head2 = head2.Next
+    }
+    return maxSum
 }
