208ae1d6f6f0d28423978100b8d3f59c598c1468
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-11-29 15:47:14 -0800

    Refactor StreamChecker and trie implementation

--- go/hard/1032_stream_of_characters.go
+++ go/hard/1032_stream_of_characters.go
@@ -1,41 +1,38 @@
 type StreamChecker struct {
     stream []byte
-    t trie
+    root *trieNode
 }
+
 
 type trieNode struct {
     children [26]*trieNode
     isEndOfWord bool
 }
 
-type trie struct {
-    root *trieNode
-}
 
-func newTrie () trie {
-    t := trie {
-        root: new(trieNode),
-    }
-    return t
-}
-
-func (t *trie) insert(word string) {
-    now := t.root
-    for i := len(word)-1; i >= 0; i-- {
-        c := word[i]
-        if now.children[c - 'a'] == nil {
-            now.children[c - 'a'] = new(trieNode)
+func Constructor(words []string) StreamChecker {
+    var stream []byte
+    root := new(trieNode)
+    for _, word := range words {
+        now := root
+        for i := len(word)-1; i >= 0; i-- {
+            c := word[i]
+            if now.children[c - 'a'] == nil {
+                now.children[c - 'a'] = new(trieNode)
+            }
+            now = now.children[c - 'a']
         }
-        now = now.children[c - 'a']
+        now.isEndOfWord = true
     }
-    now.isEndOfWord = true
+    return StreamChecker{stream, root}
 }
 
 
-func (t *trie) query(stream []byte) bool {
-    now := t.root
-    for i := len(stream) - 1; i >= 0; i-- {
-        c := stream[i]
+func (this *StreamChecker) Query(letter byte) bool {
+    this.stream = append(this.stream, letter)
+    now := this.root
+    for i := len(this.stream) - 1; i >= 0; i-- {
+        c := this.stream[i]
         if now.children[c - 'a'] == nil {
             return false
         }
@@ -45,22 +42,6 @@ func (t *trie) query(stream []byte) bool {
         }
     }
     return false
-}
-
-func Constructor(words []string) StreamChecker {
-    var stream []byte
-    t := newTrie()
-    for _, word := range words {
-        t.insert(word)
-    }
-    return StreamChecker{stream, t}
-}
-
-
-func (this *StreamChecker) Query(letter byte) bool {
-    this.stream = append(this.stream, letter)
-    // fmt.Println(string(this.stream))
-    return this.t.query(this.stream)
 }
 
 
