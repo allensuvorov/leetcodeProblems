676cd7c511fd963ed3aadb6ed9f068d8e91aba7e
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-12-04 15:17:23 -0800

    Add O(n^2) implementation of maxSubArray
    Added a second implementation of maxSubArray with O(n^2) time complexity. Which TLEs on leetcode.

--- go/medium/53_maximum_subarray.go
+++ go/medium/53_maximum_subarray.go
@@ -1,3 +1,4 @@
+// O(n) time
 func maxSubArray(nums []int) int {
     curSum := 0
     curMax := nums[0]
@@ -6,4 +7,17 @@ func maxSubArray(nums []int) int {
         curMax = max(curMax, curSum)
     }
     return curMax
+}
+
+// O(n^2) time
+func maxSubArray(nums []int) int {
+    ans := math.MinInt
+    for i := range nums {
+        curSum := 0
+        for j := i; j < len(nums); j++ {
+            curSum += nums[j]
+            ans = max(ans, curSum)
+        }
+    }
+    return ans
 }
