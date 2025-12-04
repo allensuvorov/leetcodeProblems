e09c11f217ac2a0149b37a9c5ebcd09be4ab6038
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-12-03 17:38:45 -0800

    Update 1466_reorder_routes_to_make_all_paths_lead_to_the_city_zero.go

--- go/medium/1466_reorder_routes_to_make_all_paths_lead_to_the_city_zero.go
+++ go/medium/1466_reorder_routes_to_make_all_paths_lead_to_the_city_zero.go
@@ -19,3 +19,36 @@ func minReorder(n int, connections [][]int) int {
     dfs(0, -1)
     return count
 }
+
+// explicit
+func minReorder(n int, connections [][]int) int {
+    // build undirected graph adjList with a direction parameter (1 - reversed, 0 - not reversed)
+    // DFS and count sum of those direction parameters.
+    g := make([][]pair, n) // city -> list of pairs
+    for _, v := range connections {
+        a := v[0]
+        b := v[1]
+        g[a] = append(g[a], pair{b, 1})
+        g[b] = append(g[b], pair{a, 0})
+    }
+    seen := make([]bool, n)
+    res := 0
+    
+    for stack := []int{0}; len(stack) > 0;  {
+        now := stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+        seen[now] = true
+        for _, nei := range g[now] {
+            if !seen[nei.city] {
+                stack = append(stack, nei.city)
+                res += nei.direction
+            }
+        }
+    }
+    return res
+}
+
+type pair struct {
+    city int
+    direction int // 1 - reversed, 0 - not reversed
+}
