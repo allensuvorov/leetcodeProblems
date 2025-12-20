fb61a51e0c50ae0877d9976c3fbf6b3c45cb585c
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-12-20 10:50:42 -0800

    Refactor partitionString to use hash map
    Refactor partitionString function to use a hash map for tracking seen characters instead of an array.

--- go/medium/2405_optimal_partition_of_string.go
+++ go/medium/2405_optimal_partition_of_string.go
@@ -1,5 +1,5 @@
 package medium
-
+// set implemented with array
 func partitionString(s string) int {
     set := [26]bool{}
     count := 0
@@ -11,4 +11,17 @@ func partitionString(s string) int {
         set[r-'a'] = true
     }
     return count+1
+}
+// set implemented with hash map
+func partitionString(s string) int {
+    count := 1
+    seen := make(map[byte]bool)
+    for i := range s {
+        if seen[s[i]] {
+            count++
+            seen = make(map[byte]bool)
+        }
+        seen[s[i]] = true    
+    }
+    return count
 }
