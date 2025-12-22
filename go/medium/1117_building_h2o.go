03446bdc378a2cc0260ef0662779747909abb998
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-12-21 19:39:56 -0800

    Refactor H2O struct and synchronization logic
    * mutex and 2 channels

--- go/medium/1117_building_h2o.go
+++ go/medium/1117_building_h2o.go
@@ -1,12 +1,11 @@
 type H2O struct {
-    oxyChan chan int
+    mu sync.Mutex
     hydGo chan int
     hydReleased chan int
 }
 
 func NewH2O() *H2O {
 	h := &H2O{
-        oxyChan: make(chan int, 1),
         hydGo: make(chan int, 2),
         hydReleased: make(chan int, 2), 
     }
@@ -14,40 +13,17 @@ func NewH2O() *H2O {
 }
 
 func (h *H2O) Hydrogen(releaseHydrogen func()) {
-    h.hydGo <- 1 // 
+    <- h.hydGo // 
     // releaseHydrogen() outputs "H". Do not change or remove this line.
 	releaseHydrogen()
-    h.hydReleased <- 1 // i released it. ok?
+    h.hydReleased <- 1
 }
 
 func (h *H2O) Oxygen(releaseOxygen func()) {
-    h.oxyChan <- 1 // O
+    h.mu.Lock()
+    for range 2 { h.hydGo <- 0}
+    for range 2 { <- h.hydReleased }
     // releaseOxygen() outputs "H". Do not change or remove this line.
 	releaseOxygen()
-    // by now we have the molecule - H2O
-
-    for range 2 {
-        <- h.hydGo // hydrogen release - allow
-    }
-
-    for range 2 { // wait for 2 hydrogen release acks
-        <- h.hydReleased // i see you released it. ok acks
-    }
-
-    <- h.oxyChan
+    h.mu.Unlock()
 }
-
-/*
-
-output: HHO 
-hydGo: HH
-oxyChan: O
-
-- H ->
-- H -> 
-- H ->
-
-- O -> 
-- O ->
-- O ->
-*/
