c977df7e01a5818341bdc6a1600439a2b7e4e9b6
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-12-27 18:56:59 -0800

    Update 1768_merge_strings_alternately.go

--- go/easy/1768_merge_strings_alternately.go
+++ go/easy/1768_merge_strings_alternately.go
@@ -10,3 +10,28 @@ func mergeAlternately(word1 string, word2 string) string {
     }
     return string(res)
 }
+
+// channels practice, receive from two channels
+func mergeAlternately(word1 string, word2 string) string {
+    c1 := make(chan byte)
+    c2 := make(chan byte)
+    go letters(word1, c1)
+    go letters(word2, c2)
+    res := make([]byte, 0, len(word1) + len(word2))
+    for range cap(res) {
+        if v, ok := <- c1; ok {
+            res = append(res, v)
+        }
+        if v, ok := <- c2; ok {
+            res = append(res, v)
+        }
+    }
+    return string(res)
+}
+
+func letters(word string, c chan byte) {
+    for i := range word {
+        c <- word[i]
+    }
+    close(c)
+}
