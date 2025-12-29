f2d54d16a18a2806398c0cf60820ab3bee0d1998
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-12-29 09:48:02 -0800

    Implement concurrent maxDepth function for binary tree
    Added a concurrent solution for calculating the maximum depth of a binary tree using goroutines.

--- go/easy/104_maximum_depth_of_binary_tree.go
+++ go/easy/104_maximum_depth_of_binary_tree.go
@@ -13,3 +13,21 @@ func maxDepth(root *TreeNode) int {
     // if node exists - count it's length as 1 and pick max of left and right recursively
     return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
 }
+
+// concurrent solution, 
+func maxDepth(root *TreeNode) int {
+    input := make(chan int)
+    go concurrentDFS(root, input)
+    return <- input
+}
+
+func concurrentDFS(root *TreeNode, output chan int) {
+    if root == nil {
+        output <- 0
+        return
+    }
+    input := make(chan int)
+    go concurrentDFS(root.Left, input)
+    go concurrentDFS(root.Right, input)
+    output <- 1 + max(<-input, <-input) 
+} 
