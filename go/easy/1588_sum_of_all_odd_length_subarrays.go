21096f2652fb48ec335f590dc4ca25b4a015a0bf
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2025-12-30 13:07:44 -0800

    Implement concurrent sum of odd length subarrays
    * producer workers are not blocked by consumer

--- go/easy/1588_sum_of_all_odd_length_subarrays.go
+++ go/easy/1588_sum_of_all_odd_length_subarrays.go
@@ -51,3 +51,43 @@ func sumSubarrays(arr []int, window int, mu *sync.Mutex, totalSum *int) {
     *totalSum += res
     mu.Unlock()
 }
+
+// concurrent with a buffered channel
+func sumOddLengthSubarrays(arr []int) int {
+    // n^2 with sliding window
+    // [workers] -> ch -> [consumer]
+    n := len(arr)
+    totalSum := 0
+    ch := make(chan int, n/2)
+    var wg sync.WaitGroup
+    for window := 1; window <= len(arr); window += 2 {
+        wg.Add(1)
+        go func() {
+            sumSubarrays(arr, window, ch)
+            wg.Done()
+        }()
+    }
+    go func() {
+        wg.Wait()
+        close(ch)
+    }()
+    for subSum := range ch {
+        totalSum += subSum
+    }
+    return totalSum
+}
+
+func sumSubarrays(arr []int, window int, ch chan int) {
+    res := 0
+    windowSum := 0
+    for i := range arr {
+        windowSum += arr[i]
+        if i + 1 > window { // cut tailing value
+            windowSum -= arr[i - window]
+        }
+        if i + 1 >= window {
+            res += windowSum
+        } 
+    }
+    ch <- res
+}
