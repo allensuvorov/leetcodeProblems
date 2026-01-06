f41194da26a569fb3ebe3767981fbfb6b654c380
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2026-01-05 18:59:54 -0800

    Update 136_single_number.go
    * concurrency with channels

--- go/easy/136_single_number.go
+++ go/easy/136_single_number.go
@@ -5,3 +5,31 @@ func singleNumber(nums []int) int {
     }
     return ans
 }
+
+// concurrency with channels
+// [][][] -> workers -> output chan -> consumer
+func singleNumber(nums []int) int {
+    n := len(nums)
+    workerCount := runtime.NumCPU()
+    jobSize := max(1, n / workerCount)
+    output := make(chan int, workerCount)
+
+    for i := 0; i < n; i += jobSize {
+        job := nums[i: min(n, i+jobSize)]
+        go singleNumberWorker(job, output)    
+    }
+
+    single := 0
+    for i := 0; i < n; i += jobSize {
+        single = single ^ <-output
+    }
+    return single
+}
+
+func singleNumberWorker(nums []int, output chan<- int) {
+    single := 0
+    for _, v := range nums {
+        single = single ^ v
+    }
+    output <- single
+}
