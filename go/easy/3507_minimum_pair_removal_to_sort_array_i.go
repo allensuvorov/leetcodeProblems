f4e58444951f18b49c4c867186507a3113e75dea
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2026-01-17 12:29:52 -0800

    Update 3507_minimum_pair_removal_to_sort_array_i.go

--- go/easy/3507_minimum_pair_removal_to_sort_array_i.go
+++ go/easy/3507_minimum_pair_removal_to_sort_array_i.go
@@ -1,32 +1,40 @@
 func minimumPairRemoval(nums []int) int {
-// simulation
-    opsCnt := 0
-    for len(nums) > 1 {
-        dec := false
-        // check for dec
-        for i := 1; i < len(nums); i++ {
-            if nums[i-1] > nums[i] {
-                dec = true
-                break
-            }
-        }
-        if !dec {
-            return opsCnt
-        }
-        opsCnt++
-
-        // find min sum pair
-        minSum := nums[1] + nums[0]
-        minSumIdx := 1
-        for i := 1; i < len(nums); i++ {
-            if nums[i] + nums[i-1] < minSum {
-                minSum = nums[i] + nums[i-1]
-                minSumIdx = i
-            }
-        }
-        // sum the min sum pair, remove extra item
-        nums[minSumIdx-1] += nums[minSumIdx]
-        nums = append(nums[:minSumIdx], nums[minSumIdx + 1:]...)
+    count := 0
+    for hasDecrease(nums) {
+        doOperation(nums)
+        nums = nums[:len(nums)-1]
+        count++
     }
-    return opsCnt
+    return count
+}
+
+func hasDecrease(nums []int) bool {
+    for i := 0; i + 1 < len(nums); i++ {
+        if nums[i] > nums[i+1] {
+            return true
+        }
+    }
+    return false
+}
+
+func doOperation(nums []int) {
+    // find min sum of a pair
+    minSum := nums[0] + nums[1]
+    for i := 0; i + 1 < len(nums); i++ {
+        minSum = min(minSum, nums[i]+nums[i+1])
+    }
+    // find leftmost pair with the min sum and replace
+    for i := 0; i + 1 < len(nums); i++ {
+        if nums[i] + nums[i+1] == minSum {
+            replacePair(nums, i)
+            return
+        }
+    }
+}
+
+func replacePair(nums []int, i int) {
+    nums[i] = nums[i] + nums[i+1]
+    for i++ ; i + 1 < len(nums) ; i++ {
+        nums[i] = nums[i+1]
+    }
 }
