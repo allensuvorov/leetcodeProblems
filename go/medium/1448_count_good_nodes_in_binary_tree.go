3f77be4de3b28416b094c1691a928d5743200dac
Allen S. <56049635+allensuvorov@users.noreply.github.com>
2026-02-04 13:30:15 -0800

    Update 1448_count_good_nodes_in_binary_tree.go
    * could be numCPU times faster
    *

--- go/medium/1448_count_good_nodes_in_binary_tree.go
+++ go/medium/1448_count_good_nodes_in_binary_tree.go
@@ -7,7 +7,48 @@
  * }
  */
 func goodNodes(root *TreeNode) int {
-    return dfs(root, root.Val)
+    numCPU := runtime.NumCPU()
+    fmt.Printf("numCPU: %v", numCPU )
+    sem := make(chan int, numCPU)
+    return concurrentDFS(root, root.Val, sem)
+}
+
+func concurrentDFS(root *TreeNode, good int, sem chan int) int {
+    if root == nil {
+        return 0
+    }
+    res := 0
+    if root.Val >= good {
+        res += 1
+        good = root.Val
+    }
+    var wg sync.WaitGroup
+    var l, r int
+    
+    select {
+    case sem <- 1:
+        wg.Add(1)
+        go func(){
+            l = concurrentDFS(root.Left, good, sem)
+            wg.Done()
+        }()
+    default:
+        l = dfs(root.Left, good)
+    }
+
+    select {
+    case sem <- 1:
+        wg.Add(1)
+            go func(){
+        r = concurrentDFS(root.Right, good, sem)
+        wg.Done()
+        }()
+    default:
+        r = dfs(root.Right, good)
+    }
+
+    wg.Wait()
+    return res + l + r
 }
 
 func dfs(root *TreeNode, good int) int {
@@ -25,4 +66,3 @@ func dfs(root *TreeNode, good int) int {
 
     return res + l + r
 }
-
